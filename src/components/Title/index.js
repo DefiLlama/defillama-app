f0bacb95a14fb99c416b1b636cf551533348a3a1
0xngmi <0xngmi@protonmail.com>
2021-05-06 13:38:13 +0200

    replace logo

--- src/components/Title/index.js
+++ src/components/Title/index.js
@@ -5,7 +5,7 @@ import styled from 'styled-components'
 import { Flex } from 'rebass'
 import Link from '../Link'
 import { RowFixed } from '../Row'
-import Logo from '../../assets/logo_white_long.png'
+import Logo from '../../assets/logo_white_long.svg'
 
 const TitleWrapper = styled.div`
   text-decoration: none;
