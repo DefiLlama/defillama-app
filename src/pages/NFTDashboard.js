ff5e81858bfb3c1c5dda00f3843c11e19c33955f
degendevv <degendevv@protonmail.com>
2021-10-04 20:34:15 +0100

    Add chart + chain options frontend placeholders

--- src/pages/NFTDashboard.js
+++ src/pages/NFTDashboard.js
@@ -2,10 +2,13 @@ import React, { useEffect } from 'react'
 import { useMedia } from 'react-use'
 import 'feather-icons'
 import { transparentize } from 'polished'
+import styled from 'styled-components'
 
-import { AutoRow, RowBetween } from '../components/Row'
+import { AutoRow, RowBetween, RowFlat } from '../components/Row'
 import { AutoColumn } from '../components/Column'
-import { ButtonDark } from '../components/ButtonStyled'
+import { BasicLink, CustomLink } from '../components/Link'
+import { ButtonDark, ButtonLight } from '../components/ButtonStyled'
+import DropdownSelect from '../components/DropdownSelect'
 import { PageWrapper, ContentWrapper } from '../components'
 import Panel from '../components/Panel'
 import RightSettings from '../components/RightSettings'
@@ -17,17 +20,44 @@ import { useDisplayUsdManager } from '../contexts/LocalStorage'
 import { formattedNum } from '../utils'
 import { useNFTCollectionsData } from '../contexts/NFTData'
 
-const NFTDashboard = (props) => {
+const ListOptions = styled(AutoRow)`
+  height: 40px;
+  width: 100%;
+  font-size: 1.25rem;
+  font-weight: 600;
+
+  @media screen and (max-width: 640px) {
+    font-size: 1rem;
+  }
+`
+
+const basicChainOptions = ['All', 'Ethereum']
+const extraChainOptions = []
+
+const NFTDashboard = ({ history }) => {
   const nftCollections = useNFTCollectionsData()
   const [displayUsd] = useDisplayUsdManager()
   useEffect(() => window.scrollTo(0, 0))
 
   const below800 = useMedia('(max-width: 800px)')
+  const below1400 = useMedia('(max-width: 1400px)')
+
   let title = `NFT Dashboard`
   document.title = `${title} - Defi Llama`
 
   const totalVolumeUsd = nftCollections.reduce((prevSum, collection) => prevSum + collection.dailyVolumeUsd, 0)
   const totalMarketCap = nftCollections.reduce((prevSum, collection) => prevSum + collection.marketCapUsd, 0)
+
+  let chainOptions = []
+  if (!below1400) {
+    chainOptions = [...basicChainOptions, ...extraChainOptions, 'Others']
+  } else {
+    chainOptions = [...basicChainOptions, 'Others']
+  }
+
+  const setSelectedChain = (newSelectedChain) => history.push(newSelectedChain === 'All' ? '/nfts' : `/nfts/chain/${newSelectedChain}`)
+  const selectedChain = 'All';
+  const otherChains = chainOptions.filter(chain => chain !== selectedChain)
 
   return (
     <PageWrapper>
@@ -57,6 +87,19 @@ const NFTDashboard = (props) => {
                     <TYPE.heading>Total Market Cap (USD)</TYPE.heading>
                   </RowBetween>
                   <RowBetween style={{ marginTop: '4px', marginBottom: '4px' }} align="flex-end">
+                    <TYPE.main fontSize={'33px'} lineHeight={'39px'} fontWeight={600} color={'#4f8fea'}>
+                      {formattedNum(totalMarketCap, true)}
+                    </TYPE.main>
+                  </RowBetween>
+                </AutoColumn>
+              </Panel>
+
+              <Panel style={{ padding: '18px 25px' }}>
+                <AutoColumn gap="4px">
+                  <RowBetween>
+                    <TYPE.heading>Change (24h)</TYPE.heading>
+                  </RowBetween>
+                  <RowBetween style={{ marginTop: '4px', marginBottom: '4px' }} align="flex-end">
                     <TYPE.main fontSize={'33px'} lineHeight={'39px'} fontWeight={600} color={'#fd3c99'}>
                       {formattedNum(totalMarketCap, true)}
                     </TYPE.main>
@@ -70,7 +113,7 @@ const NFTDashboard = (props) => {
                     <TYPE.heading>Daily Volume (USD)</TYPE.heading>
                   </RowBetween>
                   <RowBetween style={{ marginTop: '4px', marginBottom: '4px' }} align="flex-end">
-                    <TYPE.main fontSize={'33px'} lineHeight={'39px'} fontWeight={600} color={'#4f8fea'}>
+                    <TYPE.main fontSize={'33px'} lineHeight={'39px'} fontWeight={600} color={'#46acb7'}>
                       {formattedNum(totalVolumeUsd, true)}
                     </TYPE.main>
                   </RowBetween>
@@ -95,6 +138,19 @@ const NFTDashboard = (props) => {
                       <TYPE.heading>Total Market Cap (USD)</TYPE.heading>
                     </RowBetween>
                     <RowBetween style={{ marginTop: '4px', marginBottom: '4px' }} align="flex-end">
+                      <TYPE.main fontSize={'33px'} lineHeight={'39px'} fontWeight={600} color={'#4f8fea'}>
+                        {formattedNum(totalMarketCap, true)}
+                      </TYPE.main>
+                    </RowBetween>
+                  </AutoColumn>
+                </Panel>
+
+                <Panel style={{ padding: '18px 25px' }}>
+                  <AutoColumn gap="4px">
+                    <RowBetween>
+                      <TYPE.heading>Change (24h)</TYPE.heading>
+                    </RowBetween>
+                    <RowBetween style={{ marginTop: '4px', marginBottom: '4px' }} align="flex-end">
                       <TYPE.main fontSize={'33px'} lineHeight={'39px'} fontWeight={600} color={'#fd3c99'}>
                         {formattedNum(totalMarketCap, true)}
                       </TYPE.main>
@@ -108,15 +164,56 @@ const NFTDashboard = (props) => {
                       <TYPE.heading>Daily Volume (USD)</TYPE.heading>
                     </RowBetween>
                     <RowBetween style={{ marginTop: '4px', marginBottom: '4px' }} align="flex-end">
-                      <TYPE.main fontSize={'33px'} lineHeight={'39px'} fontWeight={600} color={'#4f8fea'}>
+                      <TYPE.main fontSize={'33px'} lineHeight={'39px'} fontWeight={600} color={'#46acb7'}>
                         {formattedNum(totalVolumeUsd, true)}
                       </TYPE.main>
                     </RowBetween>
                   </AutoColumn>
                 </Panel>
               </AutoColumn>
+              <Panel style={{ height: '100%', minHeight: '300px' }}>
+                {/* {chart} */}
+              </Panel>
             </AutoRow>
           )}
+
+          {below800 && (
+            <AutoColumn style={{ marginTop: '6px' }} gap="24px">
+              <Panel style={{ height: '100%', minHeight: '300px' }}>
+                {/* {chart} */}
+              </Panel>
+            </AutoColumn>
+          )}
+
+          <ListOptions gap="10px" style={{ marginTop: '2rem', marginBottom: '.5rem' }}>
+            <RowBetween>
+              <TYPE.main fontSize={'1.125rem'}>NFT Rankings</TYPE.main>
+              <RowFlat>
+                {below800 ?
+                  <DropdownSelect options={chainOptions.slice(0, -1).concat(otherChains).reduce((acc, item) => ({
+                    ...acc,
+                    [item]: item
+                  }), {})} active={selectedChain || 'All'} setActive={setSelectedChain} />
+                  :
+                  chainOptions.map((name, i) => {
+                    if (name === "Others") {
+                      return <DropdownSelect key={name} options={otherChains.reduce((acc, item) => ({
+                        ...acc,
+                        [item]: item
+                      }), {})} active={(chainOptions.includes(selectedChain) || selectedChain === undefined) ? 'Other' : selectedChain} setActive={setSelectedChain} />
+                    }
+                    if (selectedChain === name || (name === 'All' && selectedChain === undefined)) {
+                      return <ButtonDark style={{ margin: '0.2rem' }} key={name} >{name}</ButtonDark>
+                    } else {
+                      return <BasicLink to={name === "All" ? '/nfts' : `/nfts/chain/${name}`} key={name}>
+                        <ButtonLight style={{ margin: '0.2rem' }}>{name}</ButtonLight>
+                      </BasicLink>
+                    }
+                  })}
+              </RowFlat>
+              <CustomLink to={'/protocols'}>See All</CustomLink>
+            </RowBetween>
+          </ListOptions>
 
           {nftCollections && (
             <Panel style={{ marginTop: '6px', padding: below800 && '1rem 0 0 0 ' }}>
