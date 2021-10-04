029c696fe28538107c209b7e05e796f2fa5cc810
degendevv <degendevv@protonmail.com>
2021-10-04 15:42:16 +0100

    Load Defi vs. NFT App component based on whether path contains /nfts

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React, { useState } from 'react'
-import styled from 'styled-components'
 import { Route, Switch, BrowserRouter, Redirect } from 'react-router-dom'
+import { AppWrapper, LayoutWrapper } from './layout'
 import GlobalPage from './pages/GlobalPage'
 import TokenPage from './pages/TokenPage'
 import Settings from './pages/Settings'
@@ -14,69 +14,7 @@ import { isValidProtocol } from './utils'
 import AllTokensPage from './pages/AllTokensPage'
 import AboutPage from './pages/AboutPage'
 
-import PinnedData from './components/PinnedData'
-
-import SideNav from './components/SideNav'
 import LocalLoader from './components/LocalLoader'
-import AllNFTsPage from './pages/AllNftsPage'
-
-const AppWrapper = styled.div`
-  position: relative;
-  width: 100%;
-`
-const ContentWrapper = styled.div`
-  display: grid;
-  grid-template-columns: ${({ open }) => (open ? '220px 1fr 200px' : '220px 1fr 64px')};
-
-  @media screen and (max-width: 1400px) {
-    grid-template-columns: 220px 1fr;
-  }
-
-  @media screen and (max-width: 1080px) {
-    grid-template-columns: 1fr;
-    max-width: 100vw;
-    overflow: hidden;
-    grid-gap: 0;
-  }
-`
-
-const Right = styled.div`
-  position: fixed;
-  right: 0;
-  bottom: 0rem;
-  z-index: 99;
-  width: ${({ open }) => (open ? '220px' : '64px')};
-  height: ${({ open }) => (open ? 'fit-content' : '64px')};
-  overflow: scroll;
-  background-color: ${({ theme }) => theme.bg1};
-  @media screen and (max-width: 1400px) {
-    display: none;
-  }
-`
-
-const Center = styled.div`
-  height: 100%;
-  z-index: 9999;
-  transition: width 0.25s ease;
-  background-color: ${({ theme }) => theme.onlyLight};
-`
-
-/**
- * Wrap the component with the header and sidebar pinned tab
- */
-const LayoutWrapper = ({ children, savedOpen, setSavedOpen }) => {
-  return (
-    <>
-      <ContentWrapper open={savedOpen}>
-        <SideNav />
-        <Center id="center">{children}</Center>
-        <Right open={savedOpen}>
-          <PinnedData open={savedOpen} setSavedOpen={setSavedOpen} />
-        </Right>
-      </ContentWrapper>
-    </>
-  )
-}
 
 function App() {
   const [savedOpen, setSavedOpen] = useState(false)
@@ -141,11 +79,6 @@ function App() {
               }}
             />
 
-            <Route path="/nfts">
-              <LayoutWrapper savedOpen={savedOpen} setSavedOpen={setSavedOpen}>
-                <AllNFTsPage />
-              </LayoutWrapper>
-            </Route>
             <Route path="/jpegged">
               <LayoutWrapper savedOpen={savedOpen} setSavedOpen={setSavedOpen}>
                 <Jpegged />
---
+++ src/NFTApp.js
@@ -0,0 +1,55 @@
+import React, { useState } from 'react'
+import { Route, Switch, BrowserRouter, Redirect } from 'react-router-dom'
+
+import { AppWrapper, LayoutWrapper } from './layout'
+import AllNFTsPage from './pages/AllNftsPage'
+import NFTPage from './pages/NFTPage'
+
+import { useNFTCollectionsData } from './contexts/NFTData'
+import { isValidCollection } from './utils'
+import LocalLoader from './components/LocalLoader'
+
+function App() {
+  const [savedOpen, setSavedOpen] = useState(false)
+  const nftCollections = useNFTCollectionsData()
+
+  if (nftCollections.length === 0) {
+    return <AppWrapper><LocalLoader fill="true" /></AppWrapper>
+  }
+
+  return (
+    <AppWrapper>
+      <BrowserRouter>
+        <Switch>
+          <Route
+            exacts
+            strict
+            path="/nfts/collection/:collection"
+            render={({ match }) => {
+              if (isValidCollection(nftCollections, match.params.collection)) {
+                return (
+                  <LayoutWrapper savedOpen={savedOpen} setSavedOpen={setSavedOpen}>
+                    <NFTPage
+                      collection={match.params.collection}
+                    />
+                  </LayoutWrapper>
+                )
+              } else {
+                return <Redirect to="/nfts" />
+              }
+            }}
+          />
+
+          <Route path="/nfts">
+            <LayoutWrapper savedOpen={savedOpen} setSavedOpen={setSavedOpen}>
+              <AllNFTsPage />
+            </LayoutWrapper>
+          </Route>
+
+        </Switch>
+      </BrowserRouter>
+    </AppWrapper>
+  )
+}
+
+export default App
