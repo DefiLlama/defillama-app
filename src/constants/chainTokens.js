c6e379ad16721c9aac4fbc108d71de10dc8fd230
Real Shaman <85087525+realdealshaman@users.noreply.github.com>
2021-10-24 21:18:02 +0000

    update chains
    update chains

--- src/constants/chainTokens.js
+++ src/constants/chainTokens.js
@@ -1,170 +1,282 @@
 export const chainCoingeckoIds = {
-    "Ethereum": {
-        geckoId: "ethereum",
-        symbol: "ETH"
-    },
-    "Binance": {
-        geckoId: "binancecoin",
-        symbol: "BNB"
-    },
-    "Avalanche": {
-        geckoId: "avalanche-2",
-        symbol: "AVAX"
-    },
-    "Solana": {
-        geckoId: "solana",
-        symbol: "SOL"
-    },
-    "Polygon": {
-        geckoId: "matic-network",
-        symbol: "MATIC"
-    },
-    "Terra": {
-        geckoId: "terra-luna",
-        symbol: "LUNA"
-    },
-    "Fantom": {
-        geckoId: "fantom",
-        symbol: "FTM"
-    },
-    "xDai": {
-        geckoId: "xdai-stake",
-        symbol: "STAKE"
-    },
-    "Heco": {
-        geckoId: "huobi-token",
-        symbol: "HT"
-    },
-    "Kava": {
-        geckoId: "kava",
-        symbol: "KAVA"
-    },
-    "OKExChain": {
-        geckoId: "okexchain",
-        symbol: "OKT"
-    },
-    "Wanchain": {
-        geckoId: "wanchain",
-        symbol: "WAN"
-    },
-    "DefiChain": {
-        geckoId: "defichain",
-        symbol: "DFI"
-    },
-    "Ontology": {
-        geckoId: "ontology",
-        symbol: "ONT"
-    },
-    "Bitcoin": {
-        geckoId: "bitcoin",
-        symbol: "BTC"
-    },
-    "Energi": {
-        geckoId: "energi",
-        symbol: "NRG"
-    },
-    "Secret": {
-        geckoId: "secret",
-        symbol: "SCRT"
-    },
-    "Zilliqa": {
-        geckoId: "zilliqa",
-        symbol: "ZIL"
-    },
-    "NEO": {
-        geckoId: "neo",
-        symbol: "NEO"
-    },
-    "Harmony": {
-        geckoId: "harmony",
-        symbol: "ONE"
-    },
-    "RSK": {
-        geckoId: "rootstock",
-        symbol: "RBTC"
-    },
-    "Sifchain": {
-        geckoId: "sifchain",
-        symbol: "EROWAN"
-    },
-    "Algorand": {
-        geckoId: "algorand",
-        symbol: "ALGO"
-    },
-    "Osmosis": {
-        geckoId: "osmosis",
-        symbol: "OSMO"
-    },
-    "Thorchain": {
-        geckoId: "thorchain",
-        symbol: "RUNE"
-    },
-    "Tron": {
-        geckoId: "tron",
-        symbol: "TRON"
-    },
-    "Icon": {
-        geckoId: "icon",
-        symbol: "ICX"
-    },
-    "Tezos": {
-        geckoId: "tezos",
-        symbol: "XTZ"
-    },
-    "Celo": {
-        geckoId: "celo",
-        symbol: "CELO"
-    },
-    "Kucoin": {
-        geckoId: "kucoin-shares",
-        symbol: "KCS"
-    },
-    "Karura": {
-        geckoId: "karura",
-        symbol: "KAR"
-    },
-    "Moonriver": {
-        geckoId: "moonriver",
-        symbol: "MOVR"
-    },
-    "Waves": {
-        geckoId: "waves",
-        symbol: "WAVES"
-    },
-    "Klaytn": {
-        geckoId: "klay-token",
-        symbol: "KLAY"
-    },
-    "IoTeX": {
-        geckoId: "iotex",
-        symbol: "IOTX"
-    },
-    "Ultra": {
-        geckoId: "ultra",
-        symbol: "UOS"
-    },
-    "Kusama": {
-        geckoId: "kusama",
-        symbol: "KSM"
-    },
-    "Telos": {
-        geckoId: "telos",
-        symbol: "TLOS"
-    },
-    "ThunderCore": {
-        geckoId: "thunder-token",
-        symbol: "TT"
-    },
-    "Lamden": {
-        geckoId: "lamden",
-        symbol: "TAU"
-    },
-    "Near": {
-        geckoId: "near",
-        symbol: "NEAR"
-    },
-    "EOS": {
-        geckoId: "eos",
-        symbol: "EOS"
-    },
+  "Ethereum": {
+    geckoId: "ethereum",
+    symbol: "ETH",
+    cmcId: "1027",
+  },
+  "Arbitrum": {
+    geckoId: null,
+    symbol: null,
+    cmcId: "11841",
+  },
+  "Palm": {
+    geckoId: null,
+    symbol: null,
+    cmcId: null,
+  },
+  "Optimism": {
+    geckoId: null,
+    symbol: null,
+    cmcId: null,
+  },
+  "Stacks": {
+    geckoId: "blockstack",
+    symbol: "STX",
+    cmcId: "4847",
+  },
+  "PolyNetwork": {
+    geckoId: null,
+    symbol: null,
+    cmcId: null,
+  },
+  "Conflux": {
+    geckoId: "conflux-token",
+    symbol: "CFX",
+    cmcId: "7334",
+  },
+  "Nuls": {
+    geckoId: "nuls",
+    symbol: "NULS",
+    cmcId: "2092",
+  },
+  "Witnet": {
+    geckoId: null,
+    symbol: null,
+    cmcId: null,
+  },
+  "Binance": {
+    geckoId: "binancecoin",
+    symbol: "BNB",
+    cmcId: "1839",
+  },
+  "Avalanche": {
+    geckoId: "avalanche-2",
+    symbol: "AVAX",
+    cmcId: "5805",
+  },
+  "Solana": {
+    geckoId: "solana",
+    symbol: "SOL",
+    cmcId: "5426",
+  },
+  "Polygon": {
+    geckoId: "matic-network",
+    symbol: "MATIC",
+    cmcId: "3890",
+  },
+  "Terra": {
+    geckoId: "terra-luna",
+    symbol: "LUNA",
+    cmcId: "4172",
+  },
+  "Fantom": {
+    geckoId: "fantom",
+    symbol: "FTM",
+    cmcId: "3513",
+  },
+  "xDai": {
+    geckoId: "xdai-stake",
+    symbol: "STAKE",
+    cmcId: "5601",
+  },
+  "Heco": {
+    geckoId: "huobi-token",
+    symbol: "HT",
+    cmcId: "2502",
+  },
+  "Kava": {
+    geckoId: "kava",
+    symbol: "KAVA",
+    cmcId: "4846",
+  },
+  "OKExChain": {
+    geckoId: "okexchain",
+    symbol: "OKT",
+    cmcId: "8267",
+  },
+  "Wanchain": {
+    geckoId: "wanchain",
+    symbol: "WAN",
+    cmcId: "2606",
+  },
+  "DefiChain": {
+    geckoId: "defichain",
+    symbol: "DFI",
+    cmcId: "5804",
+  },
+  "Ontology": {
+    geckoId: "ontology",
+    symbol: "ONT",
+    cmcId: "2566",
+  },
+  "Bitcoin": {
+    geckoId: "bitcoin",
+    symbol: "BTC",
+    cmcId: "1",
+  },
+  "Energi": {
+    geckoId: "energi",
+    symbol: "NRG",
+    cmcId: "3218",
+  },
+  "Secret": {
+    geckoId: "secret",
+    symbol: "SCRT",
+    cmcId: "5604",
+  },
+  "Zilliqa": {
+    geckoId: "zilliqa",
+    symbol: "ZIL",
+    cmcId: "2469",
+  },
+  "NEO": {
+    geckoId: "neo",
+    symbol: "NEO",
+    cmcId: "1376",
+  },
+  "Harmony": {
+    geckoId: "harmony",
+    symbol: "ONE",
+    cmcId: "3945",
+  },
+  "RSK": {
+    geckoId: "rootstock",
+    symbol: "RBTC",
+    cmcId: "3626",
+  },
+  "Sifchain": {
+    geckoId: "sifchain",
+    symbol: "EROWAN",
+    cmcId: "8541",
+  },
+  "Algorand": {
+    geckoId: "algorand",
+    symbol: "ALGO",
+    cmcId: "4030",
+  },
+  "Osmosis": {
+    geckoId: "osmosis",
+    symbol: "OSMO",
+    cmcId: "12220",
+  },
+  "Thorchain": {
+    geckoId: "thorchain",
+    symbol: "RUNE",
+    cmcId: "4157",
+  },
+  "Tron": {
+    geckoId: "tron",
+    symbol: "TRON",
+    cmcId: "1958",
+  },
+  "Icon": {
+    geckoId: "icon",
+    symbol: "ICX",
+    cmcId: "2099",
+  },
+  "Tezos": {
+    geckoId: "tezos",
+    symbol: "XTZ",
+    cmcId: "2011",
+  },
+  "Celo": {
+    geckoId: "celo",
+    symbol: "CELO",
+    cmcId: "5567",
+  },
+  "Kucoin": {
+    geckoId: "kucoin-shares",
+    symbol: "KCS",
+    cmcId: "2087",
+  },
+  "Karura": {
+    geckoId: "karura",
+    symbol: "KAR",
+    cmcId: "10042",
+  },
+  "Moonriver": {
+    geckoId: "moonriver",
+    symbol: "MOVR",
+    cmcId: "9285",
+  },
+  "Waves": {
+    geckoId: "waves",
+    symbol: "WAVES",
+    cmcId: "1274",
+  },
+  "Klaytn": {
+    geckoId: "klay-token",
+    symbol: "KLAY",
+    cmcId: "4256",
+  },
+  "IoTeX": {
+    geckoId: "iotex",
+    symbol: "IOTX",
+    cmcId: "2777",
+  },
+  "Ultra": {
+    geckoId: "ultra",
+    symbol: "UOS",
+    cmcId: "4189",
+  },
+  "Kusama": {
+    geckoId: "kusama",
+    symbol: "KSM",
+    cmcId: "5034",
+  },
+  "Telos": {
+    geckoId: "telos",
+    symbol: "TLOS",
+    cmcId: "4660",
+  },
+  "ThunderCore": {
+    geckoId: "thunder-token",
+    symbol: "TT",
+    cmcId: "3930",
+  },
+  "Lamden": {
+    geckoId: "lamden",
+    symbol: "TAU",
+    cmcId: "2337",
+  },
+  "Near": {
+    geckoId: "near",
+    symbol: "NEAR",
+    cmcId: "6535",
+  },
+  "EOS": {
+    geckoId: "eos",
+    symbol: "EOS",
+    cmcId: "1765",
+  },
+  "Songbird": {
+      geckoId: "songbird",
+      symbol: "SGB",
+      cmcId: "12186",
+  },
+  "EnergyWeb": {
+      geckoId: "energy-web-token",
+      symbol: "EWT",
+      cmcId: "5268",
+  },
+  "HPB": {
+      geckoId: "high-performance-blockchain",
+      symbol: "HPB",
+      cmcId: "2345",
+  },
+  "GoChain": {
+      geckoId: "gochain",
+      symbol: "GO",
+      cmcId: "2861",
+  },
+  "TomoChain": {
+      geckoId: "tomochain",
+      symbol: "TOMO",
+      cmcId: "2570",
+  },
+  "Fusion": {
+    geckoId: "fsn",
+    symbol: "FSN",
+    cmcId: "2530",
+  },
 }
