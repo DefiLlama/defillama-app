e13a7e4d35210a3d0d03dbae560b572792dd0b09
shinitakunai <87251045+shinitakunai@users.noreply.github.com>
2021-10-30 00:50:39 -0700

    Error boundary and error page

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import React, { useState, lazy, Suspense } from 'react'
 import { Route, Switch, BrowserRouter, Redirect } from 'react-router-dom'
+import { ErrorBoundary } from 'react-error-boundary'
 
 import { useGlobalData, useGlobalChartData } from './contexts/GlobalData'
 import { useAllTokenData } from './contexts/TokenData'
 import { isValidProtocol } from './utils'
+import ErrorPage from './pages/ErrorPage'
 
 import LocalLoader from './components/LocalLoader'
 import { AppWrapper, LayoutWrapper } from './layout'
@@ -29,123 +31,126 @@ function App() {
   return (
     <AppWrapper>
       <BrowserRouter>
-        {globalData &&
-          globalData.totalLiquidityUSD &&
-          globalChartData &&
-          globalChartData[1] &&
-          allTokens &&
-          allTokens[1] ? (
-          <Suspense fallback={<LocalLoader fill="true" />}>
-            <Switch>
-              <Route
-                exacts
-                strict
-                path="/protocol/:protocol/:chain?/:denomination?"
-                render={({ match }) => {
-                  if (isValidProtocol(allTokens, match.params.protocol.toLowerCase())) {
-                    return (
-                      <LayoutWrapper savedOpen={savedOpen} setSavedOpen={setSavedOpen}>
-                        <TokenPage
-                          protocol={match.params.protocol.toLowerCase()}
-                          denomination={match.params.denomination}
-                          selectedChain={match.params.chain}
-                        />
-                      </LayoutWrapper>
-                    )
-                  } else {
-                    return <Redirect to="/" />
-                  }
-                }}
-              />
-              <Route
-                exacts
-                strict
-                path="/protocols/:category"
-                render={({ match }) => {
-                  const category = match.params.category
-                  if (Object.values(allTokens).some(protocol => (protocol.category || '').toLowerCase() === category.toLowerCase())) {
-                    return (<LayoutWrapper savedOpen={savedOpen} setSavedOpen={setSavedOpen}>
-                      <AllTokensPage category={category} categoryName={category} />
-                    </LayoutWrapper>)
-                  } else {
-                    return <Redirect to="/protocols" />
-                  }
-                }}
-              />
-              <Route
-                exacts
-                strict
-                path="/chain/:chain/:useless?/:denomination?"
-                render={({ match }) => {
-                  return <LayoutWrapper savedOpen={savedOpen} setSavedOpen={setSavedOpen}>
-                    <GlobalPage chain={match.params.chain} denomination={match.params.denomination} />
-                  </LayoutWrapper>
-                }}
-              />
+        <ErrorBoundary FallbackComponent={ErrorPage}>
 
-              <Route path="/jpegged">
-                <LayoutWrapper savedOpen={savedOpen} setSavedOpen={setSavedOpen}>
-                  <Jpegged />
-                </LayoutWrapper>
-              </Route>
+          {globalData &&
+            globalData.totalLiquidityUSD &&
+            globalChartData &&
+            globalChartData[1] &&
+            allTokens &&
+            allTokens[1] ? (
+            <Suspense fallback={<LocalLoader fill="true" />}>
+              <Switch>
+                <Route
+                  exacts
+                  strict
+                  path="/protocol/:protocol/:chain?/:denomination?"
+                  render={({ match }) => {
+                    if (isValidProtocol(allTokens, match.params.protocol.toLowerCase())) {
+                      return (
+                        <LayoutWrapper savedOpen={savedOpen} setSavedOpen={setSavedOpen}>
+                          <TokenPage
+                            protocol={match.params.protocol.toLowerCase()}
+                            denomination={match.params.denomination}
+                            selectedChain={match.params.chain}
+                          />
+                        </LayoutWrapper>
+                      )
+                    } else {
+                      return <Redirect to="/" />
+                    }
+                  }}
+                />
+                <Route
+                  exacts
+                  strict
+                  path="/protocols/:category"
+                  render={({ match }) => {
+                    const category = match.params.category
+                    if (Object.values(allTokens).some(protocol => (protocol.category || '').toLowerCase() === category.toLowerCase())) {
+                      return (<LayoutWrapper savedOpen={savedOpen} setSavedOpen={setSavedOpen}>
+                        <AllTokensPage category={category} categoryName={category} />
+                      </LayoutWrapper>)
+                    } else {
+                      return <Redirect to="/protocols" />
+                    }
+                  }}
+                />
+                <Route
+                  exacts
+                  strict
+                  path="/chain/:chain/:useless?/:denomination?"
+                  render={({ match }) => {
+                    return <LayoutWrapper savedOpen={savedOpen} setSavedOpen={setSavedOpen}>
+                      <GlobalPage chain={match.params.chain} denomination={match.params.denomination} />
+                    </LayoutWrapper>
+                  }}
+                />
 
-              <Route path="/settings">
-                <LayoutWrapper savedOpen={savedOpen} setSavedOpen={setSavedOpen}>
-                  <Settings />
-                </LayoutWrapper>
-              </Route>
-
-              <Route path="/protocols">
-                <LayoutWrapper savedOpen={savedOpen} setSavedOpen={setSavedOpen}>
-                  <AllTokensPage />
-                </LayoutWrapper>
-              </Route>
-              <Route path="/airdrops">
-                <LayoutWrapper savedOpen={savedOpen} setSavedOpen={setSavedOpen}>
-                  <NoTokenProtocols />
-                </LayoutWrapper>
-              </Route>
-              <Route path="/dexes">
-                <LayoutWrapper savedOpen={savedOpen} setSavedOpen={setSavedOpen}>
-                  <AllTokensPage category="Dexes" />
-                </LayoutWrapper>
-              </Route>
-              <Route path="/about">
-                <LayoutWrapper savedOpen={savedOpen} setSavedOpen={setSavedOpen}>
-                  <AboutPage />
-                </LayoutWrapper>
-              </Route>
-              <Route path="/recent">
-                <LayoutWrapper savedOpen={savedOpen} setSavedOpen={setSavedOpen}>
-                  <RecentListingsPage />
-                </LayoutWrapper>
-              </Route>
-              <Route path="/chains">
-                <LayoutWrapper savedOpen={savedOpen} setSavedOpen={setSavedOpen}>
-                  <ChainsViewPage />
-                </LayoutWrapper>
-              </Route>
-              <Route path="/comparison/:protocolA?/:protocolB?"
-                render={({ match }) =>
+                <Route path="/jpegged">
                   <LayoutWrapper savedOpen={savedOpen} setSavedOpen={setSavedOpen}>
-                    <ComparisonPage
-                      protocolA={match?.params?.protocolA?.toLowerCase()}
-                      protocolB={match?.params?.protocolB?.toLowerCase()}
-                    />
-                  </LayoutWrapper>}
-              />
-              <Route exact path="/">
-                <LayoutWrapper savedOpen={savedOpen} setSavedOpen={setSavedOpen}>
-                  <GlobalPage />
-                </LayoutWrapper>
-              </Route>
+                    <Jpegged />
+                  </LayoutWrapper>
+                </Route>
 
-              <Redirect to="/" />
-            </Switch>
-          </Suspense>
-        ) : (
-          <LocalLoader fill="true" />
-        )}
+                <Route path="/settings">
+                  <LayoutWrapper savedOpen={savedOpen} setSavedOpen={setSavedOpen}>
+                    <Settings />
+                  </LayoutWrapper>
+                </Route>
+
+                <Route path="/protocols">
+                  <LayoutWrapper savedOpen={savedOpen} setSavedOpen={setSavedOpen}>
+                    <AllTokensPage />
+                  </LayoutWrapper>
+                </Route>
+                <Route path="/airdrops">
+                  <LayoutWrapper savedOpen={savedOpen} setSavedOpen={setSavedOpen}>
+                    <NoTokenProtocols />
+                  </LayoutWrapper>
+                </Route>
+                <Route path="/dexes">
+                  <LayoutWrapper savedOpen={savedOpen} setSavedOpen={setSavedOpen}>
+                    <AllTokensPage category="Dexes" />
+                  </LayoutWrapper>
+                </Route>
+                <Route path="/about">
+                  <LayoutWrapper savedOpen={savedOpen} setSavedOpen={setSavedOpen}>
+                    <AboutPage />
+                  </LayoutWrapper>
+                </Route>
+                <Route path="/recent">
+                  <LayoutWrapper savedOpen={savedOpen} setSavedOpen={setSavedOpen}>
+                    <RecentListingsPage />
+                  </LayoutWrapper>
+                </Route>
+                <Route path="/chains">
+                  <LayoutWrapper savedOpen={savedOpen} setSavedOpen={setSavedOpen}>
+                    <ChainsViewPage />
+                  </LayoutWrapper>
+                </Route>
+                <Route path="/comparison/:protocolA?/:protocolB?"
+                  render={({ match }) =>
+                    <LayoutWrapper savedOpen={savedOpen} setSavedOpen={setSavedOpen}>
+                      <ComparisonPage
+                        protocolA={match?.params?.protocolA?.toLowerCase()}
+                        protocolB={match?.params?.protocolB?.toLowerCase()}
+                      />
+                    </LayoutWrapper>}
+                />
+                <Route exact path="/">
+                  <LayoutWrapper savedOpen={savedOpen} setSavedOpen={setSavedOpen}>
+                    <GlobalPage />
+                  </LayoutWrapper>
+                </Route>
+
+                <Redirect to="/" />
+              </Switch>
+            </Suspense>
+          ) : (
+            <LocalLoader fill="true" />
+          )}
+        </ErrorBoundary>
       </BrowserRouter>
     </AppWrapper>
   )
---
+++ src/pages/ErrorPage.js
@@ -0,0 +1,45 @@
+import { useEffect } from "react";
+import { Image } from 'rebass'
+
+import { LayoutWrapper } from 'layout'
+import Llama from 'assets/old_logo_white.png'
+import Row from 'components/Row'
+import Column from 'components/Column'
+import Link from "components/Link"
+import { TYPE, ThemedBackground } from 'Theme'
+
+import { setWithExpiry, getWithExpiry } from 'utils/localStorage'
+
+
+const ErrorFallback = ({ error, theme }) => {
+    // Handle failed lazy loading of a JS/CSS chunk.
+    useEffect(() => {
+        const chunkFailedMessage = /Loading chunk [\d]+ failed/;
+        if (error?.message && chunkFailedMessage.test(error.message)) {
+            if (!getWithExpiry("chunk_failed")) {
+                setWithExpiry("chunk_failed", "true", 10000);
+                window.location.reload();
+            }
+        }
+    }, [error]);
+
+    console.log(theme)
+
+    return (
+        <LayoutWrapper>
+            <ThemedBackground style={{ height: '100vh' }} />
+            <Row>
+                <Column style={{ margin: 'auto', textAlign: 'center', gap: '0.5rem', height: '100vh', padding: '1rem' }}>
+                    <Image src={Llama} alt="sad llama" />
+                    <TYPE.main>Something went wrong and we have been notified.</TYPE.main>
+                    <TYPE.main>Please try again and if the error persists please reach out on <Link href="https://discord.gg/buPFYXzDDd" target="_blank">Discord</Link></TYPE.main>
+                    <TYPE.largeHeader>Error: {error?.message}</TYPE.largeHeader>
+                </Column>
+
+            </Row>
+
+        </LayoutWrapper>
+    );
+}
+
+export default ErrorFallback
