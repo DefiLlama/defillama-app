c6ecd910bb8b14721ca9f6608ad4b2d0e1352b3c
shinitakunai <87251045+shinitakunai@users.noreply.github.com>
2021-11-14 20:12:10 -0800

    Clean up and optimize tokendata context

--- src/contexts/TokenData.js
+++ src/contexts/TokenData.js
@@ -1,26 +1,12 @@
-import React, { createContext, useContext, useReducer, useCallback, useEffect, useState } from 'react'
-import dayjs from 'dayjs'
-import utc from 'dayjs/plugin/utc'
+import React, { createContext, useContext, useReducer, useCallback, useEffect } from 'react'
 
-import { useEthPrice } from './GlobalData'
 import { priorityChainFilters } from 'constants/chainTokens'
+import { standardizeTokenName } from 'utils'
 
 import { fetchAPI } from './API'
-import {
-  PROTOCOLS_API,
-  PROTOCOL_API,
-  UPDATE,
-  UPDATE_TOKEN_TXNS,
-  UPDATE_CHART_DATA,
-  UPDATE_PRICE_DATA,
-  UPDATE_TOP_TOKENS,
-  UPDATE_ALL_PAIRS,
-  TOKEN_PAIRS_KEY
-} from '../constants'
+import { PROTOCOLS_API, PROTOCOL_API, UPDATE_TOP_TOKENS, UPDATE } from '../constants'
 
 import { useStakingManager, usePool2Manager } from './LocalStorage'
-
-dayjs.extend(utc)
 
 const TokenDataContext = createContext()
 
@@ -31,72 +17,30 @@ function useTokenDataContext() {
 function reducer(state, { type, payload }) {
   switch (type) {
     case UPDATE: {
-      const { tokenAddress, data } = payload
+      const { tokenName, data } = payload
       return {
         ...state,
-        [tokenAddress]: {
-          ...state?.[tokenAddress],
+        [tokenName]: {
+          ...state?.[tokenName],
           ...data
         }
       }
     }
     case UPDATE_TOP_TOKENS: {
       const { topTokens } = payload
-      let added = {}
-      if (topTokens) {
-        added = topTokens
-      }
+      const tokenDict = topTokens?.protocols.reduce((acc, curr) => {
+        acc[standardizeTokenName(curr.name)] = curr
+        return acc
+      }, {})
       return {
         ...state,
-        ...added
+        tokenDict,
+        tokenArr: topTokens.protocols,
+        chains: topTokens.chains,
+        categories: topTokens.protocolCategories
       }
     }
 
-    case UPDATE_TOKEN_TXNS: {
-      const { address, transactions } = payload
-      return {
-        ...state,
-        [address]: {
-          ...state?.[address],
-          txns: transactions
-        }
-      }
-    }
-    case UPDATE_CHART_DATA: {
-      const { address, chartData } = payload
-      return {
-        ...state,
-        [address]: {
-          ...state?.[address],
-          chartData
-        }
-      }
-    }
-
-    case UPDATE_PRICE_DATA: {
-      const { address, data, timeWindow, interval } = payload
-      return {
-        ...state,
-        [address]: {
-          ...state?.[address],
-          [timeWindow]: {
-            ...state?.[address]?.[timeWindow],
-            [interval]: data
-          }
-        }
-      }
-    }
-
-    case UPDATE_ALL_PAIRS: {
-      const { address, allPairs } = payload
-      return {
-        ...state,
-        [address]: {
-          ...state?.[address],
-          [TOKEN_PAIRS_KEY]: allPairs
-        }
-      }
-    }
     default: {
       throw Error(`Unexpected action type in DataContext reducer: '${type}'.`)
     }
@@ -105,11 +49,12 @@ function reducer(state, { type, payload }) {
 
 export default function Provider({ children }) {
   const [state, dispatch] = useReducer(reducer, {})
-  const update = useCallback((tokenAddress, data) => {
+
+  const updateToken = useCallback((tokenName, data) => {
     dispatch({
       type: UPDATE,
       payload: {
-        tokenAddress,
+        tokenName,
         data
       }
     })
@@ -124,45 +69,13 @@ export default function Provider({ children }) {
     })
   }, [])
 
-  const updateTokenTxns = useCallback((address, transactions) => {
-    dispatch({
-      type: UPDATE_TOKEN_TXNS,
-      payload: { address, transactions }
-    })
-  }, [])
-
-  const updateChartData = useCallback((address, chartData) => {
-    dispatch({
-      type: UPDATE_CHART_DATA,
-      payload: { address, chartData }
-    })
-  }, [])
-
-  const updateAllPairs = useCallback((address, allPairs) => {
-    dispatch({
-      type: UPDATE_ALL_PAIRS,
-      payload: { address, allPairs }
-    })
-  }, [])
-
-  const updatePriceData = useCallback((address, data, timeWindow, interval) => {
-    dispatch({
-      type: UPDATE_PRICE_DATA,
-      payload: { address, data, timeWindow, interval }
-    })
-  }, [])
-
   return (
     <TokenDataContext.Provider
       value={[
         state,
         {
-          update,
-          updateTokenTxns,
-          updateChartData,
-          updateTopTokens,
-          updateAllPairs,
-          updatePriceData
+          updateToken,
+          updateTopTokens
         }
       ]}
     >
@@ -172,75 +85,47 @@ export default function Provider({ children }) {
 }
 
 const getTopTokens = async () => {
-  //const utcCurrentTime = dayjs()
-  // const utcOneDayBack = utcCurrentTime.subtract(1, 'day').unix()
-  // const utcTwoDaysBack = utcCurrentTime.subtract(2, 'day').unix()
-  // let oneDayBlock = await getBlockFromTimestamp(utcOneDayBack)
-  // let twoDayBlock = await getBlockFromTimestamp(utcTwoDaysBack)
-
   try {
     let tokens = await fetchAPI(PROTOCOLS_API)
-    let bulkResults = tokens.map(token => {
-      return { ...token, priceUSD: token?.derivedETH * 1 || 0 }
-      //data.TVL = data.TVL;
-      // data.oneDayVolumeUSD = parseFloat(oneDayVolumeUSD)
-      // data.volumeChangeUSD = volumeChangeUSD
-      // data.priceChangeUSD = priceChangeUSD
-      // data.liquidityChangeUSD = getPercentChange(currentLiquidityUSD ?? 0, oldLiquidityUSD ?? 0)
-      // data.oneDayTxns = oneDayTxns
-      // data.txnChange = txnChange
-    })
-    return bulkResults
-
-    // calculate percentage changes and daily changes
+    return tokens
   } catch (e) {
     console.log(e)
   }
 }
 
-const getTokenByProtocol = async protocol => {
+const getTokenByProtocol = async tokenName => {
   try {
-    const tokenData = await fetchAPI(`${PROTOCOL_API}/${protocol}`)
+    const tokenData = await fetchAPI(`${PROTOCOL_API}/${tokenName}`)
     return tokenData
   } catch (e) {
     console.log(e)
   }
 }
 
-const getTokenData = async (address, protocol, ethPrice, ethPriceOld) => {
+const getTokenData = async tokenName => {
   try {
-    if (protocol) {
-      const tokenData = await getTokenByProtocol(protocol?.split(' ').join('-'))
-      const data = {
-        id: tokenData?.id,
-        name: tokenData?.name,
-        address: tokenData?.address,
-        url: tokenData?.url,
+    if (tokenName) {
+      const tokenData = await getTokenByProtocol(tokenName)
+      const historicalChainTvls = [...(tokenData?.chainTvls || [])]
+      // Don't overwrite topTokens' chainTvls response
+      delete tokenData.chainTvls
+      return {
+        ...tokenData,
         tvl: tokenData?.tvl.length > 0 ? tokenData?.tvl[tokenData?.tvl.length - 1]?.totalLiquidityUSD : 0,
         tvlList: tokenData?.tvl.filter(item => item.date),
-        tokensInUsd: tokenData?.tokensInUsd,
-        tokens: tokenData?.tokens,
-        description: tokenData?.description,
-        twitter: tokenData?.twitter,
-        historicalChainTvls: tokenData?.chainTvls,
-        methodology: tokenData?.methodology,
-        misrepresentedTokens: tokenData?.misrepresentedTokens,
-        hallmarks: tokenData?.hallmarks,
-        logo: tokenData?.logo,
-        audits: tokenData?.audits,
-        audit_links: tokenData?.["audit_links"],
+        historicalChainTvls,
+        detailed: true
       }
-      return data
     }
   } catch (e) {
-    console.log(protocol, e)
+    console.log(tokenName, e)
   }
   return {}
 }
 
 export function Updater() {
   const [, { updateTopTokens }] = useTokenDataContext()
-  //const [ethPrice, ethPriceOld] = useEthPrice()
+
   useEffect(() => {
     async function getData() {
       // get top pairs for overview list
@@ -252,71 +137,36 @@ export function Updater() {
   return null
 }
 
-export function useTokenData(tokenId, protocol = '') {
-  const [state, { update }] = useTokenDataContext()
-  const [ethPrice, ethPriceOld] = useEthPrice()
-  const [oldProtocol, setOldProtocol] = useState()
-  const tokenData = state?.[tokenId]
-
+export function useTokenData(tokenName) {
+  const [{ tokenDict }, { updateToken }] = useTokenDataContext()
+  const hasTokenData = !tokenDict?.[tokenName]?.detailed
   useEffect(() => {
-    if (protocol && oldProtocol !== protocol) {
-      getTokenData(tokenId, protocol, ethPrice, ethPriceOld).then(data => {
-        update(tokenId, data)
+    if (!hasTokenData) {
+      getTokenData(tokenName).then(data => {
+        updateToken(tokenName, data)
       })
-      setOldProtocol(protocol)
     }
-  }, [tokenId, tokenData, update, protocol, oldProtocol])
+  }, [hasTokenData, updateToken, tokenName])
 
-  return tokenData || {}
+  return tokenDict[tokenName] || {}
 }
-
-export function useTokenTransactions(tokenAddress) { }
-
-export function useTokenPairs(tokenAddress) { }
-
-export function useTokenChartData(tokenAddress) { }
-
-/**
- * get candlestick data for a token - saves in context based on the window and the
- * interval size
- * @param {*} tokenAddress
- * @param {*} timeWindow // a preset time window from constant - how far back to look
- * @param {*} interval  // the chunk size in seconds - default is 1 hour of 3600s
- */
-export function useTokenPriceData(tokenAddress, timeWindow, interval = 3600) { }
 
 export function useAllTokenData() {
   const [state] = useTokenDataContext()
-  const [stakingEnabled] = useStakingManager()
-  const [pool2Enabled] = usePool2Manager()
-  if (stakingEnabled || pool2Enabled) {
-    // TODO Optimize
-    return Object.fromEntries(
-      Object.entries(state).map(entry => [
-        entry[0],
-        {
-          ...entry[1],
-          tvl: entry[1].tvl + (stakingEnabled ? entry[1].chainTvls.staking ?? 0 : 0) + (pool2Enabled ? entry[1].chainTvls.pool2 ?? 0 : 0)
-        }
-      ])
-    )
-  }
   return state
 }
 
 export const useFilteredTokenData = ({ selectedChain = 'All', category = '' }) => {
   const allChains = selectedChain === 'All'
-  const [allTokenData] = useTokenDataContext()
+  const [{ chains, tokenArr }] = useTokenDataContext()
 
   let totalStaking = 0
   const [stakingEnabled] = useStakingManager()
   let totalPool2 = 0
   const [pool2Enabled] = usePool2Manager()
 
-  // All chains accumulated from priority list and from looping through tokens' specific chains
-  const chainsSet = new Set(priorityChainFilters)
-
-  let filteredTokens = Object.values(allTokenData).reduce((accTokenList, currTokenData) => {
+  // TODO USEMEMO
+  let filteredTokens = tokenArr.reduce((accTokenList, currTokenData) => {
     // Skip all chain tokens and if there is a category, skip all tokens that are not in that category
     if (
       currTokenData.category === 'Chain' ||
@@ -324,11 +174,6 @@ export const useFilteredTokenData = ({ selectedChain = 'All', category = '' }) =
     ) {
       return accTokenList
     }
-
-    // Accumulating unique chains from tokens
-    currTokenData.chains.forEach(chain => {
-      chainsSet.add(chain)
-    })
 
     // Calculate the correct tvl
 
@@ -381,7 +226,7 @@ export const useFilteredTokenData = ({ selectedChain = 'All', category = '' }) =
 
   return {
     filteredTokens,
-    chainsSet,
+    chains,
     totalStaking,
     totalPool2
   }
