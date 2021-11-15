38f4aee8234874fe20881321110cf1e4d6ae4ec1
0xngmi <0xngmi@protonmail.com>
2021-11-15 17:26:22 +0000

    add protocols

---
+++ src/pages/protocols.js
@@ -0,0 +1,22 @@
+import ProtocolList from '../../components/ProtocolList'
+import { PROTOCOLS_API } from '../../constants/index'
+import { GeneralLayout } from '../../layout'
+
+export async function getStaticProps({ params }) {
+    const res = await fetch(PROTOCOLS_API).then(r => r.json())
+    const protocols = res.protocols
+    return {
+        props: {
+            protocols,
+            chainsSet: res.chains,
+        }
+    }
+}
+
+export default function Protocols({ chainsSet, protocols }) {
+    return (
+        <GeneralLayout title={`TVL Rankings - DefiLlama`}>
+            <ProtocolList chainsSet={chainsSet} filteredTokens={protocols} />
+        </GeneralLayout>
+    )
+}
