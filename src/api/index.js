a7d4e4f9eb0e5bc8d32c42795f4d072589c6262a
shinitakunai <87251045+shinitakunai@users.noreply.github.com>
2021-11-18 01:32:03 -0800

    refactor to protocol

--- src/api/index.js
+++ src/api/index.js
@@ -1,12 +1,12 @@
 import { PROTOCOLS_API, PROTOCOL_API } from 'constants/index'
-import { standardizeTokenName } from 'utils'
+import { standardizeProtocolName } from 'utils'
 
 export const getProtocols = () =>
   fetch(PROTOCOLS_API)
     .then(r => r.json())
     .then(({ protocols, chains, protocolCategories }) => ({
       protocolsDict: protocols.reduce((acc, curr) => {
-        acc[standardizeTokenName(curr.name)] = curr
+        acc[standardizeProtocolName(curr.name)] = curr
         return acc
       }, {}),
       protocols,
@@ -14,10 +14,9 @@ export const getProtocols = () =>
       categories: protocolCategories
     }))
 
-export const getProtocol = async tokenName => {
+export const getProtocol = tokenName => {
   try {
-    const tokenData = await fetch(`${PROTOCOL_API}/${tokenName}`).then(r => r.json())
-    return tokenData
+    return fetch(`${PROTOCOL_API}/${tokenName}`).then(r => r.json())
   } catch (e) {
     console.log(e)
   }
