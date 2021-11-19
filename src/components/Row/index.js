2d0f32be4828dbccfaea444cd8634f2efd8c85c1
shinitakunai <87251045+shinitakunai@users.noreply.github.com>
2021-11-18 22:37:58 -0800

    wip redo rows

--- src/components/Row/index.js
+++ src/components/Row/index.js
@@ -35,3 +35,37 @@ export const RowFixed = styled(Row)`
 `
 
 export default Row
+
+// const Row = ({ children, ...props }) => (
+//   <Box display="flex" width="100%" alignItems="center" p={0} {...props}>
+//     {children}
+//   </Box>
+// )
+
+// export const RowBetween = ({ children, ...props }) => (
+//   <Row justifyContent="space-between" {...props}>
+//     {children}
+//   </Row>
+// )
+
+// export const RowFlat = ({ children, ...props }) => (
+//   <Box display="flex" alignItems="flex-end" {...props}>
+//     {children}
+//   </Box>
+// )
+
+// export const AutoRow = styled(Row)`
+//   flex-wrap: ${({ wrap }) => wrap ?? 'nowrap'};
+//   margin: -${({ gap }) => gap};
+//   & > * {
+//     margin: ${({ gap }) => gap} !important;
+//   }
+// `
+
+// export const RowFixed = ({ children, ...props }) => (
+//   <Row width="fit-content" {...props}>
+//     {children}
+//   </Row>
+// )
+
+// export default Row
