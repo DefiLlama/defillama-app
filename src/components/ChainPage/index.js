60f7ddf2be35eeb83c3cf3da4d5bb08a44c572f2
0xngmi <0xngmi@protonmail.com>
2021-11-23 05:42:36 +0000

    remove dead space

--- src/components/ChainPage/index.js
+++ src/components/ChainPage/index.js
@@ -148,7 +148,7 @@ function GlobalPage({
     <PageWrapper>
       <ThemedBackground backgroundColor={transparentize(0.8, '#445ed0')} />
       <ContentWrapper>
-        <AutoColumn gap="24px" style={{ paddingBottom: below800 ? '0' : '24px' }}>
+        <AutoColumn gap="24px">
           <Search />
           {selectedChain === 'Fantom' && (
             <Panel background={true} style={{ textAlign: 'center' }}>
