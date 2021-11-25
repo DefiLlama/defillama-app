e7a640e423c7e400859387927eee212c78ed01ce
QuirkyQwerty <MY_NAME@example.com>
2021-11-26 01:05:27 +0800

    moved "twitter" and "download dataset" buttons

--- src/containers/ProtocolContainer/index.js
+++ src/containers/ProtocolContainer/index.js
@@ -326,18 +326,6 @@ function ProtocolContainer({ protocolData, protocol, denomination, selectedChain
                   </TYPE.main>
                 </Column>
                 <div></div>
-                {/* <RowFixed>
-                  <Link color={backgroundColor} external href={`https://twitter.com/${twitter}`}>
-                    <ButtonLight color={backgroundColor} style={{ marginRight: '1rem' }}>
-                      Twitter ↗
-                    </ButtonLight>
-                  </Link>
-                  <Link color={backgroundColor} external href={`http://api.llama.fi/dataset/${protocol}.csv`}>
-                    <ButtonLight color={backgroundColor} style={{ marginRight: '1rem' }}>
-                      Download dataset ↗
-                    </ButtonLight>
-                  </Link>
-                </RowFixed> */}
               </TokenDetailsLayout>
             </Panel>
 
