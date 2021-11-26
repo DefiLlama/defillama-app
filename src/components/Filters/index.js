6370e2430e2c1b39968a35114ab17821d9d44d1e
0xngmi <0xngmi@protonmail.com>
2021-11-26 11:58:13 +0000

    fix double arrows on dropdown

--- src/components/Filters/index.js
+++ src/components/Filters/index.js
@@ -1,30 +1,16 @@
-import { useMedia } from 'react-use'
-
-import DropdownSelect from '../DropdownSelect'
 import FiltersAndDropdown from '../Filters/FiltersAndDropdown'
 import Row from '../Row'
 
-import { sm } from 'constants/breakpoints'
-
 // filter option to, and label,
 const Filters = ({ filterOptions = [], activeLabel, areLinks = true, onFilterClick, justify }) => {
-  const belowSmallTablet = useMedia(`(max-width: ${sm}px)`)
-
   return (
     <Row justify={justify}>
-      {belowSmallTablet ? (
-        <DropdownSelect
-          options={filterOptions}
-          active={activeLabel}
-        />
-      ) : (
-        <FiltersAndDropdown
-          filterOptions={filterOptions}
-          areLinks={areLinks}
-          onFilterClick={onFilterClick}
-          activeLabel={activeLabel}
-        />
-      )}
+      <FiltersAndDropdown
+        filterOptions={filterOptions}
+        areLinks={areLinks}
+        onFilterClick={onFilterClick}
+        activeLabel={activeLabel}
+      />
     </Row>
   )
 }
