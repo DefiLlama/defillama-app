63d67ab3ddae0577119c14f149a089891ece1990
0xngmi <0xngmi@protonmail.com>
2021-11-26 11:44:58 +0000

    rug bsc redirect

--- next.config.js
+++ next.config.js
@@ -2,11 +2,6 @@ module.exports = {
     async redirects() {
         return [
             {
-                source: '/chain/Binance',
-                destination: '/chain/BSC',
-                permanent: true,
-            },
-            {
                 source: '/home',
                 destination: '/',
                 permanent: true,
