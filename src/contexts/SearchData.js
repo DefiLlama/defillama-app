1284d57312adeee44214a46fafd13d32b66d8e5f
shinitakunai <87251045+shinitakunai@users.noreply.github.com>
2021-12-06 18:43:41 -0800

    set defaults to empty

---
+++ src/contexts/SearchData.js
@@ -0,0 +1,21 @@
+import React, { createContext, useContext } from 'react'
+
+const SearchDataContext = createContext({
+  protocolNames: [],
+  chainsSet: []
+})
+
+export const useSearchData = () => {
+  const contextData = useContext(SearchDataContext)
+  if (contextData.protocolNames.length === 0) {
+    return {
+      protocolNames: [],
+      chainsSet: []
+    }
+  }
+  return contextData
+}
+
+export default function Provider({ children, protocolsAndChains }) {
+  return <SearchDataContext.Provider value={protocolsAndChains}>{children}</SearchDataContext.Provider>
+}
