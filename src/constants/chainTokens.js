36efccf9a6bd2aacc6b1266337307f5e9617a331
Real Shaman <85087525+realdealshaman@users.noreply.github.com>
2022-01-05 04:57:42 +0000

    add KCC
    add KCC

--- src/constants/chainTokens.js
+++ src/constants/chainTokens.js
@@ -195,6 +195,11 @@ export const chainCoingeckoIds = {
     symbol: 'KCS',
     cmcId: '2087'
   },
+  KCC: {
+    geckoId: 'kucoin-shares',
+    symbol: 'KCS',
+    cmcId: '2087'
+  },
   Karura: {
     geckoId: 'karura',
     symbol: 'KAR',
