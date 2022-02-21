e363091379e1102ec7ad5c4ec1ccff9fb916dfeb
0xngmi <0xngmi@protonmail.com>
2022-02-21 16:28:16 +0000

    add link to new page

--- src/components/SideNav/DefiSideNav.js
+++ src/components/SideNav/DefiSideNav.js
@@ -13,6 +13,7 @@ import {
   Shield,
   Share2,
   Award,
+  Map
 } from 'react-feather'
 
 import { Entry } from './shared'
@@ -27,14 +28,15 @@ const NavMenu = ({ isMobile }) => {
       {isMobile && <Entry url="nfts" name="NFTs" history={history} Icon={Award} style={{ marginTop: '20px' }} />}
       <Entry url="" name="Overview" history={history} Icon={TrendingUp} />
       <Entry url="chains" name="Chains" history={history} Icon={LinkLogo} />
-      <Entry url="oracles" name="Oracles" history={history} Icon={Shield} newTag />
-      <Entry url="forks" name="Forks" history={history} Icon={Share2} newTag />
+      <Entry url="oracles" name="Oracles" history={history} Icon={Shield} />
+      <Entry url="forks" name="Forks" history={history} Icon={Share2} />
       {!isMobile && <Entry url="portfolio" name="Portfolio" history={history} Icon={Bookmark} />}
       <Entry url="airdrops" name="Airdrops" history={history} Icon={CloudDrizzle} />
       <Entry url="categories" name="Categories" history={history} Icon={RefreshCcw} />
       <Entry url="recent" name="Recent" history={history} Icon={Clock} />
       <Entry url="comparison" name="Comparison" history={history} Icon={Minimize2} />
       <Entry url="languages" name="Languages" history={history} Icon={Code} />
+      <Entry url="top-protocols" name="Top protocols" history={history} Icon={Map} newTag />
       <Entry url="about" name="About" history={history} Icon={HelpCircle} />
     </AutoColumn>
   )
