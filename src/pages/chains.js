86f098fbe09320a3de887dc901371eb5a4b02441
mintdart <96025197+mintdart@users.noreply.github.com>
2022-02-23 07:50:03 -0800

    fix chains categories table

--- src/pages/chains.js
+++ src/pages/chains.js
@@ -1,22 +1,7 @@
-import React, { useMemo } from 'react'
-import { Box } from 'rebass/styled-components'
-import styled from 'styled-components'
-
-import { Header } from 'Theme'
+import React from 'react'
 import { GeneralLayout } from 'layout'
-import { PageWrapper, FullWrapper } from 'components'
-import { ButtonDark } from 'components/ButtonStyled'
-import { RowBetween } from 'components/Row'
-import Search from 'components/Search'
-import { ChainPieChart, ChainDominanceChart } from 'components/Charts'
-import { AllTvlOptions } from 'components/SettingsModal'
-import Filters from 'components/Filters'
-import Table, { columnsToShow } from 'components/Table'
-
-import { toNiceCsvDate, getRandomColor, download } from 'utils'
 import { getChainsPageData, revalidate } from 'utils/dataApi'
-
-import { useCalcGroupExtraTvlsByDay, useCalcStakePool2Tvl, useGroupChainsByParent } from 'hooks/data'
+import ChainsPageView from 'components/ChainsPageView'
 
 export async function getStaticProps() {
   const data = await getChainsPageData('All')
@@ -26,93 +11,10 @@ export async function getStaticProps() {
   }
 }
 
-const ChartsWrapper = styled(Box)`
-  display: flex;
-  flex-wrap: nowrap;
-  width: 100%;
-  padding: 0;
-  align-items: center;
-  z-index: 1;
-  @media (max-width: 800px) {
-    display: grid;
-    grid-auto-rows: auto;
-  }
-`
-
-const RowWrapper = styled(RowBetween)`
-  flex-wrap: wrap;
-  @media (max-width: 680px) {
-    gap: 16px;
-  }
-`
-
-const StyledTable = styled(Table)`
-  tr > :first-child {
-    padding-left: 40px;
-  }
-`
-
-const columns = columnsToShow('chainName', 'protocols', '1dChange', '7dChange', '1mChange', 'tvl', 'mcaptvl')
-
-export default function Chains({ chainsUnique, chainTvls, stackedDataset, category, categories, chainsGroupbyParent }) {
-  const chainColor = useMemo(
-    () => Object.fromEntries([...chainsUnique, 'Others'].map((chain) => [chain, getRandomColor()])),
-    [chainsUnique]
-  )
-
-  const chainTotals = useCalcStakePool2Tvl(chainTvls)
-
-  const chainsTvlValues = useMemo(() => {
-    const data = chainTotals.map((chain) => ({ name: chain.name, value: chain.tvl }))
-
-    const otherTvl = data.slice(10).reduce((total, entry) => {
-      return (total += entry.value)
-    }, 0)
-
-    return data
-      .slice(0, 10)
-      .sort((a, b) => b.value - a.value)
-      .concat({ name: 'Others', value: otherTvl })
-  }, [chainTotals])
-
-  const { data: stackedData, daySum } = useCalcGroupExtraTvlsByDay(stackedDataset)
-
-  const downloadCsv = () => {
-    const rows = [['Timestamp', 'Date', ...chainsUnique]]
-    stackedData
-      .sort((a, b) => a.date - b.date)
-      .forEach((day) => {
-        rows.push([day.date, toNiceCsvDate(day.date), ...chainsUnique.map((chain) => day[chain] ?? '')])
-      })
-    download('chains.csv', rows.map((r) => r.join(',')).join('\n'))
-  }
-
-  const groupedChains = useGroupChainsByParent(chainTotals, chainsGroupbyParent)
+export default function Chains(props) {
   return (
     <GeneralLayout title={`Chain TVL - DefiLlama`} defaultSEO>
-      <PageWrapper>
-        <FullWrapper>
-          <Search />
-          <AllTvlOptions style={{ display: 'flex', justifyContent: 'center' }} />
-          <RowWrapper>
-            <Header>Total Value Locked All Chains</Header>
-            <ButtonDark onClick={downloadCsv}>Download all data in .csv</ButtonDark>
-          </RowWrapper>
-          <ChartsWrapper>
-            <ChainPieChart data={chainsTvlValues} chainColor={chainColor} />
-            <ChainDominanceChart
-              stackOffset="expand"
-              formatPercent={true}
-              stackedDataset={stackedData}
-              chainsUnique={chainsUnique}
-              chainColor={chainColor}
-              daySum={daySum}
-            />
-          </ChartsWrapper>
-          <Filters filterOptions={categories} activeLabel={category} />
-          <StyledTable data={groupedChains} columns={columns} />
-        </FullWrapper>
-      </PageWrapper>
+      <ChainsPageView {...props} />
     </GeneralLayout>
   )
 }
