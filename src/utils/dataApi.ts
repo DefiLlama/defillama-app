580494a766a5dd93a501891c81d4030ddf32a351
mintdart <96025197+mintdart@users.noreply.github.com>
2022-03-03 00:35:33 -0800

    filter protocol tvls by chain in categories page

--- src/utils/dataApi.ts
+++ src/utils/dataApi.ts
@@ -152,18 +152,19 @@ const formatProtocolsData = ({
 export async function getProtocolsPageData(category, chain) {
   const { protocols, chains } = await getProtocols()
 
-  let filteredProtocols = formatProtocolsData({ category, protocols })
-
   const chainsSet = new Set()
 
-  filteredProtocols.forEach(({ chains }) => {
-    chains.forEach((chain) => chainsSet.add(chain))
+  protocols.forEach(({ chains, category: pCategory }) => {
+    chains.forEach((chain) => {
+      if (pCategory.toLowerCase() === category.toLowerCase()) {
+        if (!chain || chains.includes(chain)) {
+          chainsSet.add(chain)
+        }
+      }
+    })
   })
 
-  // filter protocols by chain after we have data of all the chains of protocols in a category
-  if (chain) {
-    filteredProtocols = filteredProtocols.filter(({ chains = [] }) => chains.includes(chain))
-  }
+  let filteredProtocols = formatProtocolsData({ category, protocols, chain })
 
   return {
     filteredProtocols,
