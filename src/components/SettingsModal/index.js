1a6f25eb1f85a8991d09d3c90c6115a970958d12
Cocoahomology <83151536+Cocoahomology@users.noreply.github.com>
2022-05-05 05:44:28 -0300

    tiny formatting changes

--- src/components/SettingsModal/index.js
+++ src/components/SettingsModal/index.js
@@ -452,7 +452,7 @@ export const AllPeggedOptions = ({ style }) => {
 export const AllGroupOptions = ({ style }) => {
   const tvlToggles = useTvlToggles()
   const extraTvlEnabled = useGroupEnabled()
-  
+
   return (
     <>
       <ScrollAreaRoot>
