925b2ddf5944b3fffbb80df3cf4d3adfeaa172f2
0xngmi <0xngmi@protonmail.com>
2022-05-19 07:55:28 +0100

    fix max 6MB limit

--- src/pages/protocol/[...protocol].js
+++ src/pages/protocol/[...protocol].js
@@ -16,6 +16,17 @@ export async function getStaticProps({
       notFound: true,
     }
   }
+
+  const dataLength = JSON.stringify(protocolRes).length
+  if(dataLength >= 5.9e6){
+    delete protocolRes.tokensInUsd;
+    delete protocolRes.tokens;
+    Object.keys(protocolRes.chainTvls).forEach(chain=>{
+      delete protocolRes.chainTvls[chain].tokensInUsd;
+      delete protocolRes.chainTvls[chain].tokens;
+    })
+  }
+
   const protocolData = fuseProtocolData(protocolRes, protocol)
 
   return {
