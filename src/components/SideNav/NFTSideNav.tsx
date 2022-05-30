bcef1f4497a5b1bd5f35cb383303514448b87718
mintdart <96025197+mintdart@users.noreply.github.com>
2022-05-30 22:05:57 +0100

    fix build error

--- src/components/SideNav/NFTSideNav.tsx
+++ src/components/SideNav/NFTSideNav.tsx
@@ -1,19 +1,15 @@
 import React from 'react'
 import { TrendingUp, HelpCircle, Link as LinkLogo, ShoppingCart, BarChart2 } from 'react-feather'
 import { Nav, Entry, MobileOnlyEntry } from './shared'
-import { useRouter } from 'next/router'
 
 const NavMenu = () => {
-  const router = useRouter()
-  const history = { location: { pathname: router.pathname } }
-
   return (
     <Nav>
-      <MobileOnlyEntry url="" name="DeFi" history={history} Icon={BarChart2} style={{ marginTop: '20px' }} />
-      <Entry url="nfts" name="Overview" history={history} Icon={TrendingUp} />
-      <Entry url="nfts/chains" name="Chains" history={history} Icon={LinkLogo} />
-      <Entry url="nfts/marketplaces" name="Marketplaces" history={history} Icon={ShoppingCart} />
-      <Entry url="nfts/about" name="About" history={history} Icon={HelpCircle} />
+      <MobileOnlyEntry url="" name="DeFi" Icon={BarChart2} style={{ marginTop: '20px' }} />
+      <Entry url="nfts" name="Overview" Icon={TrendingUp} />
+      <Entry url="nfts/chains" name="Chains" Icon={LinkLogo} />
+      <Entry url="nfts/marketplaces" name="Marketplaces" Icon={ShoppingCart} />
+      <Entry url="nfts/about" name="About" Icon={HelpCircle} />
     </Nav>
   )
 }
