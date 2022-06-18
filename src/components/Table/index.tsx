33b382ddf4337dc7b23b154a2ddd9fde17826d23
Cocoahomology <83151536+Cocoahomology@users.noreply.github.com>
2022-06-18 00:53:54 -0400

    remove symbol from "Bridged from" rows in pegged table

--- src/components/Table/index.tsx
+++ src/components/Table/index.tsx
@@ -551,7 +551,7 @@ export function Name({
     return (
       <Index {...props} style={{ left: leftSpace }}>
         <span>-</span>
-        <span id="table-p-name">{name}</span>
+        <span id="table-p-name">{value}</span>
       </Index>
     )
   }
