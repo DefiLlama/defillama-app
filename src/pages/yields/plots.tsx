e5df48eb7914da39c6b0900af03e2280af0470d8
mintdart <96025197+mintdart@users.noreply.github.com>
2022-06-28 11:26:58 -0700

    Update plots.tsx

--- src/pages/yields/plots.tsx
+++ src/pages/yields/plots.tsx
@@ -6,30 +6,30 @@ import { YieldsSearch } from '~/components/Search'
 import { getAggregatedData, revalidate } from '~/utils/dataApi'
 
 interface IChartProps {
-  chartData: any
+	chartData: any
 }
 
 const ScatterChart = dynamic(() => import('~/components/TokenChart/ScatterChart'), {
-  ssr: false,
+	ssr: false
 }) as React.FC<IChartProps>
 
 export async function getStaticProps() {
-  const data = await getAggregatedData()
+	// const data = await getAggregatedData()
 
-  return {
-    props: {
-      pools: data,
-    },
-    revalidate: revalidate(),
-  }
+	return {
+		props: {
+			pools: []
+		},
+		revalidate: revalidate()
+	}
 }
 
 export default function Protocols({ pools }) {
-  return (
-    <Layout title={`Plots - DefiLlama Yield`} defaultSEO>
-      <YieldsSearch step={{ category: 'Yields', name: 'All projects' }} />
-      <PageHeader title="Plots" />
-      <ScatterChart chartData={pools}></ScatterChart>
-    </Layout>
-  )
+	return (
+		<Layout title={`Plots - DefiLlama Yield`} defaultSEO>
+			<YieldsSearch step={{ category: 'Yields', name: 'All projects' }} />
+			<PageHeader title="Plots" />
+			<ScatterChart chartData={pools}></ScatterChart>
+		</Layout>
+	)
 }
