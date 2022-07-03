3ed1a728718e794e16c5e87ebce056591f9c6ce0
mintdart <96025197+mintdart@users.noreply.github.com>
2022-07-03 07:38:29 -0700

    update area styles

--- src/components/TokenChart/AreaChart.tsx
+++ src/components/TokenChart/AreaChart.tsx
@@ -125,7 +125,16 @@ export default function AreaChart({
 						color: index === 0 ? chartColor : null
 					},
 					areaStyle: {
-						color: hideLegend ? 'transparent' : null
+						color: new echarts.graphic.LinearGradient(0, 0, 0, 1, [
+							{
+								offset: 0,
+								color: index === 0 ? chartColor : 'transparent'
+							},
+							{
+								offset: 1,
+								color: isDark ? 'rgba(0, 0, 0, 0.2)' : 'rgba(255, 255, 255, 0.2)'
+							}
+						])
 					},
 					data: [],
 					...(hallmarks && {
