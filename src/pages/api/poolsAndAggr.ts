63d5288380095416c5151e099299c4fc2e7624b8
mintdart <96025197+mintdart@users.noreply.github.com>
2022-07-08 22:23:52 -0700

    add link prefetch tags

---
+++ src/pages/api/poolsAndAggr.ts
@@ -0,0 +1,15 @@
+import type { NextApiRequest, NextApiResponse } from 'next'
+import { YIELD_AGGREGATION_API, YIELD_POOLS_API } from '~/constants'
+import { arrayFetcher } from '~/utils/useSWR'
+
+export default async function handler(_req: NextApiRequest, res: NextApiResponse) {
+	try {
+		const data = await arrayFetcher([YIELD_POOLS_API, YIELD_AGGREGATION_API])
+
+		const response = { data: data ? { pools: data[0]?.data ?? [], aggregations: data[1]?.data ?? [] } : null }
+
+		res.status(200).json({ response })
+	} catch (err) {
+		res.status(500).json({ error: 'failed to load data' })
+	}
+}
--- src/pages/yields.tsx
+++ src/pages/yields.tsx
@@ -1,14 +1,24 @@
+import Head from 'next/head'
 import Layout from '~/layout'
 import YieldPage from '~/components/YieldsPage'
 import { useYieldPageData } from '~/api/categories/yield/client'
-import { useFormatYieldsData } from '~/api/categories/yield'
+import { useFormatYieldsData, getYieldPageData } from '~/api/categories/yield'
+
+// if we are on the browser trigger a prefetch as soon as possible
+if (typeof window !== 'undefined') getYieldPageData()
 
 export default function ApyHomePage() {
 	const { data, loading } = useYieldPageData()
 	const formattedData = useFormatYieldsData(data, loading)
 	return (
-		<Layout title={`Yield Rankings - DefiLlama`} defaultSEO>
-			<YieldPage loading={loading} {...formattedData} />
-		</Layout>
+		<>
+			<Head>
+				{/* This will tell the browser to preload the data for our page */}
+				<link rel="preload" href="/api/poolsAndAggr" as="fetch" crossOrigin="anonymous" />
+			</Head>
+			<Layout title={`Yield Rankings - DefiLlama`} defaultSEO>
+				<YieldPage loading={loading} {...formattedData} />
+			</Layout>
+		</>
 	)
 }
