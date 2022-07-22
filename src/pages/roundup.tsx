5725caf7d503b167b9e858fab4488701f7dbea59
strobie <0xstrobe@protonmail.com>
2022-07-22 11:46:23 +0000

    switch to sample api temporarily

--- src/pages/roundup.tsx
+++ src/pages/roundup.tsx
@@ -98,31 +98,37 @@ export default function Chains({ messages }) {
 }
 
 export async function getStaticProps() {
-	const headers = new Headers()
-	headers.append('Authorization', `Bot ${process.env.ROUND_UP_BOT_TOKEN}`)
+	// const headers = new Headers()
+	// headers.append('Authorization', `Bot ${process.env.ROUND_UP_BOT_TOKEN}`)
 
-	let data = []
+	// let data = []
 
-	const response = await fetch('https://discordapp.com/api/channels/965023197365960734/messages', {
-		method: 'GET',
-		headers: headers,
-		redirect: 'follow'
-	})
+	// const response = await fetch('https://discordapp.com/api/channels/965023197365960734/messages', {
+	// 	method: 'GET',
+	// 	headers: headers,
+	// 	redirect: 'follow'
+	// })
 
-	if (response.ok) {
-		data = await response.json()
-	}
+	// if (response.ok) {
+	// 	data = await response.json()
+	// }
 
-	const index = data.findIndex((d) => d.content.startsWith('Daily news round-up with the')) ?? null
+	// const index = data.findIndex((d) => d.content.startsWith('Daily news round-up with the')) ?? null
 
-	const raw = Number.isNaN(index) ? [] : data.slice(0, index + 1)
+	// const raw = Number.isNaN(index) ? [] : data.slice(0, index + 1)
 
-	const messages = raw
-		.reverse()
-		.map((m) => m.content)
-		.join('')
+	// const messages = raw
+	// 	.reverse()
+	// 	.map((m) => m.content)
+	// 	.join('')
+
+	// const splitLlama = messages.split('Daily news round-up with the 🦙')
+
+	const response = await fetch('https://defillama.com/api/roundupMarkdown')
+	const messages = await response.json()
 
 	const splitLlama = messages.split('Daily news round-up with the 🦙')
+	console.log(splitLlama)
 
 	return {
 		props: {
