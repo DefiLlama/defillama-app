ec3524bd726cd90f85eab3a2e95388befa720343
strobie <0xstrobe@protonmail.com>
2022-07-26 14:08:44 +0000

    add init liquidations page

---
+++ src/pages/liquidations/index.tsx
@@ -0,0 +1,10 @@
+import Layout from '~/layout'
+import { revalidate } from '~/api'
+
+export default function Liquidations() {
+	return (
+		<Layout title={`Liquidation Levels - DefiLlama`} defaultSEO>
+			<div>henlo!!</div>
+		</Layout>
+	)
+}
