ae3a97811374d49bec481074e4957ad22714ec8c
mintdart <96025197+mintdart@users.noreply.github.com>
2022-07-27 02:04:04 -0700

    add filters to recent-noforks page

--- src/pages/recent-noforks.tsx
+++ src/pages/recent-noforks.tsx
@@ -6,32 +6,37 @@ import { FORK_API } from '~/constants'
 
 export async function getStaticProps() {
 	const protocolsRaw = await getSimpleProtocolsPageData([...basicPropertiesToKeep, 'extraTvl', 'listedAt'])
-  const forks = await fetch(FORK_API).then((r) => r.json())
-  const forked = {}
-  Object.values(forks.forks).map(forksList=>{
-    (forksList as string[]).map(f=>{
-      forked[f] = true
-    })
-  })
-  const protocols = protocolsRaw.protocols.filter((p) => forked[p.name] === undefined).map(p=>({
-		listedAt: 1609506000, // 1/1/2021
-		...p,
-	})).sort((a, b) => b.listedAt - a.listedAt)
+	const forks = await fetch(FORK_API).then((r) => r.json())
+	const forked = {}
+	Object.values(forks.forks).map((forksList) => {
+		;(forksList as string[]).map((f) => {
+			forked[f] = true
+		})
+	})
+
+	const protocols = protocolsRaw.protocols
+		.filter((p) => forked[p.name] === undefined)
+		.map((p) => ({
+			listedAt: 1609506000, // 1/1/2021
+			...p
+		}))
+		.sort((a, b) => b.listedAt - a.listedAt)
 	return {
 		props: {
-			protocols
+			protocols,
+			chainList: protocolsRaw.chains
 		},
 		revalidate: revalidate()
 	}
 }
 
-export default function Protocols({ protocols }) {
+export default function Protocols(props) {
 	return (
 		<RecentProtocols
-			protocols={protocols}
 			title="TVL Rankings - DefiLlama"
 			name="Recent"
 			header="Recently Listed Protocols - No forks"
+			{...props}
 		/>
 	)
 }
