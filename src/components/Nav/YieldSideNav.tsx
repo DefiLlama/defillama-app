68590c32e280626368ccb74cf22b7585f06e286b
slasher125 <slasher125@protonmail.com>
2022-08-05 15:00:51 -0700

    update nav menu

--- src/components/Nav/YieldSideNav.tsx
+++ src/components/Nav/YieldSideNav.tsx
@@ -9,8 +9,8 @@ const NavMenu = () => {
 
 			<Entry url="/yields/overview" name="Overview" Icon={PieChart} />
 			<Entry url="/yields" name="Pools" Icon={TrendingUp} />
+			<Entry url="/yields/stablecoins" name="Stablecoin Pools" Icon={Pocket} />
 			<Entry url="/yields/projects" name="Projects" Icon={List} />
-			<Entry url="/yields/stablecoins" name="Stablecoins" Icon={Pocket} />
 			<Entry url="/yields/watchlist" name="Watchlist" Icon={Bookmark} />
 			<Entry url="https://github.com/DefiLlama/yield-server#readme" name="List your protocol" Icon={PlusCircle} />
 
