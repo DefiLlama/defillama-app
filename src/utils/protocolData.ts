2dff57a356878396c8b32d06718524685c8aace1
mintdart <96025197+mintdart@users.noreply.github.com>
2022-08-05 13:36:46 -0700

    limit no.of tokens show on stacked charts in protocol page

--- src/utils/protocolData.ts
+++ src/utils/protocolData.ts
@@ -42,20 +42,62 @@ function buildChainBreakdown(chainTvls) {
 	return chainsStacked
 }
 
-function buildTokensBreakdown(tokensInUsd) {
-	const tokenSet = new Set()
+// build unique tokens based on top 10 tokens in usd value on each day
+function getUniqueTokens(tokensInUsd) {
+	const tokenSet: Set<string> = new Set()
+	let othersCategoryExist = false
+	tokensInUsd.forEach((dayTokens) => {
+		// filters tokens that have no name or their value is near zero and pick top 15 tokens from the list
+		const topTokens = Object.entries(dayTokens.tokens)
+			.filter((t) => !(t[0].startsWith('UNKNOWN') && t[1] < 1))
+			.sort((a: [string, number], b: [string, number]) => b[1] - a[1])
+
+		if (topTokens.length > 10) {
+			othersCategoryExist = true
+		}
+
+		topTokens.slice(0, 11).forEach(([symbol]) => tokenSet.add(symbol))
+	})
+
+	// if 'others' exist, add it to the end of unique token list
+	if (othersCategoryExist) {
+		tokenSet.add('Others')
+	}
+
+	return Array.from(tokenSet)
+}
+
+function buildTokensBreakdown(tokensInUsd, tokensUnique: Array<string>) {
 	const stacked = tokensInUsd.map((dayTokens) => {
-		Object.keys(dayTokens.tokens).forEach((symbol) => tokenSet.add(symbol))
+		// filters tokens that have no name or their value is near zero
+		const tokensOfTheDay = Object.entries(dayTokens.tokens).filter((t) => !(t[0].startsWith('UNKNOWN') && t[1] < 1))
+
+		const tokensToShow = []
+		let remainingTokensSum = 0
+
+		// split tokens of the day into tokens present in top 15 tokens list and add tvl of remaining tokens into category named 'Others'
+		tokensOfTheDay.forEach((token: [string, number]) => {
+			if (tokensUnique.includes(token[0])) {
+				tokensToShow.push(token)
+			} else {
+				remainingTokensSum += token[1]
+			}
+		})
+
+		// add "Others" to tokens list
+		if (tokensUnique.includes('Others') && remainingTokensSum > 0) {
+			tokensToShow.push(['Others', remainingTokensSum])
+		}
+
 		return {
-			...Object.fromEntries(Object.entries(dayTokens.tokens).filter((t) => !(t[0].startsWith('UNKNOWN') && t[1] < 1))),
+			...Object.fromEntries(tokensToShow),
 			date: dayTokens.date
 		}
 	})
-	return [stacked, Array.from(tokenSet)]
+	return stacked
 }
 
-function buildInflows(tokensInUsd, tokens) {
-	let tokenSet = new Set()
+function buildInflows({ tokensInUsd, tokens, tokensUnique }) {
 	const usdInflows = []
 	const tokenInflows = []
 
@@ -66,13 +108,18 @@ function buildInflows(tokensInUsd, tokens) {
 		let dayDifference = 0
 		let tokenDayDifference = {}
 		for (const token in tokensInUsd[i]?.tokens) {
-			tokenSet.add(token)
 			const price = tokensInUsd[i].tokens[token] / tokens[i]?.tokens[token]
 			const diff = (tokens[i]?.tokens[token] ?? 0) - (tokens[i - 1]?.tokens[token] ?? 0)
 			const diffUsd = price * diff
 
 			if (!Number.isNaN(diffUsd) && isFinite(price)) {
-				tokenDayDifference[token] = diffUsd
+				// Show only top 15 inflow tokens of the day, add remaining inlfows under "Others" category
+				if (tokensUnique.includes(token)) {
+					tokenDayDifference[token] = diffUsd
+				} else {
+					tokenDayDifference['Others'] = (tokenDayDifference['Others'] || 0) + diffUsd
+				}
+
 				dayDifference += diffUsd
 			}
 		}
@@ -86,6 +133,7 @@ function buildInflows(tokensInUsd, tokens) {
 		}
 
 		usdInflows.push([tokensInUsd[i].date, dayDifference])
+
 		tokenInflows.push({
 			...tokenDayDifference,
 			date: tokensInUsd[i].date
@@ -103,16 +151,22 @@ export const buildProtocolData = (protocolData) => {
 		const chainsStacked = buildChainBreakdown(protocolData.chainTvls)
 
 		if (!protocolData.misrepresentedTokens && protocolData.tokensInUsd && protocolData.tokens) {
-			const [tokenBreakdownUSD, tokensUnique] = buildTokensBreakdown(protocolData.tokensInUsd)
-			const [tokenBreakdown] = buildTokensBreakdown(protocolData.tokens)
-			const { usdInflows, tokenInflows } = buildInflows(protocolData.tokensInUsd, protocolData.tokens)
+			const tokensUnique = getUniqueTokens(protocolData.tokensInUsd)
+			const tokenBreakdownUSD = buildTokensBreakdown(protocolData.tokensInUsd, tokensUnique)
+			const tokenBreakdown = buildTokensBreakdown(protocolData.tokens, tokensUnique)
+			const { usdInflows, tokenInflows } = buildInflows({
+				tokensInUsd: protocolData.tokensInUsd,
+				tokens: protocolData.tokens,
+				tokensUnique
+			})
+
 			return {
-				tokenBreakdown,
-				tokenBreakdownUSD,
+				chainsStacked,
 				tokensUnique,
+				tokenBreakdownUSD,
+				tokenBreakdown,
 				usdInflows,
-				tokenInflows,
-				chainsStacked
+				tokenInflows
 			}
 		}
 
