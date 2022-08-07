f756fd131e1b9e8ac1d4678fde0a2c88471869e4
strobie <0xstrobe@protonmail.com>
2022-08-07 22:47:51 +0000

    add liquidations api route

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -37,6 +37,8 @@ export const YIELD_CHART_API = 'https://yields.llama.fi/chart'
 export const YIELD_CONFIG_API = 'https://api.llama.fi/config/yields?a=1'
 export const YIELD_MEDIAN_API = 'https://yields.llama.fi/median'
 
+export const LIQUIDATIONS_API = 'https://api.llama.fi/liquidations'
+
 export const DEXS_API = 'https://api.llama.fi/dexs'
 export const DEX_BASE_API = 'https://api.llama.fi/dex'
 
---
+++ src/pages/api/liquidations.ts
@@ -0,0 +1,22 @@
+// Next API route for handling liquidation returns
+import { NextApiRequest, NextApiResponse } from 'next'
+import { ChartData, getResponse } from '../../utils/liquidations'
+
+export default async function liquidationsHandler(
+	req: NextApiRequest,
+	res: NextApiResponse<ChartData | { error: string }>
+) {
+	const { symbol } = req.query as LiquidationsApiQuery
+	if (!symbol) {
+		res.status(400).json({ error: 'Missing symbol' })
+		return
+	}
+	// if symbol/aggregateBy is an array (by accident), get the first one
+	const _symbol: string = Array.isArray(symbol) ? symbol[0] : symbol
+	const chartData = await getResponse(_symbol)
+	res.status(200).json(chartData)
+}
+
+export type LiquidationsApiQuery = {
+	symbol: string
+}
