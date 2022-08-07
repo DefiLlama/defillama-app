0c193ab96476731176462621819aa55be9aa9881
strobie <0xstrobe@protonmail.com>
2022-08-07 22:26:53 +0000

    refactor liquidationsHandler

--- src/pages/api/liquidations.ts
+++ src/pages/api/liquidations.ts
@@ -1,16 +1,15 @@
 // Next API route for handling liquidation returns
 import { NextApiRequest, NextApiResponse } from 'next'
-import { getResponse } from '../../utils/liquidations'
+import { ChartData, getResponse } from '../../utils/liquidations'
 
-export default async function liquidationsHandler(req: NextApiRequest, res: NextApiResponse) {
-	const { symbol, aggregateBy, filters } = req.query
-	console.log('[/api/liquidations] received these filters but filters are currently handled on the frontend', filters)
+export default async function liquidationsHandler(req: NextApiRequest, res: NextApiResponse<ChartData>) {
+	const { symbol } = req.query as LiquidationsApiQuery
 	// if symbol/aggregateBy is an array (by accident), get the first one
-	const _symbol = Array.isArray(symbol) ? symbol[0] : symbol
-	const _aggregateBy = Array.isArray(aggregateBy) ? aggregateBy[0] : aggregateBy
-	const __aggregateBy = (['chain', 'protocol'].includes(_aggregateBy) ? _aggregateBy : 'protocol') as
-		| 'chain'
-		| 'protocol'
-	const chartData = await getResponse(_symbol, __aggregateBy)
+	const _symbol: string = Array.isArray(symbol) ? symbol[0] : symbol
+	const chartData = await getResponse(_symbol)
 	res.status(200).json(chartData)
+}
+
+export type LiquidationsApiQuery = {
+	symbol: string
 }
