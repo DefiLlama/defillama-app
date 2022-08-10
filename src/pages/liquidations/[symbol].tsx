6a130b38409dd305eee71461e59e6d0e213cb8a3
strobie <0xstrobe@protonmail.com>
2022-08-10 13:28:41 +0200

    add header

--- src/pages/liquidations/[symbol].tsx
+++ src/pages/liquidations/[symbol].tsx
@@ -1,10 +1,16 @@
 /* eslint-disable no-unused-vars*/
 // eslint sucks at types
 import { NextPage, GetStaticProps, GetStaticPaths } from 'next'
-import Layout from '~/layout'
 import { revalidate } from '~/api'
 import { ChartData, getLatestChartData } from '~/utils/liquidations'
+
+import Layout from '~/layout'
 import { LiquidationsSearch } from '~/components/Search'
+import { Header } from '~/Theme'
+import TokenLogo from '~/components/TokenLogo'
+import { ProtocolName, Symbol } from '~/components/ProtocolAndPool'
+import FormattedName from '~/components/FormattedName'
+import styled from 'styled-components'
 
 export const getStaticProps: GetStaticProps<ChartData> = async ({ params }) => {
 	const symbol = params.symbol as string
@@ -24,10 +30,20 @@ export const getStaticPaths: GetStaticPaths = async () => {
 	return { paths, fallback: 'blocking' }
 }
 
+const StyledProtocolName = styled(ProtocolName)`
+	margin-top: 1rem;
+`
+
 const LiquidationsHomePage: NextPage<ChartData> = (props) => {
 	return (
 		<Layout title={`${props.symbol} Liquidation Levels - DefiLlama`} defaultSEO>
 			<LiquidationsSearch step={{ category: 'Liquidation Levels', name: props.symbol, hideOptions: true }} />
+			<Header>Liquidation levels in DeFi 💦</Header>
+			<StyledProtocolName>
+				<TokenLogo logo={props.coingeckoAsset.thumb} size={24} />
+				<FormattedName text={props.coingeckoAsset.name} maxCharacters={16} fontWeight={700} />
+				<Symbol>({props.symbol})</Symbol>
+			</StyledProtocolName>
 		</Layout>
 	)
 }
