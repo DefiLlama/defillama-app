a5b4361849ed9628379f29f2f9ade6372945697f
slasher125 <slasher125@protonmail.com>
2022-08-10 15:52:05 -0700

    change to reward token symbols on tooltip, remove links

--- src/components/YieldsPage/shared.tsx
+++ src/components/YieldsPage/shared.tsx
@@ -302,9 +302,19 @@ export const columns = [
 			return (
 				<AutoRow sx={{ width: '100%', justifyContent: 'flex-end', gap: '4px' }}>
 					{rewards.includes('Optimism') || rewards.includes('Avalanche') ? (
-						<IconsRow links={rewards} url="/yields?chain" iconType="chain" />
+						<IconsRow
+							links={rewards}
+							url="/yields?chain"
+							iconType="chain"
+							yieldRewardsSymbols={rowValues.rewardTokensSymbols}
+						/>
 					) : (
-						<IconsRow links={rewards} url="/yields?project" iconType="token" />
+						<IconsRow
+							links={rewards}
+							url="/yields?project"
+							iconType="token"
+							yieldRewardsSymbols={rowValues.rewardTokensSymbols}
+						/>
 					)}
 					{formattedPercent(value, true)}
 				</AutoRow>
