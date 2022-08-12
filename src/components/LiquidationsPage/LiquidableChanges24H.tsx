ebf1f4b0ca9bed138d1a769c38d9e1a5b5306689
strobie <0xstrobe@protonmail.com>
2022-08-12 21:21:24 +0200

    add 24h change

--- src/components/LiquidationsPage/LiquidableChanges24H.tsx
+++ src/components/LiquidationsPage/LiquidableChanges24H.tsx
@@ -1,4 +1,4 @@
-import React, { useContext } from 'react'
+import React, { useContext, useMemo } from 'react'
 import { ChartData } from '~/utils/liquidations'
 import { useStackBy } from './utils'
 import { LiquidationsContext } from '~/pages/liquidations/[symbol]'
@@ -6,13 +6,50 @@ import { LiquidationsContext } from '~/pages/liquidations/[symbol]'
 export const LiquidableChanges24H = (props: { data: ChartData; prevData: ChartData }) => {
 	const stackBy = useStackBy()
 	const { selectedSeries } = useContext(LiquidationsContext)
+
+	const liquidableChanges = useMemo(
+		() => getLiquidableChangesRatio(props.data, props.prevData, stackBy, selectedSeries),
+		[props.data, props.prevData, stackBy, selectedSeries]
+	)
 	return (
 		<>
 			<h2>Change (24h)</h2>
 			<p style={{ '--tile-text-color': '#fd3c99' } as React.CSSProperties}>
-				{/* {(props.historicalChange[24] * 100).toFixed(1) || 0}% */}
-				TODO: WIP
+				{(liquidableChanges * 100).toFixed(1) || 0}%
 			</p>
 		</>
 	)
+}
+
+const getLiquidableChangesRatio = (
+	data: ChartData,
+	prevData: ChartData,
+	stackBy: 'chains' | 'protocols',
+	selectedSeries: {
+		[key: string]: boolean
+	}
+) => {
+	let current = 0
+	let prev = 0
+	if (!selectedSeries) {
+		current = data.totalLiquidable
+		prev = prevData.totalLiquidable
+	} else if (stackBy === 'chains') {
+		Object.keys(selectedSeries)
+			.filter((chain) => selectedSeries[chain])
+			.forEach((chain) => {
+				current += data.totalLiquidables.chains[chain]
+				prev += prevData.totalLiquidables.chains[chain]
+			})
+	} else {
+		Object.keys(selectedSeries)
+			.filter((protocol) => selectedSeries[protocol])
+			.forEach((protocol) => {
+				current += data.totalLiquidables.protocols[protocol]
+				prev += prevData.totalLiquidables.protocols[protocol]
+			})
+	}
+
+	const changesRatio = (current - prev) / prev
+	return isNaN(changesRatio) ? 0 : changesRatio
 }
