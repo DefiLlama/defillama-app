0970875fe2b27e046590a0f48c551e0ff5c1f11c
slasher125 <slasher125@protonmail.com>
2022-08-13 19:14:23 -0700

    add compression to yield pages

--- src/pages/yields/overview.tsx
+++ src/pages/yields/overview.tsx
@@ -2,22 +2,29 @@ import Layout from '~/layout'
 import PlotsPage from '~/components/YieldsPage/indexPlots'
 import { revalidate } from '~/api'
 import { getYieldPageData, getYieldMedianData } from '~/api/categories/yield'
+import pako from 'pako'
 
 export async function getStaticProps() {
 	const data = await getYieldPageData()
 	const median = await getYieldMedianData()
 	data['props']['median'] = median.props
+	const strData = JSON.stringify(data)
+
+	const a = pako.deflate(strData)
+	const compressed = Buffer.from(a).toString('base64')
 
 	return {
-		...data,
+		props: { compressed },
 		revalidate: revalidate(23)
 	}
 }
 
-export default function YieldPlots(props) {
+export default function YieldPlots(compressedProps) {
+	const b = new Uint8Array(Buffer.from(compressedProps.compressed, 'base64'))
+	const data = JSON.parse(pako.inflate(b, { to: 'string' }))
 	return (
 		<Layout title={`Overview - DefiLlama Yield`} defaultSEO>
-			<PlotsPage {...props} />
+			<PlotsPage {...data.props} />
 		</Layout>
 	)
 }
--- src/pages/yields/watchlist.tsx
+++ src/pages/yields/watchlist.tsx
@@ -2,20 +2,27 @@ import Layout from '~/layout'
 import { revalidate } from '~/api'
 import { getYieldPageData } from '~/api/categories/yield'
 import { YieldsWatchlistContainer } from '~/containers/Watchlist'
+import pako from 'pako'
 
 export async function getStaticProps() {
 	const data = await getYieldPageData()
+	const strData = JSON.stringify(data.props.pools)
+
+	const a = pako.deflate(strData)
+	const compressed = Buffer.from(a).toString('base64')
 
 	return {
-		...data,
+		props: { compressed },
 		revalidate: revalidate(23)
 	}
 }
 
-export default function Portfolio({ pools }) {
+export default function Portfolio(compressedProps) {
+	const b = new Uint8Array(Buffer.from(compressedProps.compressed, 'base64'))
+	const data = JSON.parse(pako.inflate(b, { to: 'string' }))
 	return (
 		<Layout title={`Saved Pools - DefiLlama`} defaultSEO>
-			<YieldsWatchlistContainer protocolsDict={pools} />
+			<YieldsWatchlistContainer protocolsDict={data} />
 		</Layout>
 	)
 }
