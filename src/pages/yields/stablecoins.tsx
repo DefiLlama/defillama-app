0421cb60d559f0eefda23343e97b3cb4759af727
slasher125 <74533329+slasher125@users.noreply.github.com>
2022-08-14 04:21:39 +0200

    add compression to yield pages (#629)

--- src/pages/yields/stablecoins.tsx
+++ src/pages/yields/stablecoins.tsx
@@ -2,20 +2,27 @@ import Layout from '~/layout'
 import YieldPage from '~/components/YieldsPage'
 import { revalidate } from '~/api'
 import { getYieldPageData } from '~/api/categories/yield'
+import pako from 'pako'
 
 export async function getStaticProps() {
 	const data = await getYieldPageData()
+	const strData = JSON.stringify(data)
+
+	const a = pako.deflate(strData)
+	const compressed = Buffer.from(a).toString('base64')
 
 	return {
-		...data,
+		props: { compressed },
 		revalidate: revalidate(23)
 	}
 }
 
-export default function YieldPlots(props) {
+export default function YieldPlots(compressedProps) {
+	const b = new Uint8Array(Buffer.from(compressedProps.compressed, 'base64'))
+	const data = JSON.parse(pako.inflate(b, { to: 'string' }))
 	return (
 		<Layout title={`Stablecoins - DefiLlama Yield`} defaultSEO>
-			<YieldPage {...props} />
+			<YieldPage {...data.props} />
 		</Layout>
 	)
 }
