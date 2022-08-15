09e1b68bab4565b568c76b33a6a1fb3f8ce8ea27
strobie <0xstrobe@protonmail.com>
2022-08-15 20:47:09 +0200

    use static asset list for now

--- src/components/Search/LiquidationsSearch/index.tsx
+++ src/components/Search/LiquidationsSearch/index.tsx
@@ -1,12 +1,12 @@
 import { BaseSearch } from '~/components/Search/BaseSearch'
 import type { ICommonSearchProps } from '~/components/Search/BaseSearch'
-import { useAssetsList } from '../../../utils/liquidations/useAssetsList'
+import { DEFAULT_ASSETS_LIST } from '~/utils/liquidations'
 
 interface ILiquidationsSearchProps extends ICommonSearchProps {}
 
 export default function LiquidationsSearch(props: ILiquidationsSearchProps) {
 	const loading = false
-	const assetsList = useAssetsList()
+	const assetsList = DEFAULT_ASSETS_LIST
 
 	return <BaseSearch {...props} data={assetsList} loading={loading} placeholder="Search liquidation levels..." />
 }
