9fb1ea36b2b31c2efd1e29a53684e8ac46005126
strobie <0xstrobe@protonmail.com>
2022-08-19 22:30:32 +0200

    make symbol on nav bar uppercase

--- src/pages/liquidations/[symbol].tsx
+++ src/pages/liquidations/[symbol].tsx
@@ -82,7 +82,9 @@ const LiquidationsHomePage: NextPage<{ data: ChartData; prevData: ChartData }> =
 				tvl={'$' + getReadableValue(data.totalLiquidable)}
 			/>
 
-			<LiquidationsSearch step={{ category: 'Home', name: `${data.symbol} Liquidation Levels`, hideOptions: true }} />
+			<LiquidationsSearch
+				step={{ category: 'Home', name: `${data.symbol.toUpperCase()} Liquidation Levels`, hideOptions: true }}
+			/>
 			<Header>Liquidation levels in DeFi 💦</Header>
 			<LiquidationsHeader {...data} />
 			<LiquidationsProvider>
