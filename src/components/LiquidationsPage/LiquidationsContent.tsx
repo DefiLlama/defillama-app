4e33ac493ec17a1e4075d5d7efa41fdf043d2c5b
strobie <0xstrobe@protonmail.com>
2022-08-26 17:35:20 +0200

    add toggle

--- src/components/LiquidationsPage/LiquidationsContent.tsx
+++ src/components/LiquidationsPage/LiquidationsContent.tsx
@@ -1,5 +1,5 @@
 /* eslint-disable no-unused-vars*/
-import React, { useContext, useMemo } from 'react'
+import React, { useContext, useMemo, useState } from 'react'
 import { ChartData, getReadableValue, PROTOCOL_NAMES_MAP_REVERSE } from '~/utils/liquidations'
 import { BreakpointPanel, BreakpointPanels, ChartAndValuesWrapper, PanelHiddenMobile } from '~/components'
 import { LiquidationsChart } from './LiquidationsChart'
@@ -8,6 +8,8 @@ import { LiquidableChanges24H } from './LiquidableChanges24H'
 import { LiquidationsContext } from '~/pages/liquidations/[symbol]'
 import { useStackBy } from './utils'
 import styled from 'styled-components'
+import ReactSwitch from 'react-switch'
+import { useLiqsManager } from '~/contexts/LocalStorage'
 
 export const LiquidationsContent = (props: { data: ChartData; prevData: ChartData }) => {
 	const { data, prevData } = props
@@ -25,9 +27,43 @@ export const LiquidationsContent = (props: { data: ChartData; prevData: ChartDat
 				</PanelHiddenMobile>
 			</BreakpointPanels>
 			<BreakpointPanel>
+				<CurrencyToggle symbol={data.symbol} />
 				<LiquidationsChart chartData={data} uid={data.symbol} />
 			</BreakpointPanel>
 		</Wrapper>
+	)
+}
+
+const CurrencyToggleWrapper = styled.div`
+	display: flex;
+	flex-direction: row;
+	justify-content: flex-end;
+	gap: 0.5rem;
+	align-items: center;
+	margin-bottom: 1rem;
+`
+
+const CurrencyToggle = (props: { symbol: string }) => {
+	const [isLiqsUsingUsd, toggleLiqsUsingUsd] = useLiqsManager()
+
+	return (
+		<CurrencyToggleWrapper>
+			{props.symbol.toUpperCase()}
+			{/* @ts-ignore:next-line */}
+			<ReactSwitch
+				onChange={() => {
+					toggleLiqsUsingUsd()
+				}}
+				checked={isLiqsUsingUsd}
+				onColor="#0A71F1"
+				offColor="#F18909"
+				height={20}
+				width={40}
+				uncheckedIcon={false}
+				checkedIcon={false}
+			/>
+			USD
+		</CurrencyToggleWrapper>
 	)
 }
 
