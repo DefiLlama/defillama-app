2b09daf02ca3ed55c5c3a79cbd49f9801852debe
wrappor <wrapporr@gmail.com>
2022-09-07 12:22:09 -0400

    updated to use stacked barchat

--- src/pages/fees/[protocol].tsx
+++ src/pages/fees/[protocol].tsx
@@ -5,12 +5,12 @@ import dynamic from 'next/dynamic'
 import { ProtocolDetails, ProtocolName, Stats, Tvl, TvlWrapper } from '~/containers/DexContainer'
 import TokenLogo from '~/components/TokenLogo'
 import FormattedName from '~/components/FormattedName'
-import { formattedNum, slug } from '~/utils'
-import { IBarChartProps } from '~/components/ECharts/types'
+import { formattedNum } from '~/utils'
+import { IStackedBarChartProps } from '~/components/ECharts/BarChart/Stacked'
 
-const BarChart = dynamic(() => import('~/components/ECharts/BarChart'), {
+const StackedChart = dynamic(() => import('~/components/ECharts/BarChart/Stacked'), {
 	ssr: false
-}) as React.FC<IBarChartProps>
+}) as React.FC<IStackedBarChartProps>
 
 const mapProtocolName = (protocolName: string) => {
 	if (protocolName === 'trader-joe') {
@@ -25,12 +25,18 @@ export const getStaticProps = async ({
 	}
 }) => {
 	const data = await fetch(`https://fees.llama.fi/fees/${mapProtocolName(protocol)}`).then(r=>r.json())
-  	const chart = data.feesHistory.map(t=>[t.timestamp, Object.values(t.dailyFees).reduce((sum:number, curr:number)=>curr[data.adapterKey]+sum, 0)])
+  	const feesData = data.feesHistory.map(t => [new Date(t.timestamp * 1000).toISOString(), Object.values(t.dailyFees).reduce((sum:number, curr:number) => curr[data.adapterKey] + sum, 0)])
+  	const revenueData = data.revenueHistory.map(t => [new Date(t.timestamp * 1000).toISOString(), Object.values(t.dailyRevenue).reduce((sum:number, curr:number) => curr[data.adapterKey] + sum, 0)])
+
+	let chartData = [
+		{ name: "Fees", data: feesData },
+		{ name: "Revenue", data: revenueData },
+	]
 
 	return {
 		props: {
 			data,
-      		chart
+			chartData
 		},
 		revalidate: revalidate()
 	}
@@ -40,33 +46,33 @@ export async function getStaticPaths() {
 	return { paths:[], fallback: 'blocking' }
 }
 
-export default function FeeProtocol({ data, chart }: InferGetStaticPropsType<typeof getStaticProps>) {
+export default function FeeProtocol({ data, chartData }: InferGetStaticPropsType<typeof getStaticProps>) {
 	return <Layout title={`${data.name} Fees - DefiLlama`} style={{ gap: '36px' }}>
-  <Stats>
-				<ProtocolDetails style={{ borderTopLeftRadius: '12px' }}>
-					<ProtocolName>
-						<TokenLogo logo={data.logo} size={24} />
-						<FormattedName text={data.name} maxCharacters={16} fontWeight={700} />
-					</ProtocolName>
+  		<Stats>
+			<ProtocolDetails style={{ borderTopLeftRadius: '12px' }}>
+				<ProtocolName>
+					<TokenLogo logo={data.logo} size={24} />
+					<FormattedName text={data.name} maxCharacters={16} fontWeight={700} />
+				</ProtocolName>
 
-					<TvlWrapper>
-						<Tvl>
-							<span>24h fees</span>
-							<span>{formattedNum(data.total1dFees || 0, true)}</span>
-						</Tvl>
-					</TvlWrapper>
-					<TvlWrapper>
-						<Tvl>
-							<span>24h protocol revenue</span>
-							<span>{formattedNum(data.total1dRevenue || 0, true)}</span>
-						</Tvl>
-					</TvlWrapper>
-				</ProtocolDetails>
+				<TvlWrapper>
+					<Tvl>
+						<span>24h fees</span>
+						<span>{formattedNum(data.total1dFees || 0, true)}</span>
+					</Tvl>
+				</TvlWrapper>
+				<TvlWrapper>
+					<Tvl>
+						<span>24h protocol revenue</span>
+						<span>{formattedNum(data.total1dRevenue || 0, true)}</span>
+					</Tvl>
+				</TvlWrapper>
+			</ProtocolDetails>
 
-				<BarChart
-					chartData={chart}
-          title=""
-				/>
-			</Stats>
-      </Layout>
+			<StackedChart
+				chartData={chartData}
+				title="Fees And Revenue"
+			/>
+		</Stats>
+	</Layout>
 }
