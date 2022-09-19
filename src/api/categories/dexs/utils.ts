00e912c644ba84570afcd2e7c27ee1a39d532eb0
0xtawa <0xtawanofi@gmail.com>
2022-09-19 15:40:51 +0200

    Properly format xdai chain name

--- src/api/categories/dexs/utils.ts
+++ src/api/categories/dexs/utils.ts
@@ -3,6 +3,7 @@ export const formatChain = (chain: string) => {
 	const ch = chain.toLowerCase()
 	let c = ch === 'avax' ? "avalanche" : ch
 	if (c === 'bsc') return c.toUpperCase()
+	if (c === 'xdai') return "xDai"
 	else
 		return c[0].toUpperCase() + c.slice(1)
 }
