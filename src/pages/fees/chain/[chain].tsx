4b10f32bcf9b8c905eedb82f7a91ffb509a053f4
0xngmi <0xngmi@protonmail.com>
2022-09-20 02:24:29 +0100

    restructure to move most code serverside

---
+++ src/pages/fees/chain/[chain].tsx
@@ -0,0 +1,18 @@
+import FeesContainer, { feesStaticProps, feesChainsSet } from "~/containers/FeesContainer"
+
+export async function getStaticProps({ params }) {
+	const chain = params.chain
+  return feesStaticProps(chain)
+}
+
+export async function getStaticPaths() {
+	const paths = feesChainsSet.map((chain) => ({
+		params: { chain }
+	}))
+
+	return { paths, fallback: 'blocking' }
+}
+
+export default function Fees(props) {
+  return FeesContainer(props)
+}
