4703f4532c3599ad7b0117eee332d387687bf9ad
mintdart <96025197+mintdart@users.noreply.github.com>
2022-09-25 09:36:19 -0400

    add active link styles

--- src/components/Nav/Desktop/SubMenu.tsx
+++ src/components/Nav/Desktop/SubMenu.tsx
@@ -2,43 +2,58 @@ import { forwardRef } from 'react'
 import Link from 'next/link'
 import styled from 'styled-components'
 import { linksWithNoSubMenu, navLinks } from '../Links'
+import { useRouter } from 'next/router'
+import { darken } from 'polished'
 
-const SubMenu = forwardRef<HTMLDetailsElement, { name: string }>(function card({ name }, ref) {
+const SubMenu = forwardRef<HTMLDetailsElement, { name: string }>(function Menu({ name }, ref) {
+	const { pathname, asPath } = useRouter()
+
 	const noSubMenu = linksWithNoSubMenu.find((x) => x.name === name)
+
+	const active = isActive({ category: name, pathname })
 
 	if (noSubMenu) {
 		return (
 			<Link href={noSubMenu.url} passHref>
-				<MainLink>
+				<MainLink data-active={active} data-linkactive={active}>
 					{navLinks[name].icon}
 					<span data-togglemenuoff={false}>{name}</span>
 				</MainLink>
 			</Link>
 		)
 	}
-	if (linksWithNoSubMenu)
-		return (
-			<Details ref={ref}>
-				<summary data-togglemenuoff={false}>
-					{navLinks[name].icon}
-					<span data-togglemenuoff={false}>{name}</span>
-				</summary>
-				<SubMenuWrapper>
-					{navLinks[name].main.map((subLink) => (
-						<Link href={subLink.path} key={subLink.path} prefetch={false} passHref>
-							<a>
-								<span style={{ width: '28px', display: 'inline-block' }}></span>
-								<span>{subLink.name}</span>
-							</a>
-						</Link>
-					))}
-				</SubMenuWrapper>
-			</Details>
-		)
+
+	return (
+		<Details ref={ref}>
+			<summary data-togglemenuoff={false} data-active={active}>
+				{navLinks[name].icon}
+				<span data-togglemenuoff={false}>{name}</span>
+			</summary>
+			<SubMenuWrapper>
+				{navLinks[name].main.map((subLink) => (
+					<Link href={subLink.path} key={subLink.path} prefetch={false} passHref>
+						<a data-linkactive={subLink.path === asPath}>
+							<span style={{ width: '28px', display: 'inline-block' }}></span>
+							<span>{subLink.name}</span>
+						</a>
+					</Link>
+				))}
+			</SubMenuWrapper>
+		</Details>
+	)
 })
 
 const Details = styled.details`
 	cursor: pointer;
+
+	summary {
+		margin-left: -6px;
+		margin-top: -6px;
+
+		padding: 6px;
+		border-radius: 6px;
+		white-space: nowrap;
+	}
 
 	summary {
 		display: flex;
@@ -54,6 +69,22 @@ const Details = styled.details`
 		opacity: 1;
 	}
 
+	summary[data-active='true'] {
+		background-color: #2172e5;
+		color: white;
+		opacity: 1;
+
+		:hover,
+		:focus-visible {
+			background-color: ${darken(0.1, '#2172e5')};
+		}
+	}
+
+	a[data-linkactive='true'] {
+		opacity: 1;
+		font-weight: 500;
+	}
+
 	summary::-webkit-details-marker {
 		display: none;
 	}
@@ -64,6 +95,10 @@ const SubMenuWrapper = styled.span`
 	display: flex;
 	flex-direction: column;
 	gap: 20px;
+
+	a:hover {
+		opacity: 1;
+	}
 `
 
 const MainLink = styled.a`
@@ -72,10 +107,55 @@ const MainLink = styled.a`
 	gap: 12px;
 	opacity: 0.8;
 	font-size: 1rem;
+	margin: -6px 0 -6px -6px;
+	padding: 6px;
+	border-radius: 6px;
+	white-space: nowrap;
 
-	&:hover {
+	&[data-active='true'] {
+		background-color: #2172e5;
+		color: white;
 		opacity: 1;
+		:hover,
+		:focus-visible {
+			background-color: ${darken(0.1, '#2172e5')};
+		}
 	}
 `
+
+const isYields = (pathname: string) =>
+	pathname === '/yields' || pathname.startsWith('/yields/') || pathname.startsWith('/yields?')
+const isStables = (pathname: string) =>
+	pathname === '/stablecoins' || pathname.startsWith('/stablecoin/') || pathname.startsWith('/stablecoins/')
+const isLiquidations = (pathname: string) => pathname === '/liquidations' || pathname.startsWith('/liquidations/')
+const isDexs = (pathname: string) =>
+	pathname === '/dexs' || pathname.startsWith('/dexs/') || pathname.startsWith('/dex/')
+const isFees = (pathname: string) =>
+	pathname === '/fees' || pathname.startsWith('/fees/') || pathname.startsWith('/fee/')
+
+const isActive = ({ pathname, category }: { pathname: string; category: string }) => {
+	switch (category) {
+		case 'Yields':
+			return isYields(pathname)
+		case 'Stables':
+			return isStables(pathname)
+		case 'Liquidations':
+			return isLiquidations(pathname)
+		case 'DEXs':
+			return isDexs(pathname)
+		case 'Fees':
+			return isFees(pathname)
+		case 'DeFi':
+			return (
+				!isYields(pathname) &&
+				!isStables(pathname) &&
+				!isLiquidations(pathname) &&
+				!isDexs(pathname) &&
+				!isFees(pathname)
+			)
+		default:
+			return false
+	}
+}
 
 export default SubMenu
