50a1e9e545897dd8af76dc7bb30d075ae7fa8d91
0xtawa <0xtawanofi@gmail.com>
2022-10-03 22:49:56 +0200

    Format terra chain key

--- src/api/categories/dexs/utils.ts
+++ src/api/categories/dexs/utils.ts
@@ -4,6 +4,7 @@ export const formatChain = (chain: string) => {
 	let c = ch === 'avax' ? "avalanche" : ch
 	if (c === 'bsc') return c.toUpperCase()
 	if (c === 'xdai') return "xDai"
+	if (c === 'terra' || c === 'terra classic') return "Terra Classic"
 	else
 		return c[0].toUpperCase() + c.slice(1)
 }
