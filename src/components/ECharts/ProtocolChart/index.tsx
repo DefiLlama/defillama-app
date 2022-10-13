4a2f7fa97b395e5398bb4b8a383b9260d1649268
vrtnd <vrotend@proton.me>
2022-10-13 18:06:50 +0300

    Add volumes to tvl chart

---
+++ src/components/ECharts/ProtocolChart/index.tsx
@@ -0,0 +1,188 @@
+import { useCallback, useEffect, useMemo, useState } from 'react'
+import * as echarts from 'echarts/core'
+import { v4 as uuid } from 'uuid'
+import styled from 'styled-components'
+import { useDarkModeManager } from '~/contexts/LocalStorage'
+import { getUtcDateObject, stringToColour } from '../utils'
+import { SelectLegendMultiple } from '../shared'
+import type { IChartProps } from '../types'
+import { useDefaults } from '../useDefaults'
+
+const Wrapper = styled.div`
+	--gradient-end: ${({ theme }) => (theme.mode === 'dark' ? 'rgba(0, 0, 0, 0.2)' : 'rgba(255, 255, 255, 0.2)')};
+`
+
+export default function AreaBarChart({
+	chartData,
+	stacks,
+	stackColors,
+	valueSymbol = '',
+	title,
+	color,
+	hallmarks,
+	customLegendName,
+	customLegendOptions,
+	tooltipSort = true,
+	chartOptions,
+	height = '360px',
+	...props
+}: IChartProps) {
+	const id = useMemo(() => uuid(), [])
+
+	const [legendOptions, setLegendOptions] = useState(customLegendOptions)
+
+	const chartsStack = stacks || customLegendOptions
+
+	const [isDark] = useDarkModeManager()
+
+	const defaultChartSettings = useDefaults({
+		color,
+		title,
+		valueSymbol,
+		tooltipSort,
+		hideLegend: true
+	})
+
+	const series = useMemo(() => {
+		const chartColor = color || stringToColour()
+
+		const series = chartsStack.map((token, index) => {
+			const stackColor = stackColors?.[token]
+
+			return {
+				name: token,
+				type: token === 'Volume' ? 'bar' : 'line',
+				scale: true,
+				yAxisIndex: token === 'Volume' ? 1 : undefined,
+				emphasis: {
+					focus: 'series',
+					shadowBlur: 10
+				},
+				symbol: 'none',
+				itemStyle: {
+					color: stackColor ? stackColor : index === 0 ? chartColor : null
+				},
+				areaStyle: {
+					color: !customLegendName
+						? new echarts.graphic.LinearGradient(0, 0, 0, 1, [
+								{
+									offset: 0,
+									color: stackColor ? stackColor : index === 0 ? chartColor : 'transparent'
+								},
+								{
+									offset: 1,
+									color: isDark ? 'rgba(0, 0, 0, 0.2)' : 'rgba(255, 255, 255, 0.2)'
+								}
+						  ])
+						: null
+				},
+				data: [],
+				...(hallmarks && {
+					markLine: {
+						data: hallmarks.map(([date, event], index) => [
+							{
+								name: event,
+								xAxis: getUtcDateObject(date),
+								yAxis: 0,
+								label: {
+									color: isDark ? 'rgba(255, 255, 255, 1)' : 'rgba(0, 0, 0, 1)',
+									fontFamily: 'inter, sans-serif',
+									fontSize: 14,
+									fontWeight: 500
+								}
+							},
+							{
+								name: 'end',
+								xAxis: getUtcDateObject(date),
+								yAxis: 'max',
+								y: Math.max(hallmarks.length * 40 - index * 40, 40)
+							}
+						])
+					}
+				})
+			}
+		})
+
+		chartData.forEach(({ date, ...item }) => {
+			chartsStack.forEach((stack) => {
+				if (legendOptions && customLegendName ? legendOptions.includes(stack) : true) {
+					series.find((t) => t.name === stack)?.data.push([getUtcDateObject(date), item[stack] || 0])
+				}
+			})
+		})
+
+		return series
+	}, [chartData, chartsStack, color, customLegendName, hallmarks, isDark, legendOptions, stackColors])
+
+	const createInstance = useCallback(() => {
+		const instance = echarts.getInstanceByDom(document.getElementById(id))
+
+		return instance || echarts.init(document.getElementById(id))
+	}, [id])
+
+	useEffect(() => {
+		// create instance
+		const chartInstance = createInstance()
+
+		const { graphic, titleDefaults, grid, tooltip, xAxis, yAxis, dataZoom, legend } = defaultChartSettings
+
+		for (const option in chartOptions) {
+			if (defaultChartSettings[option]) {
+				defaultChartSettings[option] = { ...defaultChartSettings[option], ...chartOptions[option] }
+			} else {
+				defaultChartSettings[option] = { ...chartOptions[option] }
+			}
+		}
+
+		chartInstance.setOption({
+			graphic: { ...graphic },
+			legend: {
+				...legend,
+				right: 350,
+				show: chartsStack.length > 1
+			},
+			tooltip: {
+				...tooltip
+			},
+			title: {
+				...titleDefaults
+			},
+			grid: {
+				...grid
+			},
+			xAxis: {
+				...xAxis
+			},
+			yAxis: [yAxis, { ...yAxis, type: 'value' }],
+			dataZoom: [...dataZoom],
+			series
+		})
+
+		function resize() {
+			chartInstance.resize()
+		}
+
+		window.addEventListener('resize', resize)
+
+		return () => {
+			window.removeEventListener('resize', resize)
+			chartInstance.dispose()
+		}
+	}, [createInstance, defaultChartSettings, series, chartOptions])
+
+	const legendTitle = customLegendName === 'Category' && legendOptions.length > 1 ? 'Categorie' : customLegendName
+
+	return (
+		<div style={{ position: 'relative', marginTop: 16 }} {...props}>
+			{customLegendName && customLegendOptions?.length > 1 && (
+				<SelectLegendMultiple
+					allOptions={customLegendOptions}
+					options={legendOptions}
+					setOptions={setLegendOptions}
+					title={legendOptions.length === 1 ? legendTitle : legendTitle + 's'}
+				/>
+			)}
+			<Wrapper id={id} style={{ height, margin: 'auto 0' }}></Wrapper>
+		</div>
+	)
+}
