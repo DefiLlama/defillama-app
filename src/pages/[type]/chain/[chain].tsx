5711ddf7aa0524541031aa6a6b3b848123db5c93
0xtawa <0xtawanofi@gmail.com>
2022-10-24 12:28:58 +0200

    Add new paths

--- src/pages/[type]/chain/[chain].tsx
+++ src/pages/[type]/chain/[chain].tsx
@@ -1,26 +1,16 @@
 import { getOverview } from '~/api/categories/adaptors'
+import { types } from '../[item]'
 
 export async function getStaticPaths() {
-	const { allChains: arrFees } = await getOverview('fees')
-	const { allChains: arrAggreg } = await getOverview('aggregators')
-	const { allChains: arrDeri } = await getOverview('derivatives')
-	const { allChains: arrVols } = await getOverview('volumes')
-	const paths = [
-		...arrFees.map((chain) => ({
-			params: { type: 'fees', chain: chain.toLowerCase() }
-		})),
-		...arrAggreg.map((chain) => ({
-			params: { type: 'aggregators', chain: chain.toLowerCase() }
-		})),
-		...arrVols.map((chain) => ({
-			params: { type: 'volumes', chain: chain.toLowerCase() }
-		})),
-		...arrDeri.map((chain) => ({
-			params: { type: 'derivatives', chain: chain.toLowerCase() }
-		}))
-	]
-
-	return { paths, fallback: 'blocking' }
+	const rawPaths = await Promise.all(
+		types.map(async (type) => {
+			const { allChains } = await getOverview(type)
+			return allChains.map((chain) => ({
+				params: { type, chain: chain.toLowerCase() }
+			}))
+		})
+	)
+	return { paths: rawPaths.flat(), fallback: 'blocking' }
 }
 
 export { default, getStaticProps } from './../../[type]'
