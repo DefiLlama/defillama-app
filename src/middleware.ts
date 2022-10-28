b551242aa82074fb3a14a4d8504df08c84dd4ff0
mintdart <96025197+mintdart@users.noreply.github.com>
2022-10-28 05:43:15 +0200

    cleanup

--- src/middleware.ts
+++ src/middleware.ts
@@ -9,9 +9,7 @@ export function middleware(request: NextRequest) {
 		? request.url.split('http://localhost:3000/')[1]
 		: request.url.split('.com/')[1]
 
-	console.log(requestUrl)
-
-	const { key, value } = ANNOUNCEMENT[requestUrl.startsWith('yields') ? 'yields' : 'defi']
+	const { key, value } = ANNOUNCEMENT[requestUrl?.startsWith('yields') ? 'yields' : 'defi']
 
 	// Getting cookies from the request
 	const currentKey = request.cookies.get(key)
