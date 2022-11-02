55bfe74e958c594264a15a93c64f95361f5e4280
0xtawa <0xtawanofi@gmail.com>
2022-11-02 15:30:35 +0100

    Remove volumesv2 folder

--- src/pages/volumesv2/index.tsx
+++
@@ -1,3 +0,0 @@
-import { getStaticPropsByType } from '../../utils/adaptorsPages/[type]'
-export const getStaticProps = getStaticPropsByType('fees')
-export { default } from '../../utils/adaptorsPages/[type]'
