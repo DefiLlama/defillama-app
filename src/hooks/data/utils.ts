db0c18a360296dd9bc2436fb646457a27f7c76c0
mintdart <96025197+mintdart@users.noreply.github.com>
2022-11-11 03:02:33 +0100

    revert aggregated protocols tvl calc change

--- src/hooks/data/utils.ts
+++ src/hooks/data/utils.ts
@@ -5,14 +5,16 @@ import { getPercentChange } from '~/utils'
 export const groupData = (protocols: IFormattedProtocol[], parent: IParentProtocol) => {
 	const { mcap, tvl, tvlPrevDay, tvlPrevWeek, tvlPrevMonth } = protocols.reduce(
 		(acc, curr) => {
-			curr.tvl && (acc.tvl = (acc.tvl || 0) + curr.tvl)
-			curr.tvlPrevDay && (acc.tvlPrevDay = (acc.tvlPrevDay || 0) + curr.tvlPrevDay)
-			curr.tvlPrevWeek && (acc.tvlPrevWeek = (acc.tvlPrevWeek || 0) + curr.tvlPrevWeek)
-			curr.tvlPrevMonth && (acc.tvlPrevMonth = (acc.tvlPrevMonth || 0) + curr.tvlPrevMonth)
+			if (!curr.strikeTvl) {
+				curr.tvl && (acc.tvl = (acc.tvl || 0) + curr.tvl)
+				curr.tvlPrevDay && (acc.tvlPrevDay = (acc.tvlPrevDay || 0) + curr.tvlPrevDay)
+				curr.tvlPrevWeek && (acc.tvlPrevWeek = (acc.tvlPrevWeek || 0) + curr.tvlPrevWeek)
+				curr.tvlPrevMonth && (acc.tvlPrevMonth = (acc.tvlPrevMonth || 0) + curr.tvlPrevMonth)
 
-			if (curr.mcap) {
-				acc.mcap = (acc.mcap || 0) + curr.mcap
-			} else acc.mcap = null
+				if (curr.mcap) {
+					acc.mcap = (acc.mcap || 0) + curr.mcap
+				} else acc.mcap = null
+			}
 
 			return acc
 		},
