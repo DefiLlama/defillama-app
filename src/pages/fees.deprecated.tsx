f474b4a8b1459c75c96656da9360a3009b79206c
0xtawa <0xtawanofi@gmail.com>
2022-11-11 21:03:35 +0100

    Remove fees.deprecated

--- src/pages/fees.deprecated.tsx
+++
@@ -1,9 +0,0 @@
-import FeesContainer, { feesStaticProps } from "~/containers/FeesContainer"
-
-export async function getStaticProps() {
-  return feesStaticProps("All")
-}
-
-export default function Fees(props) {
-  return FeesContainer(props)
-}
