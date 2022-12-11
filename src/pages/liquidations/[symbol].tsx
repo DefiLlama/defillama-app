d2063d02277be86043fee2f254a8a0d0a9c14481
strobie <0xstrobe@protonmail.com>
2022-12-11 15:14:40 +0100

    use ssr

--- src/pages/liquidations/[symbol].tsx
+++ src/pages/liquidations/[symbol].tsx
@@ -1,6 +1,6 @@
 /* eslint-disable no-unused-vars*/
 // eslint sucks at types
-import { NextPage, GetStaticProps, GetStaticPaths } from 'next'
+import { NextPage, GetStaticProps, GetStaticPaths, GetServerSideProps } from 'next'
 import * as React from 'react'
 import styled from 'styled-components'
 import { Clock } from 'react-feather'
@@ -15,7 +15,7 @@ import { TableSwitch } from '~/components/LiquidationsPage/TableSwitch'
 import { PositionsTable, SmolHints } from '~/components/LiquidationsPage/PositionsTable'
 import { LIQS_SETTINGS, useLiqsManager } from '~/contexts/LocalStorage'
 import type { ISearchItem } from '~/components/Search/types'
-import { revalidate } from '~/api'
+import { maxAgeForNext, revalidate } from '~/api'
 import { assetIconUrl } from '~/utils'
 import {
 	ChartData,
@@ -26,26 +26,41 @@ import {
 } from '~/utils/liquidations'
 import { LiquidationsContext } from '~/components/LiquidationsPage/context'
 
-export const getStaticProps: GetStaticProps<{ data: ChartData; prevData: ChartData }> = async ({ params }) => {
+// export const getStaticProps: GetStaticProps<{ data: ChartData; prevData: ChartData }> = async ({ params }) => {
+// 	const symbol = (params.symbol as string).toLowerCase()
+// 	const { assets: options } = await getAvailableAssetsList()
+// 	const data = await getLatestChartData(symbol, 100)
+// 	const prevData = (await getPrevChartData(symbol, 100, 3600 * 24)) ?? data
+// 	return {
+// 		props: { data, prevData, options },
+// 		revalidate: revalidate(5)
+// 	}
+// }
+
+// export const getStaticPaths: GetStaticPaths = async () => {
+// 	const { assets } = await getAvailableAssetsList()
+// 	const paths = assets
+// 		.map((x) => (x.route as string).split('/').pop())
+// 		.map((x) => ({
+// 			params: { symbol: x.toLowerCase() }
+// 		}))
+
+// 	return { paths: paths.slice(0, 5), fallback: 'blocking' }
+// }
+
+export const getServerSideProps: GetServerSideProps = async ({ params, res }) => {
 	const symbol = (params.symbol as string).toLowerCase()
 	const { assets: options } = await getAvailableAssetsList()
 	const data = await getLatestChartData(symbol, 100)
 	const prevData = (await getPrevChartData(symbol, 100, 3600 * 24)) ?? data
+	res.setHeader('CDN-Cache-Control', `max-age=${maxAgeForNext([0, 10, 20, 30, 40, 50, 60])}`)
+	res.setHeader(
+		'Cache-Control',
+		`public, s-maxage=${maxAgeForNext([0, 10, 20, 30, 40, 50, 60])}, stale-while-revalidate=10`
+	)
 	return {
-		props: { data, prevData, options },
-		revalidate: revalidate(5)
+		props: { data, prevData, options }
 	}
-}
-
-export const getStaticPaths: GetStaticPaths = async () => {
-	const { assets } = await getAvailableAssetsList()
-	const paths = assets
-		.map((x) => (x.route as string).split('/').pop())
-		.map((x) => ({
-			params: { symbol: x.toLowerCase() }
-		}))
-
-	return { paths: paths.slice(0, 5), fallback: 'blocking' }
 }
 
 const LiquidationsProvider = ({ children }) => {
