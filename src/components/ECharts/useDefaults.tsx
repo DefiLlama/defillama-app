1b6b6f7bed8db9ecdfb9befa34f84efe2ec8e6f0
0xtawa <0xtawanofi@gmail.com>
2022-12-14 12:11:42 +0100

    Temp: filter back 0 values until data is cleaned

--- src/components/ECharts/useDefaults.tsx
+++ src/components/ECharts/useDefaults.tsx
@@ -85,7 +85,7 @@ export function useDefaults({ color, title, tooltipSort = true, valueSymbol = ''
 
 				let vals
 				let filteredParams = params
-					.filter((item) => item.value[1] !== '-' && item.value[1] !== null)
+					.filter((item) => item.value[1] !== 0 && item.value[1] !== '-' && item.value[1] !== null)
 					.sort((a, b) =>
 						tooltipSort
 							? (b.value[1] < 0 ? b.value[1] * -1 : b.value[1]) - (a.value[1] < 0 ? a.value[1] * -1 : a.value[1])
