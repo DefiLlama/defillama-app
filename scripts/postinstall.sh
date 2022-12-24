a07b8efc4eae542fb3d70b3e308f2c56804a8fc2
strobie <0xstrobe@protonmail.com>
2022-12-24 23:11:36 +0100

    check NOT_VERCEL to patch

--- scripts/postinstall.sh
+++ scripts/postinstall.sh
@@ -1,2 +1,12 @@
 #!/bin/sh
+
+# source .env if it exists
+set -a
+[ -f .env ] && . .env
+
+if [ -z "$NOT_VERCEL" ]; then
+  echo "NOT_VERCEL is not set, skipping patching"
+  exit 0
+fi
+
 patch ./node_modules/next/dist/server/send-payload/revalidate-headers.js <scripts/revalidate-headers.patch
