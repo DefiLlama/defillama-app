f60a137d66f4caeac3273faf6204b15857eda193
strobie <0xstrobe@protonmail.com>
2022-12-25 22:31:18 +0100

    implement working patches

--- scripts/postinstall.sh
+++ scripts/postinstall.sh
@@ -9,5 +9,13 @@ if [ -z "$NOT_VERCEL" ]; then
   exit 0
 fi
 
+if ! grep -q '"next": "12.3.1"' package.json; then
+  echo "Next.js version is not 12.3.1, patching is not supported"
+  exit 1
+fi
+
 patch ./node_modules/next/dist/server/send-payload/revalidate-headers.js <scripts/revalidate-headers.patch
-patch ./node_modules/next/dist/server/render.js <scripts/render.patch
+patch ./node_modules/next/dist/server/response-cache/index.js <scripts/response-cache.patch
+patch ./node_modules/next/dist/server/base-server.js <scripts/base-server.patch
+
+echo "Patched Next.js"
