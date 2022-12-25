75994d46487439b67ddd4aa19a3723e832d801ef
0xngmi <0xngmi@protonmail.com>
2022-12-25 15:53:23 +0000

    add iframe

---
+++ src/pages/liquidity.tsx
@@ -0,0 +1,5 @@
+export default function Liquidity(props) {
+	return (
+		<iframe style={{width: "100%", height: "100vh", border: "0", marginBottom: "-3px"}} src="https://llamaswap.vercel.app/token-liquidity?chain=ethereum&token=WBTC" />
+	)
+}
