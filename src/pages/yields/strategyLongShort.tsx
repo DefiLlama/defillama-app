5f5298f8866d1ae2c52ca0f98d67d4262bc8c87a
slasher125 <slasher125@protonmail.com>
2023-01-14 21:25:32 +0400

    pls

--- src/pages/yields/strategyLongShort.tsx
+++ src/pages/yields/strategyLongShort.tsx
@@ -29,7 +29,6 @@ export async function getStaticProps() {
 
 	const perps = (await getPerpData()).filter((m) => m.fundingRate > 0)
 	// filter search token to only include what we have in pool arrays
-	//
 	const cgTokens = (await getAllCGTokensList()).filter((t) => poolsUniqueSymbols.includes(t.symbol?.toUpperCase()))
 	const tokens = []
 	const tokenSymbolsList = []
