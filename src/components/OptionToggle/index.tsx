88a5914579973a37186f019dfe7b3dccc0d7b213
0xtawa <0xtawanofi@gmail.com>
2023-01-23 12:28:29 +0100

    Add loading icon

--- src/components/OptionToggle/index.tsx
+++ src/components/OptionToggle/index.tsx
@@ -3,7 +3,8 @@ import ReactSwitch from 'react-switch'
 import styled from 'styled-components'
 import HeadHelp from '~/components/HeadHelp'
 import Image from 'next/future/image'
-import Loading from '~/assets/logo_white.png'
+import Loading from '~/assets/loading_circle.svg'
+import LocalLoader from '../LocalLoader'
 
 interface IProps {
 	toggle: () => void
@@ -22,26 +23,35 @@ const Wrapper = styled.div`
 
 const Switch = ReactSwitch as any
 
+const baseWidth = 40
+const baseHeight = 20
+
 const OptionToggle = ({ toggle, enabled = false, help, name, isLoading = false, ...props }: IProps) => {
 	return (
-		<Wrapper {...props}>
-			{isLoading ? (
-				/* <Image height={20} src={Loading} alt="Loading" /> */
-				<>Loading</>
-			) : (
-				<Switch
-					onChange={toggle}
-					checked={enabled}
-					onColor="#0A71F1"
-					height={20}
-					width={40}
-					uncheckedIcon={false}
-					checkedIcon={false}
-				/>
-			)}
-			&nbsp;
-			{help ? <HeadHelp title={name} text={help} /> : name}
-		</Wrapper>
+		<>
+			<Wrapper {...props}>
+				{isLoading ? (
+					<Image
+						style={{ margin: `0 ${(baseWidth - baseHeight) / 2}px` }}
+						height={baseHeight * 0.75}
+						src={Loading}
+						alt="Loading"
+					/>
+				) : (
+					<Switch
+						onChange={toggle}
+						checked={enabled}
+						onColor="#0A71F1"
+						height={baseHeight}
+						width={baseWidth}
+						uncheckedIcon={false}
+						checkedIcon={false}
+					/>
+				)}
+				&nbsp;
+				{help ? <HeadHelp title={name} text={help} /> : name}
+			</Wrapper>
+		</>
 	)
 }
 
