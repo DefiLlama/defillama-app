d3e07b890379334a38e13cffb6c5edc304087a0b
slasher125 <slasher125@protonmail.com>
2023-01-30 16:14:08 +0400

    euler scale ltv by borrowFactor

--- src/components/YieldsPage/utils.ts
+++ src/components/YieldsPage/utils.ts
@@ -118,7 +118,8 @@ export const findOptimizerPools = (pools, tokenToLend, tokenToBorrow, cdpRoutes)
 		const poolsPairs = collatteralPools.map((collatteralPool) => ({
 			...collatteralPool,
 			chains: [collatteralPool.chain],
-			borrow: pool
+			borrow: pool,
+			ltv: collatteralPool.project === 'euler' ? collatteralPool.ltv * pool.borrowFactor : collatteralPool.ltv
 		}))
 
 		return acc.concat(poolsPairs)
@@ -181,7 +182,8 @@ export const findStrategyPools = (pools, tokenToLend, tokenToBorrow, allPools, c
 		const poolsPairs = collatteralPools.map((collatteralPool) => ({
 			...collatteralPool,
 			chains: [collatteralPool.chain],
-			borrow: pool
+			borrow: pool,
+			ltv: collatteralPool.project === 'euler' ? collatteralPool.ltv * pool.borrowFactor : collatteralPool.ltv
 		}))
 
 		return acc.concat(poolsPairs)
