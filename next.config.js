2057a8208a597af0647908bbdc68d1754565ca5a
strobie <0xstrobe@protonmail.com>
2023-01-30 22:52:42 +0100

    use new yield charts

--- next.config.js
+++ next.config.js
@@ -65,7 +65,7 @@ const nextConfig = {
 		]
 	},
 	images: {
-		domains: ['icons.llama.fi', 'assets.coingecko.com', 'yield-charts.onrender.com', 'icons.llamao.fi']
+		domains: ['icons.llama.fi', 'assets.coingecko.com', 'yield-charts.llamao.fi', 'icons.llamao.fi']
 	},
 	compiler: {
 		styledComponents: true
