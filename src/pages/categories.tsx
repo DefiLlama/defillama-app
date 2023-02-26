1fc556b9c5849c3850bc21837696ceb950379985
realdealshaman <realdealshaman11@gmail.com>
2023-02-26 05:31:40 +0000

    update description for liquid staking

--- src/pages/categories.tsx
+++ src/pages/categories.tsx
@@ -74,7 +74,7 @@ export const descriptions = {
 	RWA: 'Protocols that involve Real World Assets, such as house tokenization',
 	Farm: 'Protocols that allow users to lock money in exchange for a protocol token',
 	'Liquid Staking':
-		'Protocols that allow you to stake assets in exchange of a reward, plus the receipt for the staking position is tradeable and liquid',
+		'Liquid staking protocols enable you to earn staking rewards on your tokens while also providing a tradeable and liquid receipt for your staked position',
 	Oracle: 'Protocols that connect data from the outside world (off-chain) with the blockchain world (on-chain)',
 	'Undercollateralized Lending': 'Lending with no collateral backing loans'
 }
