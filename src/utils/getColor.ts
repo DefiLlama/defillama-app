c37dd167844da4a0aad31ff62d47dad63d779bc9
mintdart <96025197+mintdart@users.noreply.github.com>
2023-03-29 11:17:49 -0500

    fix build error

--- src/utils/getColor.ts
+++ src/utils/getColor.ts
@@ -4,6 +4,8 @@ export const getColor = async (path: string) => {
 	try {
 		const color = await fetch(path).then((res) => res.text())
 
+		if (color.startsWith('404 page not')) return primaryColor
+
 		return color
 	} catch (error) {
 		return primaryColor
