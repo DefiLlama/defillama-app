c09361eb09aadbf0607fe1b81cdc3750e03503ed
slasher125 <slasher125@protonmail.com>
2023-04-13 23:42:05 +0400

    use 1inch for eth peg info

--- src/pages/lsd.tsx
+++ src/pages/lsd.tsx
@@ -151,20 +151,20 @@ const PageView = ({ chartData, lsdColors, lsdRates, nameGeckoMapping, lsdApy }) 
 		const stakedEthSum = tokenTvls.reduce((sum, a) => sum + a.stakedEth, 0)
 		const stakedEthInUsdSum = tokenTvls.reduce((sum, a) => sum + a.stakedEthInUsd, 0)
 		const tokensList = tokenTvls.map((p) => {
-			const address = lsdRates.expectedRates.find((r) => r.name === p.name)?.address
-			const priceInfo = lsdRates.marketRates[`ethereum:${address}`]
+			const priceInfo = lsdRates.marketRates?.find(
+				(i) => i.fromToken?.address?.toLowerCase() === lsdRates.expectedRates.find((r) => r.name === p.name)?.address
+			)
 			const expectedInfo = lsdRates.expectedRates.find((r) => r.name === p.name)
 
-			const marketRate = priceInfo?.marketRate
+			const marketRate = priceInfo?.toTokenAmount / 10 ** priceInfo?.fromToken?.decimals
 			const expectedRate = expectedInfo?.expectedRate
 
 			const ethPeg = (marketRate / expectedRate - 1) * 100
 			const pegInfo = expectedInfo?.peg
 
 			const lsdSymbol =
-				p.name === 'Ankr'
-					? 'ANKRETH'
-					: priceInfo?.symbol ?? (p.name === 'StakeHound' ? 'stETH' : p.name === 'Hord' ? 'hETH' : null)
+				priceInfo?.fromToken?.symbol ??
+				(p.name === 'StakeWise' ? 'sETH2' : p.name === 'StakeHound' ? 'stETH' : p.name === 'Hord' ? 'hETH' : null)
 
 			const mcaptvl = p.mcap / p.stakedEthInUsd
 
