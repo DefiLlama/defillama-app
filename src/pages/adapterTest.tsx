47031f41a9525956a0b93d0c47dafadb2d4b6472
0xngmi <0xngmi@protonmail.com>
2023-04-21 01:00:28 +0100

    encode better

--- src/pages/adapterTest.tsx
+++ src/pages/adapterTest.tsx
@@ -6,13 +6,20 @@ import type { IBarChartProps } from '~/components/ECharts/types'
 import Announcement from '~/components/Announcement'
 import { useRouter } from 'next/router'
 
+function decode(str: string) {
+	const digit = '0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz-_'
+	const fromB64 = (x: string) => x.split('').reduce((s, v) => s * 64 + digit.indexOf(v), 0)
+	return str.match(/.{1,4}/g)?.map((g) => fromB64(g.replaceAll('=', '')))
+}
+
 const BarChart = dynamic(() => import('~/components/ECharts/BarChart'), {
 	ssr: false
 }) as React.FC<IBarChartProps>
 
-export default function Protocols(props) {
+export default function AdapterTest() {
 	const router = useRouter()
-	const chartData = JSON.parse(atob((router.query?.data as string) ?? 'W10='))
+	const start = Number(router.query?.start)
+	const chartData = decode((router.query?.data as string) ?? 'W10=').map((u, i) => [start + i * (24 * 3600), u])
 	return (
 		<Layout title={`Tests`} defaultSEO>
 			<Announcement>This page is just used for tests, don't trust anything on this page</Announcement>
