cd501d858fcc6a1849fb9fa54baeede6a53c960e
vrtnd <vrotend@proton.me>
2023-04-29 17:48:41 +0300

    fix

--- src/components/ECharts/ProtocolChart/ProtocolChart.tsx
+++ src/components/ECharts/ProtocolChart/ProtocolChart.tsx
@@ -1275,40 +1275,9 @@ export const FiltersWrapper = styled.div`
 	}
 `
 
-
-export const Filters = styled.div`
-	display: flex;
-	align-items: center;
-	justify-content: space-between;
-	gap: 16px;
-	padding: 4px;
-	background-color: ${({ theme, color }) => (color ? transparentize(0.8, color) : transparentize(0.8, theme.primary1))};
-	border-radius: 12px;
-	width: fit-content;
-	flex-wrap: wrap;
-`
-
 interface IDenomination {
 	active?: boolean
 }
-
-export const Denomination = styled.a<IDenomination>`
-	display: inline-block;
-	font-weight: 500;
-	font-size: 0.875rem;
-	border-radius: 10px;
-	background: ${({ theme, active }) =>
-		active ? transparentize(0.5, theme.mode === 'dark' ? '#000' : '#fff') : 'none'};
-	padding: 6px 8px;
-	color: ${({ theme, active }) =>
-		active
-			? theme.mode === 'dark'
-				? '#fff'
-				: '#000'
-			: theme.mode === 'dark'
-			? 'rgba(255, 255, 255, 0.6)'
-			: 'rgba(0, 0, 0, 0.6)'};
-`
 
 export const formatProtocolsTvlChartData = ({ historicalChainTvls, extraTvlEnabled }) => {
 	const tvlDictionary: { [key: number]: number } = {}
@@ -1419,7 +1388,6 @@ const ToggleWrapper = styled.span`
 	margin: 0 16px;
 `
 
-
 const oneWeek = 7 * 24 * 60 * 60
 const oneMonth = 30 * 24 * 60 * 60
 
@@ -1469,4 +1437,3 @@ const groupDataByDays = (data, groupBy: string | null, chartsUnique: Array<strin
 }
 
 export { Denomination, Filters }
-
