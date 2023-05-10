e99f497d726051af319310650cba606cced97d94
0xtawa <0xtawanofi@gmail.com>
2023-05-10 16:11:19 +0200

    Remove breakdown toggle from royalties

--- src/containers/DexsAndFees/index.tsx
+++ src/containers/DexsAndFees/index.tsx
@@ -167,7 +167,7 @@ export default function OverviewContainer(props: IOverviewContainerProps) {
 					category: chain === 'All' ? 'Home' : capitalizeFirstLetter(props.type),
 					name: chain === 'All' ? capitalizeFirstLetter(props.type) : chain === 'all' ? 'Chains' : chain
 				}}
-				enableToggle={props.type !== 'fees' && props.chain !== 'all'}
+				enableToggle={props.type !== 'fees' && props.type !== 'royalties' && props.chain !== 'all'}
 				onToggleClick={
 					charts.totalDataChartBreakdown && charts.totalDataChartBreakdown.length > 0
 						? (enabled) => setEnableBreakdownChart(enabled)
