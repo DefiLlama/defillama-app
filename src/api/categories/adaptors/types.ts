11be3c94aee4bb3a9df04f6bb7b046faf28284f7
0xtawa <0xtawanofi@gmail.com>
2023-05-11 12:06:45 +0200

    Fix chain display names

--- src/api/categories/adaptors/types.ts
+++ src/api/categories/adaptors/types.ts
@@ -20,6 +20,7 @@ export interface IGetOverviewResponseBody {
 	totalDataChart: Array<[number, number]>
 	protocols: ProtocolAdaptorSummary[]
 	allChains: string[]
+	chain: string | null
 	mcap?: number
 }
 
