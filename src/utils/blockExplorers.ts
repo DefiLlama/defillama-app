1741f791a3bcdbe8cdf10b8d62d23beefd0efc5a
realdealshaman <realdealshaman11@gmail.com>
2023-05-19 05:02:16 +0100

    add pulse blockexplorer

--- src/utils/blockExplorers.ts
+++ src/utils/blockExplorers.ts
@@ -74,7 +74,8 @@ const blockExplorers = {
 	conflux: ['https://evm.confluxscan.net/address/', 'Conflux Scan'],
 	eos_evm: ['https://explorer.evm.eosnetwork.com/address/', 'EOS EVM Explorer'],
 	thorchain: ['https://thorchain.net/address/', 'Thorchain Explorer'],
-	sui: ['https://suiscan.xyz/mainnet/object/', 'Suiscan']
+	sui: ['https://suiscan.xyz/mainnet/object/', 'Suiscan'],
+	pulse: ['https://scan.pulsechain.com/address/', 'PulseChain Scan']
 }
 
 export const getBlockExplorer = (address: string = '') => {
