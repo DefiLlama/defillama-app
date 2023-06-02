ed2f8bbbc9a039264317b14ef4e0eaffff2b28df
mintdart <96025197+mintdart@users.noreply.github.com>
2023-06-02 08:13:33 -0500

    cleanup

--- src/components/ECharts/ProtocolChart/ProtocolChart.tsx
+++ src/components/ECharts/ProtocolChart/ProtocolChart.tsx
@@ -942,7 +942,7 @@ export default function ProtocolChart({
 							}
 						/>
 						<span data-wrapper="true">
-							<span>TVL</span>
+							<span>{isCEX ? 'Total Assets' : 'TVL'}</span>
 						</span>
 					</Toggle>
 
