2b98162f093e9032b38a57d10e2f4a18b3cb6b6c
mintdart <96025197+mintdart@users.noreply.github.com>
2023-06-03 21:46:49 -0500

    Update build-msg.js

--- scripts/build-msg.js
+++ scripts/build-msg.js
@@ -76,7 +76,7 @@ if (BUILD_ID) {
 
 let commitSummary = ''
 commitSummary += `📂 defillama-app`
-commitSummary += `💬 ${COMMIT_COMMENT}`
+commitSummary += `\n 💬 ${COMMIT_COMMENT}`
 commitSummary += '\n' + `🦙 ${COMMIT_AUTHOR}`
 commitSummary += '\n' + `📸 ${COMMIT_HASH}`
 
