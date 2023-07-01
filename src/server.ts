79cc1963975edc9fef8b4915902815eb0ce21aa6
strobie <0xstrobe@protonmail.com>
2023-07-01 10:06:27 +0200

    add custom server

---
+++ src/server.ts
@@ -0,0 +1,17 @@
+import { createServer } from 'http'
+import { parse } from 'url'
+import next from 'next'
+
+const port = 3000
+const dev = process.env.NODE_ENV !== 'production'
+const app = next({ dev })
+const handle = app.getRequestHandler()
+
+app.prepare().then(() => {
+	createServer((req, res) => {
+		const parsedUrl = parse(req.url!, true)
+		handle(req, res, parsedUrl)
+	}).listen(port)
+
+	console.log(`> Server listening at http://localhost:${port} as ${dev ? 'development' : process.env.NODE_ENV}`)
+})
