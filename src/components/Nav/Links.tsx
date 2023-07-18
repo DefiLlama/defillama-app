155d24029c15e855a5dbcc15c228ece0ee044680
0xngmi <0xngmi@protonmail.com>
2023-07-19 00:13:10 +0100

    add derivatives link

--- src/components/Nav/Links.tsx
+++ src/components/Nav/Links.tsx
@@ -268,6 +268,7 @@ export const navLinks: ILinks = {
 		main: [
 			{ name: 'Overview', path: '/dexs' },
 			{ name: 'Chains', path: '/dexs/chains' },
+			{ name: 'Derivatives', path: '/derivatives' },
 			{ name: 'Options', path: '/options' }
 		],
 		...defaultToolsAndFooterLinks,
