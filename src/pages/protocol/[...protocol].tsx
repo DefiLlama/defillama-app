180ad4f5698cd81c710476324b3d233d1fb46b76
0xngmi <0xngmi@protonmail.com>
2023-11-23 15:59:09 +0000

    move code execution to client

--- src/pages/protocol/[...protocol].tsx
+++ src/pages/protocol/[...protocol].tsx
@@ -4,31 +4,32 @@ import { getProtocols } from '~/api/categories/protocols'
 import { DummyProtocol } from '~/containers/Defi/Protocol/Dummy'
 import { withPerformanceLogging } from '~/utils/perf'
 import { getProtocolData } from '~/api/categories/protocols/getProtocolData'
+import { useRouter } from 'next/router'
+import Layout from '~/layout'
+import LocalLoader from '~/components/LocalLoader'
+import useSWR from 'swr'
 
-export const getStaticProps = withPerformanceLogging(
-	'protocol/[...protocol]',
-	async ({
-		params: {
-			protocol: [protocol]
-		}
-	}) => {
-		const data = await getProtocolData(protocol)
-		return data
-	}
-)
-
-export async function getStaticPaths() {
-	const res = await getProtocols()
-
-	const paths: string[] = res.protocols.slice(0, 30).map(({ name }) => ({
-		params: { protocol: [standardizeProtocolName(name)] }
-	}))
-
-	return { paths, fallback: 'blocking' }
+export const useCollectionData = (slug) => {
+	const { data, error } = useSWR(slug, getProtocolData)
+	return { data, error, loading: !data && !error }
 }
 
-export default function Protocols({ protocolData, ...props }) {
+export default function Protocols() {
+	const router = useRouter()
+	const { data, loading: fetchingData } = useCollectionData(router.query.protocol)
+	if (fetchingData) {
+		return (
+			<Layout title={'Protocol - DefiLlama'}>
+				<LocalLoader />
+			</Layout>
+		)
+	}
+	const props = data.props
 	return (
-		<ProtocolContainer title={`${protocolData.name} - DefiLlama`} protocolData={protocolData} {...(props as any)} />
+		<ProtocolContainer
+			title={`${props.protocolData.name} - DefiLlama`}
+			protocolData={props.protocolData}
+			{...(props as any)}
+		/>
 	)
 }
