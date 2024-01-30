2285d1cb2b0e814b03b4d07c29aad61f7df29d85
strobie <0xstrobe@protonmail.com>
2024-01-30 13:20:37 +0100

    post msg to discord

--- scripts/start.sh
+++ scripts/start.sh
@@ -5,3 +5,14 @@ set -a
 [ -f .env ] && . .env
 
 pm2 start ecosystem.config.js
+
+COMMIT_HASH=$(git rev-parse --short HEAD)
+APP_NAME=dla-$COMMIT_HASH
+
+# post message to discord
+if [ -z "$BUILD_STATUS_WEBHOOK" ]; then
+  echo "BUILD_STATUS_WEBHOOK is not set, skipping post message to discord"
+  exit 0
+fi
+
+curl -H "Content-Type: application/json" -X POST -d '{"content": "App started: '$APP_NAME'"}' $BUILD_STATUS_WEBHOOK
--- scripts/stop.sh
+++ scripts/stop.sh
@@ -5,3 +5,14 @@ set -a
 [ -f .env ] && . .env
 
 pm2 delete ecosystem.config.js
+
+COMMIT_HASH=$(git rev-parse --short HEAD)
+APP_NAME=dla-$COMMIT_HASH
+
+# post message to discord
+if [ -z "$BUILD_STATUS_WEBHOOK" ]; then
+  echo "BUILD_STATUS_WEBHOOK is not set, skipping post message to discord"
+  exit 0
+fi
+
+curl -H "Content-Type: application/json" -X POST -d '{"content": "App stopped: '$APP_NAME'"}' $BUILD_STATUS_WEBHOOK
