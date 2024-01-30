75c0d23f44175cd4f9eaf4085bf85fc3cc1688ca
strobie <0xstrobe@protonmail.com>
2024-01-30 12:57:11 +0100

    betta patching logic

--- scripts/postinstall.sh
+++ scripts/postinstall.sh
@@ -14,8 +14,26 @@ if ! grep -q '"next": "14.1.0"' package.json; then
   exit 1
 fi
 
-patch ./node_modules/next/dist/server/send-payload.js <scripts/send-payload.patch
-patch ./node_modules/next/dist/server/lib/revalidate.js <scripts/revalidate.patch
-patch ./node_modules/next/dist/server/base-server.js <scripts/base-server.patch
+apply_patch() {
+  local file_to_patch="$1"
+  local patch_file="$2"
 
-echo "TODO: Patched Next.js"
+  # Check if the patch is already applied
+  if patch -R --dry-run -s -f -p1 <"$patch_file" >/dev/null; then
+    echo "Patch already applied to $file_to_patch, skipping..."
+    return 0
+  else
+    echo "Applying patch to $file_to_patch..."
+    if patch "$file_to_patch" <"$patch_file"; then
+      echo "Patch applied successfully to $file_to_patch"
+      return 0
+    else
+      echo "Failed to apply patch to $file_to_patch"
+      return 1
+    fi
+  fi
+}
+
+apply_patch ./node_modules/next/dist/server/send-payload.js scripts/send-payload.patch
+apply_patch ./node_modules/next/dist/server/lib/revalidate.js scripts/revalidate.patch
+apply_patch ./node_modules/next/dist/server/base-server.js scripts/base-server.patch
