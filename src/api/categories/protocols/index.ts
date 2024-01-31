eda93c9ecafddd938c92b647f97ff08dcf66134a
slasher125 <slasher125@protonmail.com>
2024-01-31 15:38:23 +0400

    lsd switch areachart to %

--- src/api/categories/protocols/index.ts
+++ src/api/categories/protocols/index.ts
@@ -697,7 +697,7 @@ export async function getLSDPageData() {
 	const lsdProtocols = protocols
 		.filter((p) => (p.category === 'Liquid Staking' || ['Stafi'].includes(p.name)) && p.chains.includes('Ethereum'))
 		.map((p) => p.name)
-		.filter((p) => p !== 'Genius')
+		.filter((p) => !['StakeHound', 'Genius', 'SharedStake'].includes(p))
 
 	// get historical data
 	const lsdProtocolsSlug = lsdProtocols.map((p) => p.replace(/\s+/g, '-').toLowerCase())
