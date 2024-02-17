ca24a5d0d5905a5d7da40448b828b5daf3bd6848
0xngmi <0xngmi@protonmail.com>
2024-02-17 02:35:20 +0000

    csv of historical data

--- src/containers/DexsAndFees/index.tsx
+++ src/containers/DexsAndFees/index.tsx
@@ -33,35 +33,6 @@ export default function OverviewContainer(props: IOverviewContainerProps) {
 	const isSimpleFees = props.isSimpleFees
 	const router = useRouter()
 
-	const downloadCsv = React.useCallback(() => {
-		const rows = {
-			displayName: 'Name',
-			total24h: 'Total 24h',
-			tvl: 'DEX TVL',
-			change_7dover7d: 'Change 7d/7d',
-			total7d: 'Total 7d',
-			total30d: 'Total 30d',
-			change_1d: 'Change 1d',
-			change_7d: 'Change 7d',
-			change_1m: 'Change 1m',
-			total1y: 'Total 1y'
-		}
-
-		const columnsToPick = Object.keys(rows)
-
-		const data = props.protocols.map((p) => {
-			const row = []
-			columnsToPick.forEach((r) => {
-				row.push(p[r])
-			})
-			return row
-		})
-
-		const header = columnsToPick.map((c) => rows[c]).join(',')
-		const rowsData = data.map((d) => d.join(',')).join('\n')
-
-		download('protocols.csv', header + '\n' + rowsData)
-	}, [props.protocols])
 	const { dataType: selectedDataType = 'Notional volume' } = router.query
 	const [enableBreakdownChart, setEnableBreakdownChart] = React.useState(false)
 	const [enabledSettings] = useFeesManager()
@@ -215,6 +186,23 @@ export default function OverviewContainer(props: IOverviewContainerProps) {
 		}
 		return props.totalDataChart
 	}, [enableBreakdownChart, charts, props.totalDataChart, props.protocols, selectedDataType])
+
+	const downloadCsv = React.useCallback(() => {
+		const columnsToPick = ['date', ...chartData[1]]
+
+		const data = chartData[0].map((p) => {
+			const row = []
+			columnsToPick.forEach((r) => {
+				row.push(p[r])
+			})
+			return row
+		})
+
+		const header = columnsToPick.join(',')
+		const rowsData = data.map((d) => d.join(',')).join('\n')
+
+		download('protocols.csv', header + '\n' + rowsData)
+	}, [props.protocols])
 
 	return (
 		<>
