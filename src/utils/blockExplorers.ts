5639661d8ebd808113c147ee7443a5a6e816e4ba
realdealshaman <realdealshaman11@gmail.com>
2024-02-27 01:02:07 +0000

    add blockexplorer

--- src/utils/blockExplorers.ts
+++ src/utils/blockExplorers.ts
@@ -108,7 +108,8 @@ const blockExplorers = {
 	zkfair: ['https://scan.zkfair.io/address/', 'Zkfair Explorer'],
 	bitnet: ['https://btnscan.com/address/', 'BTNScan'],
 	aptos: ['https://aptoscan.com/account/', 'Aptoscan'],
-	zeta: ['https://zetachain.blockscout.com/token/', 'Zeta Blockscout']
+	zeta: ['https://zetachain.blockscout.com/token/', 'Zeta Blockscout'],
+	manta_atlantic: ['https://manta.subscan.io/account/', 'Manta Atlantic Subscan']
 }
 
 export const getBlockExplorer = (address: string = '') => {
