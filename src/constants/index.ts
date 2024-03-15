ca83f7745ebdc1e5627ab3f3710924d0776b5eaf
waynebruce0x <waynebruce0x@gmail.com>
2024-03-15 10:42:01 +0000

    chain assets endpoint

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -94,7 +94,7 @@ export const ETF_HISTORY_API = 'https://etfs.llama.fi/history'
 export const LIQUIDATIONS_HISTORICAL_R2_PATH = DATASETS_R2_PATH + '/liqs'
 
 export const CHAINS_API = 'https://api.llama.fi/chains'
-export const CHAINS_ASSETS = 'https://api.llama.fi/chainAssets'
+export const CHAINS_ASSETS = 'https://api.llama.fi/chain-assets/chains'
 export const CHAINS_API_V2 = 'https://api.llama.fi/chains2'
 
 export const DEXS_API = 'https://api.llama.fi/dexs'
