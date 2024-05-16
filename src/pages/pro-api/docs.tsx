a4b6c7e58e5ed3bc4583096a4ed1774548f6966b
0xngmi <0xngmi@protonmail.com>
2024-05-16 19:38:35 +0100

    include api key in docs

--- src/pages/pro-api/docs.tsx
+++ src/pages/pro-api/docs.tsx
@@ -3,5 +3,19 @@ import 'swagger-ui/dist/swagger-ui.css'
 import ApiDocs from '../docs/api'
 
 export default function Docs() {
-	return <ApiDocs spec={yamlApiSpec} />
+	if (typeof window === 'undefined') {
+		return <>Loading...</>
+	}
+	const apiKey = window.localStorage.getItem(`pro_apikey`)
+	if (apiKey === null) {
+		return <>You are not subscribed or logged in</>
+	}
+	return (
+		<ApiDocs
+			spec={{
+				...yamlApiSpec,
+				servers: yamlApiSpec.servers.map((s) => ({ ...s, url: s.url.replaceAll('APIKEY', apiKey) }))
+			}}
+		/>
+	)
 }
