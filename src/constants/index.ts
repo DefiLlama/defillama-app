f33fcbaffbbcff0cadf5eb2b86350008dc16d706
slasher125 <slasher125@protonmail.com>
2024-06-28 15:09:41 +0400

    fix api rul

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -140,7 +140,7 @@ export const MCAPS_API = 'https://coins.llama.fi/mcaps'
 
 export const CACHE_SERVER = 'https://fe-cache.llama.fi'
 
-export const CATEGORY_RETURNS_API = 'http://localhost:3000/returns'
+export const CATEGORY_RETURNS_API = 'https://fdv-server.llama.fi/returns'
 
 export const scams = [
 	'SyncDEX Finance',
