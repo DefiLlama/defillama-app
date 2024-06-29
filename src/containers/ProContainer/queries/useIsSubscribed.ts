de1ef9c39b1994012cde1a086b104df84418f9fa
0xngmi <0xngmi@protonmail.com>
2024-06-30 00:52:45 +0100

    fix subgraph

--- src/containers/ProContainer/queries/useIsSubscribed.ts
+++ src/containers/ProContainer/queries/useIsSubscribed.ts
@@ -1,30 +1,14 @@
 import { useQuery } from 'react-query'
 import { useAccount } from 'wagmi'
-import { SUBSCRIPTION_PRICE } from '../constants'
+import { SERVER_API } from '~/containers/ProApi/lib/constants'
 
-function isSubscribed(toAddress, amount, userAddress) {
-	return fetch('https://api.thegraph.com/subgraphs/name/0xngmi/llamasubs-optimism', {
-		method: 'post',
-		body: JSON.stringify({
-			query: `
-query Subs($now: BigInt, $userAddress: Bytes, $receiver: Bytes, $minAmountPerCycle:BigInt) {
-  subs(
-    where: {owner: $userAddress, startTimestamp_lt: $now, realExpiration_gte: $now, receiver: $receiver, amountPerCycle_gte: $minAmountPerCycle}
-  ) {
-    startTimestamp
-    realExpiration
-  }
-}`,
-			variables: {
-				now: Math.floor(Date.now() / 1e3),
-				userAddress,
-				receiver: toAddress,
-				minAmountPerCycle: amount.toString()
-			}
-		})
-	})
-		.then((r) => r.json())
-		.then((r) => r.data.subs.length > 0)
+export async function isSubscribed(userAddress) {
+	const { subscribed } = await fetch(`${SERVER_API}/auth/subscribed/${userAddress}`, {
+		headers: {
+			'Content-Type': 'application/json'
+		},
+	}).then((r) => r.json())
+	return subscribed
 }
 
 const useIsSubscribed = () => {
@@ -32,8 +16,6 @@ const useIsSubscribed = () => {
 
 	const res = useQuery(['isSubscribed', wallet.address], () =>
 		isSubscribed(
-			`0x08a3c2A819E3de7ACa384c798269B3Ce1CD0e437`.toLowerCase(),
-			SUBSCRIPTION_PRICE,
 			wallet.address.toLowerCase()
 		)
 	)
