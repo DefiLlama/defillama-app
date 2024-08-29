d573639d225f1b6fab4b6dc0095c17043016ea0f
slasher125 <74533329+slasher125@users.noreply.github.com>
2024-08-29 12:00:15 +0400

    Revert "Update hooks.tsx (#1745)" (#1746)
    This reverts commit 56a13f97c48ed0e56895d0c21c7d654fab18e4c3.

--- src/components/Search/ProtocolsChains/hooks.tsx
+++ src/components/Search/ProtocolsChains/hooks.tsx
@@ -1,5 +1,19 @@
 import { useMemo } from 'react'
 
 export const useFormatDefiSearchResults = (results) => {
-	return results.hits
+	return useMemo(() => {
+		return results.hits.map((hit) => ({
+			name: hit.name,
+			route: `/${hit.id.startsWith('chain') ? 'chain' : hit.id.startsWith('category') ? 'protocols' : 'protocol'}/${
+				hit.id.startsWith('parent') || hit.id.startsWith('protocol')
+					? hit.id.split('_')[1]
+					: hit.name.replace(' ', '%20')
+			}`,
+			logo: hit.id.startsWith('category')
+				? null
+				: hit.id.startsWith('chain')
+				? `https://icons.llamao.fi/icons/chains/rsz_${hit.id.split('_')[1]}?w=48&h=48`
+				: `https://icons.llamao.fi/icons/protocols/${hit.id.split('_')[1]}?w=48&h=48`
+		}))
+	}, [results])
 }
