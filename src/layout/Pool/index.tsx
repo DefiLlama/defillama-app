6f9459ab4a55270fc5a5f248a1321c0cba4444e7
mintdart <96025197+mintdart@users.noreply.github.com>
2024-10-29 07:20:13 -0500

    update styles

--- src/layout/Pool/index.tsx
+++ src/layout/Pool/index.tsx
@@ -4,7 +4,7 @@ import { DetailsWrapper } from '../ProtocolAndPool'
 import { ComponentProps } from 'react'
 
 export const ButtonSquare = ({ children, ...props }: ComponentProps<typeof ButtonLight>) => (
-	<ButtonLight className="flex items-center gap-4 font-normal whitespace-nowrap w-6" {...props}>
+	<ButtonLight className="flex items-center justify-center gap-4 !p-[6px]" {...props}>
 		{children}
 	</ButtonLight>
 )
