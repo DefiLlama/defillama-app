b5c61f83d798ffe3ee91caa775a9526c0677c9a3
mintdart <96025197+mintdart@users.noreply.github.com>
2024-12-12 16:54:21 -0600

    add odos airdrop eligibility check

--- src/containers/RecentProtocols/airdrops.ts
+++ src/containers/RecentProtocols/airdrops.ts
@@ -5,6 +5,14 @@ export async function airdropsEligibilityCheck({ addresses }: { addresses: Array
 			fetch('https://airdrops.llama.fi/config').then((res) => res.json())
 		])
 
+		const odos = await Promise.all(
+			addresses.map((add) => fetch(`https://api.odos.xyz/loyalty/users/${add}/balances`).then((res) => res.json()))
+		)
+
+		const odosClaimableByAddress = Object.fromEntries(
+			addresses.map((address, index) => [address.toLowerCase(), odos[index]?.data ?? {}])
+		)
+
 		const allAirdrops: Array<
 			[
 				string,
@@ -19,18 +27,32 @@ export async function airdropsEligibilityCheck({ addresses }: { addresses: Array
 			]
 		> = Object.entries(others).map(([address, airdrops]: [string, Record<string, number>]) => [
 			address,
-			Object.entries(airdrops ?? {})
-				.map((airdrop) => ({
-					name: config[airdrop[0]]?.name ?? airdrop[0],
-					claimableAmount: airdrop[1],
-					page: config[airdrop[0]]?.page ?? null,
-					token: config[airdrop[0]]?.token ?? null,
-					tokenSymbol: config[airdrop[0]]?.tokenSymbol ?? null,
-					isActive: config[airdrop[0]]?.endTime
-						? new Date().getTime() < new Date(config[airdrop[0]].endTime * 1000).getTime()
-						: config[airdrop[0]]?.isActive ?? false
-				}))
-				.filter((x) => x.isActive)
+			[
+				...Object.entries(airdrops ?? {})
+					.map((airdrop) => ({
+						name: config[airdrop[0]]?.name ?? airdrop[0],
+						claimableAmount: airdrop[1],
+						page: config[airdrop[0]]?.page ?? null,
+						token: config[airdrop[0]]?.token ?? null,
+						tokenSymbol: config[airdrop[0]]?.tokenSymbol ?? null,
+						isActive: config[airdrop[0]]?.endTime
+							? new Date().getTime() < new Date(config[airdrop[0]].endTime * 1000).getTime()
+							: config[airdrop[0]]?.isActive ?? false
+					}))
+					.filter((x) => x.isActive),
+				...(odosClaimableByAddress[address]
+					? [
+							{
+								name: 'Odos',
+								claimableAmount: Number((+odosClaimableByAddress[address].pendingTokenBalance / 10 ** 18).toFixed(2)),
+								page: null,
+								token: null,
+								tokenSymbol: 'ODOS',
+								isActive: true
+							}
+					  ]
+					: [])
+			]
 		])
 
 		return allAirdrops
