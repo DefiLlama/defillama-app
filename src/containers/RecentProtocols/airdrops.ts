588913b09f34c9184447289c313ec664bbcde0df
mintdart <96025197+mintdart@users.noreply.github.com>
2024-12-13 06:52:41 -0600

    update airdrop eligibility check

--- src/containers/RecentProtocols/airdrops.ts
+++ src/containers/RecentProtocols/airdrops.ts
@@ -5,14 +5,6 @@ export async function airdropsEligibilityCheck({ addresses }: { addresses: Array
 			fetch('https://airdrops.llama.fi/config').then((res) => res.json())
 		])
 
-		const odos = await Promise.all(
-			addresses.map((add) => fetch(`https://api.odos.xyz/loyalty/users/${add}/balances`).then((res) => res.json()))
-		)
-
-		const odosClaimableByAddress = Object.fromEntries(
-			addresses.map((address, index) => [address.toLowerCase(), odos[index]?.data ?? {}])
-		)
-
 		const allAirdrops: Array<
 			[
 				string,
@@ -27,32 +19,18 @@ export async function airdropsEligibilityCheck({ addresses }: { addresses: Array
 			]
 		> = Object.entries(others).map(([address, airdrops]: [string, Record<string, number>]) => [
 			address,
-			[
-				...Object.entries(airdrops ?? {})
-					.map((airdrop) => ({
-						name: config[airdrop[0]]?.name ?? airdrop[0],
-						claimableAmount: airdrop[1],
-						page: config[airdrop[0]]?.page ?? null,
-						token: config[airdrop[0]]?.token ?? null,
-						tokenSymbol: config[airdrop[0]]?.tokenSymbol ?? null,
-						isActive: config[airdrop[0]]?.endTime
-							? new Date().getTime() < new Date(config[airdrop[0]].endTime * 1000).getTime()
-							: config[airdrop[0]]?.isActive ?? false
-					}))
-					.filter((x) => x.isActive),
-				...(odosClaimableByAddress[address]
-					? [
-							{
-								name: 'Odos',
-								claimableAmount: Number((+odosClaimableByAddress[address].pendingTokenBalance / 10 ** 18).toFixed(2)),
-								page: null,
-								token: null,
-								tokenSymbol: 'ODOS',
-								isActive: true
-							}
-					  ]
-					: [])
-			]
+			Object.entries(airdrops ?? {})
+				.map((airdrop) => ({
+					name: config[airdrop[0]]?.name ?? airdrop[0],
+					claimableAmount: airdrop[1],
+					page: config[airdrop[0]]?.page ?? null,
+					token: config[airdrop[0]]?.token ?? null,
+					tokenSymbol: config[airdrop[0]]?.tokenSymbol ?? null,
+					isActive: config[airdrop[0]]?.endTime
+						? new Date().getTime() < new Date(config[airdrop[0]].endTime * 1000).getTime()
+						: config[airdrop[0]]?.isActive ?? false
+				}))
+				.filter((x) => x.isActive)
 		])
 
 		return allAirdrops
