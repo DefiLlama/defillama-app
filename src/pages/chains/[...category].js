bbd0e9cd21f9d300851999384824d901fdf5ec08
0xngmi <0xngmi@protonmail.com>
2024-12-23 15:10:15 +0000

    fix static paths thing

--- src/pages/chains/[...category].js
+++ src/pages/chains/[...category].js
@@ -3,12 +3,7 @@ import Layout from '~/layout'
 import ChainsContainer from '~/containers/Defi/Chains'
 import { maxAgeForNext } from '~/api'
 import { getNewChainsPageData } from '~/api/categories/protocols'
-import { CONFIG_API } from '~/constants/index'
 import { withPerformanceLogging } from '~/utils/perf'
-
-import { fetchWithErrorLogging } from '~/utils/async'
-
-const fetch = fetchWithErrorLogging
 
 export const getStaticProps = withPerformanceLogging(
 	'chains/[...category]',
@@ -24,6 +19,10 @@ export const getStaticProps = withPerformanceLogging(
 		}
 	}
 )
+
+export async function getStaticPaths() {
+	return { paths: [], fallback: 'blocking' }
+}
 
 export default function Chains(props) {
 	const { category } = props
