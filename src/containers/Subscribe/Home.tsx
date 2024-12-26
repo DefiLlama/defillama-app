645b8d8973d20fb8c78d9f61c574f9b748b13949
mintdart <96025197+mintdart@users.noreply.github.com>
2024-12-26 07:25:10 -0600

    fix

---
+++ src/containers/Subscribe/Home.tsx
@@ -0,0 +1,267 @@
+import { Tooltip, TooltipAnchor, useTooltipState } from 'ariakit'
+import { Fragment, useEffect, useRef, useState } from 'react'
+import { Icon } from '~/components/Icon'
+import { useGithubAuth, useIsSubscribed } from '~/containers/Subscribe/queries'
+
+export function SubscribeHome() {
+	const { data: githubAuthData } = useGithubAuth()
+	const { data: isSubscribed } = useIsSubscribed()
+
+	const pricingContainer = useRef<HTMLDivElement>(null)
+	const [activePriceCard, setActivePriceCard] = useState(1)
+	useEffect(() => {
+		const ref = pricingContainer.current
+		if (!ref) return
+
+		const onScroll = () => {
+			setActivePriceCard(Math.round(ref!.scrollLeft / window.innerWidth) + 1)
+		}
+
+		ref?.addEventListener('scrollend', onScroll)
+
+		return () => {
+			ref?.removeEventListener('scrollend', onScroll)
+		}
+	}, [])
+
+	const tooltip = useTooltipState()
+	return (
+		<>
+			<div className="flex flex-col gap-3 w-full max-w-6xl xl:max-w-7xl 2xl:max-w-[1440px] mx-auto pb-[64px] px-5 relative">
+				<div className="relative h-[118px] w-[118px] aspect-square object-contain mx-auto rounded-full">
+					<div
+						style={{
+							filter: 'blur(64px)',
+							background: 'linear-gradient(90deg, #5C5EFC 0%, #462A92 100%)'
+						}}
+						className="h-[132px] w-[132px] aspect-square object-contain mx-auto rounded-full absolute -z-10"
+					/>
+					<img
+						src="/llama.png"
+						height={118}
+						width={118}
+						className="aspect-square object-contain mx-auto rounded-full z-10"
+						alt=""
+					/>
+				</div>
+				<h1 className="text-[2rem] font-[800] text-center">DL Pro API</h1>
+				{isSubscribed ? null : (
+					<p className="text-[#919296] text-center">
+						Upgrade now for increased api limits and premium api endpoints. DefiLlama contributors will have free 3
+						month access to premium API.
+					</p>
+				)}
+
+				<div
+					className="h-[132px] w-[90%] mx-auto rounded-[50%] relative -bottom-[87px] -mb-[45px] -z-10"
+					style={{
+						filter: 'blur(64px)',
+						background: 'linear-gradient(90deg, #5C5EFC 0%, #462A92 100%)'
+					}}
+				/>
+
+				{githubAuthData?.login ? (
+					<div className="bg-[#222429] border border-[#39393E] rounded-2xl relative z-10 py-10 px-5 flex flex-col gap-1">
+						{/* <GithubApiKey data={githubAuthData} /> */}
+					</div>
+				) : isSubscribed ? (
+					<div className="bg-[#222429] border border-[#39393E] rounded-2xl relative z-10 py-10 px-5 flex flex-col gap-1">
+						{/* <ProApiKey /> */}
+					</div>
+				) : (
+					<div
+						className="pricing-container bg-[#222429] border border-[#39393E] rounded-2xl flex flex-row relative z-10 overflow-x-auto max-lg:-mx-5"
+						ref={pricingContainer}
+					>
+						<div className="price-card py-10 px-5 lg:flex-1 flex flex-col max-lg:w-[85vw] flex-shrink-0">
+							<h2 className="whitespace-nowrap text-[2rem] font-[800] text-center">Open API</h2>
+							<p className="text-center text-2xl font-medium mt-3">Free</p>
+							<p className="text-center font-medium text-[#8a8c90]">
+								Check the{' '}
+								<a
+									href="https://defillama.com/docs/api"
+									target="_blank"
+									rel="noreferrer noopener"
+									className="underline"
+								>
+									open API doc
+								</a>
+							</p>
+							<ul className="flex flex-col mx-auto gap-4 mt-11 mb-auto">
+								<li className="flex flex-nowrap gap-[10px]">
+									<Icon name="check" height={16} width={16} className="relative top-1" />
+									<span>Access to TVL, revenue/fees and prices</span>
+								</li>
+								<li className="text-[#8a8c90] flex flex-nowrap gap-[10px]">
+									<Icon name="x" height={16} width={16} className="relative top-1" />
+									<span>Access to all data (unlocks, active users, token liq...)</span>
+								</li>
+								<li className="px-[26px] flex flex-col gap-1">
+									<span>10-200 requests/minutes</span>
+									<ProgressBar pct={12} />
+								</li>
+								<li className="px-[26px] flex flex-col gap-1">
+									<span>0 calls/month</span>
+									<ProgressBar pct={0} />
+								</li>
+								<li className="px-[26px] flex flex-col gap-1">Support on public Discord</li>
+							</ul>
+							<a
+								href="https://defillama.com/docs/api"
+								target="_blank"
+								rel="noreferrer noopener"
+								className="flex-shrink-0 relative -top-2 font-medium rounded-lg border border-[#39393E] py-[14px] w-full max-w-[200px] text-center mx-auto mt-16"
+							>
+								Open API doc
+							</a>
+						</div>
+						<div className="price-card py-10 px-5 lg:flex-1 flex flex-col border-l border-[#39393E] max-lg:w-[85vw] flex-shrink-0">
+							<h2 className="whitespace-nowrap text-[2rem] font-[800] text-center text-[#5C5CF9]">Pro API</h2>
+							<p className="text-center text-2xl font-medium mt-3">
+								300 USD <span className="text-[#8a8c90]">/month</span>
+							</p>
+							<p className="text-center font-medium text-[#8a8c90]">Multiple payment options</p>
+							<ul className="flex flex-col mx-auto gap-4 mt-11 mb-auto">
+								<li className="flex flex-nowrap gap-[10px]">
+									<Icon name="check" height={16} width={16} className="relative top-1" />
+									<span>Access to TVL, revenue/fees and prices</span>
+								</li>
+								<li className="flex flex-nowrap gap-[10px]">
+									<Icon name="check" height={16} width={16} className="relative top-1" />
+									<span>Access to all data (unlocks, active users, token liq...)</span>
+								</li>
+								<li className="px-[26px] flex flex-col gap-1">
+									<span>1000 requests/minute</span>
+									<ProgressBar pct={100} />
+								</li>
+								<li className="px-[26px] flex flex-col gap-1">
+									<span>1M calls/month</span>
+									<ProgressBar pct={100} />
+								</li>
+								<li className="px-[26px] flex flex-col gap-1">Priority support</li>
+							</ul>
+							<a
+								href="https://stripe.com"
+								target="_blank"
+								rel="noreferrer noopener"
+								className="relative -top-2 flex items-center justify-center gap-1 font-medium rounded-lg border border-[#39393E] bg-[#5C5CF9] py-[14px] w-full max-w-[200px] text-center mx-auto shadow-[0px_0px_32px_0px_#5C5CF980]"
+							>
+								<span>Pay with Stripe</span>
+								<Icon name="card" height={16} width={16} />
+							</a>
+
+							<a
+								href="https://defillama.com/pro-api/docs"
+								target="_blank"
+								rel="noreferrer noopener"
+								className="-mb-8 mt-2 text-center underline text-[#8a8c90]"
+							>
+								Click here a full lists of all endpoints available in Pro
+							</a>
+						</div>
+						<div className="price-card py-10 px-5 lg:flex-1 flex flex-col border-l border-[#39393E] max-lg:w-[85vw] flex-shrink-0">
+							<h2 className="text-[2rem] font-[800] text-center flex flex-nowrap items-center justify-center gap-1">
+								<span>Contributor</span>
+
+								<TooltipAnchor
+									state={tooltip}
+									className="flex flex-nowrap items-center justify-center gap-1 relative top-1"
+								>
+									<span className="sr-only">Availability</span>
+									<Icon name="circle-help" height={16} width={16} />
+								</TooltipAnchor>
+								<Tooltip
+									state={tooltip}
+									className="bg-black border border-[#39393E] rounded-2xl relative z-10 p-4 max-w-sm text-sm"
+								>
+									Only available to users who have contributed 5 commits or more over at least 2 months on the following
+									repos:{' '}
+									{eligibleRepos.map((repo, index) => (
+										<Fragment key={repo}>
+											<a
+												href={`https://github.com/DefiLlama/${repo}`}
+												target="_blank"
+												rel="noreferrer noopener"
+												className="p-1 bg-[#222429] rounded"
+											>
+												{repo}
+											</a>
+											{index + 1 !== eligibleRepos.length ? `, ` : ''}
+										</Fragment>
+									))}
+								</Tooltip>
+							</h2>
+							<p className="text-center text-2xl font-medium mt-3">Free</p>
+							<p className="text-center font-medium text-[#8a8c90]">For all Github contributors</p>
+							<ul className="flex flex-col mx-auto gap-4 mt-11 mb-auto">
+								<li className="flex flex-nowrap gap-[10px]">
+									<Icon name="check" height={16} width={16} className="relative top-1" />
+									<span>Access to TVL, revenue/fees and prices</span>
+								</li>
+								<li className="flex flex-nowrap gap-[10px]">
+									<Icon name="check" height={16} width={16} className="relative top-1" />
+									<span>Access to all data (unlocks, active users, token liq...)</span>
+								</li>
+								<li className="px-[26px] flex flex-col gap-1">
+									<span>1000 requests/minute</span>
+									<ProgressBar pct={100} />
+								</li>
+								<li className="px-[26px] flex flex-col gap-1">
+									<span>200k calls/month</span>
+									<ProgressBar pct={20} />
+								</li>
+								<li className="px-[26px] flex flex-col gap-1">Priority support</li>
+							</ul>
+							{/* <SignInWithGithub /> */}
+						</div>
+					</div>
+				)}
+				<div className="flex items-center justify-center flex-nowrap gap-2 lg:hidden mb-3">
+					<button
+						className="h-3 w-3 bg-[#39393E] data-[active=true]:bg-[#5c5cf9] rounded-full flex-shrink-0"
+						data-active={activePriceCard === 1}
+						disabled
+					>
+						<span className="sr-only">go to pricing type 1</span>
+					</button>
+					<button
+						className="h-3 w-3 bg-[#39393E] data-[active=true]:bg-[#5c5cf9] rounded-full flex-shrink-0"
+						data-active={activePriceCard === 2}
+						disabled
+					>
+						<span className="sr-only">go to pricing type 2</span>
+					</button>
+					<button
+						className="h-3 w-3 bg-[#39393E] data-[active=true]:bg-[#5c5cf9] rounded-full flex-shrink-0"
+						data-active={activePriceCard === 3}
+						disabled
+					>
+						<span className="sr-only">go to pricing type 3</span>
+					</button>
+				</div>
+			</div>
+		</>
+	)
+}
+
+const ProgressBar = ({ pct }: { pct: number }) => {
+	return (
+		<div
+			className="h-1 rounded-full w-full"
+			style={{
+				background: `linear-gradient(90deg, #5c5cf9 ${pct}%, #696a6d ${pct}%)`
+			}}
+		/>
+	)
+}
+
+const eligibleRepos = [
+	'defillama-app',
+	'defillama-server',
+	'dimension-adapters',
+	'yield-server',
+	'bridges-server',
+	'peggedassets-server',
+	'emissions-adapters',
+	'DefiLlama-Adapters'
+]
---
+++ src/containers/Subscribe/queries.tsx
@@ -0,0 +1,324 @@
+import { useEffect, useSyncExternalStore } from 'react'
+import { useMutation, useQuery } from '@tanstack/react-query'
+import { useAccount } from 'wagmi'
+import { optimism } from 'viem/chains'
+import { signMessage, verifyMessage } from 'wagmi/actions'
+import { config } from '~/layout/WalletProvider'
+import { SERVER_API } from '../ProApi/lib/constants'
+import { useRouter } from 'next/router'
+
+export interface IGithubAuthData {
+	apiKey: string | null
+	isContributor: boolean
+	login: string
+	token: string | null
+}
+
+async function exchangeCodeForAccessToken({ code, accessToken }: { code: string | null; accessToken: string | null }) {
+	if (!accessToken && !code) return null
+
+	try {
+		const response = await fetch(`${SERVER_API}/github-sign-in`, {
+			method: 'POST',
+			body: JSON.stringify({
+				code: accessToken ? null : code,
+				accessToken
+			})
+		}).then(async (r) => {
+			if (!r.ok) {
+				throw new Error('Failed to fetch')
+			}
+			const data = await r.json()
+			return data
+		})
+
+		if (response.token) {
+			localStorage.setItem('gh_authtoken', response.token)
+			window.dispatchEvent(new Event('storage'))
+		}
+
+		return response
+	} catch (error) {
+		const msg = `[CODE&ACCESS_TOKEN]: ${error instanceof Error ? error.message : 'Failed to fetch'}`
+		console.error(msg)
+		throw new Error(msg)
+	}
+}
+
+function subscribe(callback: () => void) {
+	window.addEventListener('storage', callback)
+
+	return () => {
+		window.removeEventListener('storage', callback)
+	}
+}
+
+function getGithubAuthToken() {
+	return localStorage.getItem('gh_authtoken') || null
+}
+
+export const useGithubAuth = () => {
+	const router = useRouter()
+	const code = typeof router?.query?.code === 'string' ? router.query.code : null
+
+	const token = useSyncExternalStore(subscribe, getGithubAuthToken, () => null)
+
+	useEffect(() => {
+		if (token && code) {
+			router.push(
+				{
+					pathname: router.pathname,
+					query: {}
+				},
+				undefined
+			)
+		}
+	}, [code, token, router])
+
+	return useQuery<IGithubAuthData | null>({
+		queryKey: ['github-auth', code, token],
+		queryFn: () => exchangeCodeForAccessToken({ code, accessToken: token })
+	})
+}
+
+async function getCreditsUsage({ apiKey }: { apiKey?: string | null }) {
+	if (!apiKey) return null
+	try {
+		const data = await fetch(`https://pro-api.llama.fi/usage/${apiKey}`).then(async (r) => {
+			if (!r.ok) {
+				throw new Error('Failed to fetch')
+			}
+			const data = await r.json()
+			return data
+		})
+		return data
+	} catch (error) {
+		const msg = `[CREDITS_USAGE]: ${error instanceof Error ? error.message : 'Failed to fetch'}`
+		console.error(msg)
+		throw new Error(msg)
+	}
+}
+
+export const useGetCreditsUsage = ({ apiKey }: { apiKey?: string | null }) => {
+	return useQuery<{ creditsLeft: number } | null>({
+		queryKey: ['credits-usage', apiKey],
+		queryFn: () => getCreditsUsage({ apiKey })
+	})
+}
+
+export function logoutProSubscription({ address }: { address?: string | null }) {
+	if (address) {
+		window.localStorage.removeItem(`auth_token_${address.toLowerCase()}`)
+		window.dispatchEvent(new Event('storage'))
+	}
+}
+
+function getProAuthToken(address?: string | null) {
+	if (!address) return null
+	return localStorage.getItem(`auth_token_${address.toLowerCase()}`) || null
+}
+
+async function signAndGetAuthToken({ address }: { address?: string | null }) {
+	try {
+		if (!address) {
+			throw new Error('Invalid arguments')
+		}
+		const host = window.location.origin
+
+		const siweMessage = getSIWEMessage({
+			domain: host,
+			address,
+			statement: `Sign in to ${host} to get API Key`,
+			uri: host,
+			version: '1',
+			chainId: optimism.id,
+			nonce: `${Math.floor(Math.random() * 64)}`
+		})
+
+		const data = await signMessage(config, { message: siweMessage as string })
+		const isVerified = await verifyMessage(config, {
+			address: address as `0x${string}`,
+			message: siweMessage,
+			signature: data
+		})
+
+		if (!data) {
+			throw new Error('Failed to generate signature')
+		}
+
+		if (!isVerified) {
+			throw new Error('Failed to verify signature')
+		}
+
+		localStorage.setItem(`signature_${address}_api`, JSON.stringify({ signature: data }))
+		window.dispatchEvent(new Event('storage'))
+
+		const verifyRes = await fetch(`${SERVER_API}/sign-in`, {
+			method: 'POST',
+			headers: {
+				'Content-Type': 'application/json'
+			},
+			body: JSON.stringify({ message: siweMessage, signature: data })
+		}).then((r) => r.json())
+
+		if (!verifyRes.key) {
+			throw new Error(verifyRes?.message)
+		}
+
+		localStorage.setItem(`auth_token_${address.toLowerCase()}`, verifyRes.key)
+		window.dispatchEvent(new Event('storage'))
+
+		return null
+	} catch (error: any) {
+		const msg = `[SIGN]: ${error instanceof Error ? error.message : 'Failed to sign'}`
+		console.error(msg)
+		throw new Error(msg)
+	}
+}
+
+export function useSignInWithEthereum() {
+	const wallet = useAccount()
+
+	return useMutation({
+		mutationFn: () =>
+			signAndGetAuthToken({
+				address: wallet.address
+			})
+	})
+}
+
+const getSIWEMessage = ({
+	domain,
+	address,
+	statement,
+	uri,
+	version,
+	chainId,
+	nonce
+}: {
+	domain: string
+	address: string
+	statement: string
+	uri: string
+	version: string
+	chainId: number
+	nonce: string
+}) => {
+	const header = `${domain} wants you to sign in with your Ethereum account:`
+	const uriField = `URI: ${uri}`
+	let prefix = [header, address].join('\n')
+	const versionField = `Version: ${version}`
+	const chainField = `Chain ID: ` + chainId || '1'
+	const nonceField = `Nonce: ${nonce}`
+	const suffixArray = [uriField, versionField, chainField, nonceField]
+	suffixArray.push(`Issued At: ${new Date().toISOString()}`)
+
+	const suffix = suffixArray.join('\n')
+	prefix = [prefix, statement].join('\n\n')
+	if (statement) {
+		prefix += '\n'
+	}
+	return [prefix, suffix].join('\n')
+}
+
+const generateNewApiKey = async ({ authToken }: { authToken?: string | null }) => {
+	try {
+		if (!authToken) {
+			throw new Error('Not Authorized')
+		}
+
+		const newApiKey = await fetch(`${SERVER_API}/auth/generate`, {
+			method: 'POST',
+			headers: {
+				Authorization: authToken
+			}
+		}).then((r) => r.json())
+
+		if (!newApiKey) {
+			throw new Error('Failed to generate new api key')
+		}
+
+		return newApiKey?.apiKey ?? null
+	} catch (error: any) {
+		throw new Error(error.message)
+	} finally {
+	}
+}
+
+export function useGenerateNewApiKey() {
+	return useMutation({
+		mutationFn: generateNewApiKey
+	})
+}
+
+async function getCurrentKey(authToken?: string | null) {
+	try {
+		if (!authToken) return null
+
+		const currentToken = await fetch(`${SERVER_API}/auth/api-key`, {
+			method: 'GET',
+			headers: {
+				Authorization: authToken
+			}
+		}).then((r) => r.json())
+
+		if (currentToken.message === 'bad signature') {
+			for (let i = 0; i < localStorage.length; i++) {
+				const key = localStorage.key(i)
+				if (key?.startsWith('auth_token')) {
+					localStorage.removeItem(key)
+				}
+			}
+		} else {
+			if (currentToken?.apiKey === null) {
+				currentToken.apiKey = await generateNewApiKey({ authToken })
+			}
+		}
+
+		return currentToken?.apiKey ? currentToken : null
+	} catch (error: any) {
+		throw new Error(error.message ?? 'Failed to fetch current api key')
+	}
+}
+
+export const useGetCurrentKey = () => {
+	const { address } = useAccount()
+
+	const authToken = useSyncExternalStore(
+		subscribe,
+		() => getProAuthToken(address),
+		() => null
+	)
+
+	return useQuery<{ email: string; apiKey: string } | null>({
+		queryKey: ['currentKey', authToken],
+		queryFn: () => getCurrentKey(authToken),
+		retry: 1
+	})
+}
+
+async function isSubscribed(userAddress?: string) {
+	if (!userAddress) return false
+	const { subscribed } = await fetch(`${SERVER_API}/auth/subscribed/${userAddress.toLowerCase()}`, {
+		headers: {
+			'Content-Type': 'application/json'
+		}
+	}).then(async (r) => {
+		if (!r.ok) {
+			throw new Error('Failed to fetch')
+		}
+		const data = await r.json()
+		return data
+	})
+
+	return subscribed
+}
+
+export const useIsSubscribed = () => {
+	const wallet = useAccount()
+
+	return useQuery({
+		queryKey: ['isSubscribed', wallet.address],
+		queryFn: () => isSubscribed(wallet.address)
+	})
+}
