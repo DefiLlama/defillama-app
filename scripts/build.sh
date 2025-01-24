913da69a228dab7c6c7ee19cabb3e98d68fbdcdf
0xngmi <80541789+0xngmi@users.noreply.github.com>
2025-01-24 22:15:46 +0000

    remove logs

--- scripts/build.sh
+++ scripts/build.sh
@@ -55,8 +55,6 @@ echo ""
 rclone --config scripts/rclone.conf copy ./.next/static artifacts:defillama-app-artifacts
 rclone --config scripts/rclone.conf copy artifacts:defillama-app-artifacts ./.next/static
 
-ls -R ./.next/static
-
 if [ -z "$NOT_VERCEL" ]; then
   echo "NOT_VERCEL is not set, skipping discord notification"
   exit $BUILD_STATUS
