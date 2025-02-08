0a9d36b4889f303ea7056df49d6d9aefa991bb52
mintdart <96025197+mintdart@users.noreply.github.com>
2025-02-08 02:45:16 -0600

    add page loading progress bar

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,12 +1,49 @@
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 import { ReactQueryDevtools } from '@tanstack/react-query-devtools'
+import NProgress from 'nprogress'
 import { AppContext } from '~/contexts'
 import { useAnalytics } from '~/hooks/useAnalytics'
 import '../tailwind.css'
+import '../nprogress.css'
+import { useEffect } from 'react'
+import { useRouter } from 'next/router'
+
+NProgress.configure({ showSpinner: false })
 
 const client = new QueryClient()
+
 function App({ Component, pageProps }) {
 	useAnalytics()
+
+	const router = useRouter()
+
+	useEffect(() => {
+		const handleRouteChange = () => {
+			NProgress.start()
+		}
+
+		router.events.on('routeChangeStart', handleRouteChange)
+
+		// If the component is unmounted, unsubscribe
+		// from the event with the `off` method:
+		return () => {
+			router.events.off('routeChangeStart', handleRouteChange)
+		}
+	}, [router])
+
+	useEffect(() => {
+		const handleRouteChange = () => {
+			NProgress.done()
+		}
+
+		router.events.on('routeChangeComplete', handleRouteChange)
+
+		// If the component is unmounted, unsubscribe
+		// from the event with the `off` method:
+		return () => {
+			router.events.off('routeChangeComplete', handleRouteChange)
+		}
+	}, [router])
 
 	return (
 		<QueryClientProvider client={client}>
