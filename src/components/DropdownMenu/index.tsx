025263bc8fd261100aaa7906b98d3d71a8d58e9b
mintdart <96025197+mintdart@users.noreply.github.com>
2025-04-02 02:19:02 -0700

    update styles

--- src/components/DropdownMenu/index.tsx
+++ src/components/DropdownMenu/index.tsx
@@ -1,6 +1,7 @@
 import Link from 'next/link'
-import { Menu as AriakitMenu, MenuButton, MenuItem, MenuButtonArrow, useMenuState } from 'ariakit/menu'
+import * as Ariakit from '@ariakit/react'
 import { transparentize } from 'polished'
+import { useMemo } from 'react'
 
 interface IMenuProps {
 	options: string[] | string
@@ -12,61 +13,59 @@ interface IMenuProps {
 }
 
 export function Menu({ options, name, color, isExternal, onItemClick, variant = 'primary', ...props }: IMenuProps) {
-	const _options = typeof options === 'string' ? [options] : options
-
-	const menu = useMenuState({ gutter: 8 })
+	const { _options, style } = useMemo(() => {
+		return {
+			_options: typeof options === 'string' ? [options] : options,
+			style: color
+				? ({ '--btn2-bg': transparentize(0.9, color), '--btn2-hover-bg': transparentize(0.8, color) } as any)
+				: {}
+		}
+	}, [options, color])
 
 	return (
-		<>
-			<MenuButton
+		<Ariakit.MenuProvider>
+			<Ariakit.MenuButton
 				{...props}
-				style={
-					color
-						? ({ '--btn2-bg': transparentize(0.9, color), '--btn2-hover-bg': transparentize(0.8, color) } as any)
-						: {}
-				}
-				state={menu}
+				style={style}
 				className="bg-[var(--btn2-bg)] hover:bg-[var(--btn2-hover-bg)] focus-visible:bg-[var(--btn2-hover-bg)] flex items-center justify-between gap-2 py-2 px-3 rounded-xl cursor-pointer text-[var(--text1)] flex-nowrap relative max-w-fit"
 			>
 				{name}
-				<MenuButtonArrow />
-			</MenuButton>
-			{menu.mounted ? (
-				<AriakitMenu
-					state={menu}
-					className="flex flex-col bg-[var(--bg1)] rounded-md z-10 overflow-auto overscroll-contain min-w-[180px] max-h-[60vh] border border-[hsl(204,20%,88%)] dark:border-[hsl(204,3%,32%)] max-sm:drawer"
-				>
-					{_options.map((value, i) => {
-						return onItemClick ? (
-							<MenuItem
-								as="button"
-								key={value + i}
-								onClick={() => onItemClick(value)}
+				<Ariakit.MenuButtonArrow />
+			</Ariakit.MenuButton>
+			<Ariakit.Menu
+				unmountOnHide
+				gutter={8}
+				className="flex flex-col bg-[var(--bg1)] rounded-md z-10 overflow-auto overscroll-contain min-w-[180px] max-h-[60vh] border border-[hsl(204,20%,88%)] dark:border-[hsl(204,3%,32%)] max-sm:drawer"
+			>
+				{_options.map((value, i) => {
+					return onItemClick ? (
+						<Ariakit.MenuItem
+							key={value + i}
+							onClick={() => onItemClick(value)}
+							className="flex items-center justify-between gap-4 py-2 px-3 flex-shrink-0 hover:bg-[var(--primary1-hover)] focus-visible:bg-[var(--primary1-hover)] cursor-pointer first-of-type:rounded-t-md last-of-type:rounded-b-md border-b border-black/10 dark:border-white/10"
+						>
+							{value}
+						</Ariakit.MenuItem>
+					) : isExternal ? (
+						<Ariakit.MenuItem
+							render={<a href={value} target="_blank" rel="noopener noreferrer" />}
+							key={value + i}
+							className="flex items-center justify-between gap-4 py-2 px-3 flex-shrink-0 hover:bg-[var(--primary1-hover)] focus-visible:bg-[var(--primary1-hover)] cursor-pointer first-of-type:rounded-t-md last-of-type:rounded-b-md border-b border-black/10 dark:border-white/10"
+						>
+							{value}
+						</Ariakit.MenuItem>
+					) : (
+						<Link href={value} key={value + i} passHref>
+							<Ariakit.MenuItem
+								render={<a />}
 								className="flex items-center justify-between gap-4 py-2 px-3 flex-shrink-0 hover:bg-[var(--primary1-hover)] focus-visible:bg-[var(--primary1-hover)] cursor-pointer first-of-type:rounded-t-md last-of-type:rounded-b-md border-b border-black/10 dark:border-white/10"
 							>
 								{value}
-							</MenuItem>
-						) : isExternal ? (
-							<MenuItem
-								as="a"
-								href={value}
-								target="_blank"
-								rel="noopener noreferrer"
-								key={value + i}
-								className="flex items-center justify-between gap-4 py-2 px-3 flex-shrink-0 hover:bg-[var(--primary1-hover)] focus-visible:bg-[var(--primary1-hover)] cursor-pointer first-of-type:rounded-t-md last-of-type:rounded-b-md border-b border-black/10 dark:border-white/10"
-							>
-								{value}
-							</MenuItem>
-						) : (
-							<Link href={value} key={value + i} passHref>
-								<MenuItem className="flex items-center justify-between gap-4 py-2 px-3 flex-shrink-0 hover:bg-[var(--primary1-hover)] focus-visible:bg-[var(--primary1-hover)] cursor-pointer first-of-type:rounded-t-md last-of-type:rounded-b-md border-b border-black/10 dark:border-white/10">
-									{value}
-								</MenuItem>
-							</Link>
-						)
-					})}
-				</AriakitMenu>
-			) : null}
-		</>
+							</Ariakit.MenuItem>
+						</Link>
+					)
+				})}
+			</Ariakit.Menu>
+		</Ariakit.MenuProvider>
 	)
 }
--- src/components/ECharts/shared.tsx
+++ src/components/ECharts/shared.tsx
@@ -1,15 +1,13 @@
 import * as React from 'react'
-import { useSelectState, SelectArrow, SelectPopover, Select, SelectItem } from 'ariakit/select'
-import { Checkbox } from '~/components/Checkbox'
-import { useSetPopoverStyles } from '~/components/Popover/utils'
-import { Combobox, ComboboxList, useComboboxState } from 'ariakit/combobox'
+import * as Ariakit from '@ariakit/react'
 import { useRouter } from 'next/router'
+import { matchSorter } from 'match-sorter'
 
-function renderValue(value: Array<string>, title: string) {
+function renderValue(value: Array<string> | string, title: string) {
 	return (
 		<span className="flex items-center gap-1">
 			<span className="text-[10px] rounded-full min-w-4 flex items-center justify-center bg-[var(--bg4)] px-[1px]">
-				{value?.length ?? 0}
+				{typeof value === 'string' ? 1 : value?.length ?? 0}
 			</span>
 			<span>{title}</span>
 		</span>
@@ -26,87 +24,82 @@ interface ISelectLegendMultipleProps {
 export function SelectLegendMultiple({ allOptions, options, setOptions, title, ...props }: ISelectLegendMultipleProps) {
 	const router = useRouter()
 
-	const [isLarge, renderCallback] = useSetPopoverStyles()
+	const [searchValue, setSearchValue] = React.useState('')
 
-	const combobox = useComboboxState({ list: allOptions })
-	const { value, setValue, ...selectProps } = combobox
-
-	const onChange = (values) => {
-		setOptions(values)
-	}
-
-	const select = useSelectState({
-		...selectProps,
-		value: options,
-		setValue: onChange,
-		gutter: 6,
-		animated: isLarge ? false : true,
-		renderCallback
-	})
-
-	// Resets combobox value when popover is collapsed
-	if (!select.mounted && combobox.value) {
-		combobox.setValue('')
-	}
-
-	const focusItemRef = React.useRef(null)
+	const matches = React.useMemo(() => {
+		return matchSorter(allOptions, searchValue, {
+			baseSort: (a, b) => (a.index < b.index ? -1 : 1)
+		})
+	}, [allOptions, searchValue])
 
 	return (
-		<>
-			<Select
-				state={select}
-				{...props}
-				className="bg-[var(--btn-bg)] hover:bg-[var(--btn-hover-bg)] focus-visible:bg-[var(--btn-hover-bg)] flex items-center gap-2 py-2 px-3 text-xs rounded-md cursor-pointer text-[var(--text1)] flex-nowrap ml-auto"
+		<Ariakit.ComboboxProvider
+			resetValueOnHide
+			setValue={(value) => {
+				React.startTransition(() => {
+					setSearchValue(value)
+				})
+			}}
+		>
+			<Ariakit.SelectProvider
+				value={options}
+				setValue={(values) => {
+					setOptions(values)
+				}}
 			>
-				{renderValue(select.value, title)}
-				<SelectArrow />
-			</Select>
-			{select.mounted ? (
-				<SelectPopover
-					state={select}
-					composite={false}
-					initialFocusRef={focusItemRef}
+				<Ariakit.Select
+					{...props}
+					className="bg-[var(--btn-bg)] hover:bg-[var(--btn-hover-bg)] focus-visible:bg-[var(--btn-hover-bg)] flex items-center gap-2 py-2 px-3 text-xs rounded-md cursor-pointer text-[var(--text1)] flex-nowrap ml-auto"
+				>
+					{renderValue(options, title)}
+					<Ariakit.SelectArrow />
+				</Ariakit.Select>
+				<Ariakit.SelectPopover
+					unmountOnHide
+					hideOnInteractOutside
+					gutter={6}
+					wrapperProps={{
+						className: 'max-sm:!fixed max-sm:!bottom-0 max-sm:!top-[unset] max-sm:!transform-none max-sm:!w-full'
+					}}
 					className="flex flex-col bg-[var(--bg1)] rounded-md z-10 overflow-auto overscroll-contain min-w-[180px] max-h-[60vh] border border-[hsl(204,20%,88%)] dark:border-[hsl(204,3%,32%)] max-sm:drawer"
 				>
-					<Combobox
-						state={combobox}
+					<Ariakit.Combobox
 						placeholder="Search..."
 						autoFocus
 						className="bg-white dark:bg-black rounded-md py-2 px-3 m-3 mb-0"
 					/>
 
-					{combobox.matches.length > 0 ? (
+					{matches.length > 0 ? (
 						<>
 							<span className="sticky z-[1] top-0 flex flex-wrap justify-between gap-1 bg-[var(--bg1)] text-[var(--link)] text-xs border-b border-black/10 dark:border-white/10">
-								<button onClick={() => select.setValue([])} className="p-3">
+								<button onClick={() => setOptions([])} className="p-3">
 									Clear
 								</button>
 								{router.pathname !== '/comparison' && (
-									<button onClick={() => select.setValue(allOptions)} className="p-3">
+									<button onClick={() => setOptions(allOptions)} className="p-3">
 										Toggle all
 									</button>
 								)}
 							</span>
-							<ComboboxList state={combobox} className="flex flex-col overflow-auto overscroll-contain text-xs">
-								{combobox.matches.map((value, i) => (
-									<SelectItem
+							<Ariakit.ComboboxList>
+								{matches.map((value) => (
+									<Ariakit.SelectItem
+										key={`${title}-${value}`}
 										value={value}
-										key={value + i}
-										ref={i === 0 && options.length === allOptions.length ? focusItemRef : null}
-										focusOnHover
 										className="flex items-center justify-between gap-4 py-2 px-3 flex-shrink-0 hover:bg-[var(--primary1-hover)] focus-visible:bg-[var(--primary1-hover)] cursor-pointer last-of-type:rounded-b-md border-b border-black/10 dark:border-white/10"
+										render={<Ariakit.ComboboxItem />}
 									>
 										<span>{value}</span>
-										<Checkbox checked={select.value.includes(value) ? true : false} />
-									</SelectItem>
+										<Ariakit.SelectItemCheck className="h-3 w-3 flex items-center justify-center rounded-sm flex-shrink-0 border border-[#28a2b5]" />
+									</Ariakit.SelectItem>
 								))}
-							</ComboboxList>
+							</Ariakit.ComboboxList>
 						</>
 					) : (
 						<p className="text-[var(--text1)] py-6 px-3 text-center">No results found</p>
 					)}
-				</SelectPopover>
-			) : null}
-		</>
+				</Ariakit.SelectPopover>
+			</Ariakit.SelectProvider>
+		</Ariakit.ComboboxProvider>
 	)
 }
