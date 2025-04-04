645c69dbde4886f3ed4b43448e539e39fb203801
mintdart <96025197+mintdart@users.noreply.github.com>
2025-04-04 04:29:31 -0700

    improve build times

--- src/pages/comparison.tsx
+++ src/pages/comparison.tsx
@@ -13,83 +13,79 @@ import { useDefiManager } from '~/contexts/LocalStorage'
 import { formatProtocolsTvlChartData } from '~/components/ECharts/ProtocolChart/useFetchAndFormatChartData'
 import { fuseProtocolData } from '~/api/categories/protocols'
 import { withPerformanceLogging } from '~/utils/perf'
-import { useQuery } from '@tanstack/react-query'
+import { useQueries } from '@tanstack/react-query'
 import { SelectWithCombobox } from '~/components/SelectWithCombobox'
 
 const AreaChart = dynamic(() => import('~/components/ECharts/AreaChart'), {
 	ssr: false
 }) as React.FC<IChartProps>
 
-const protocolColor = async (name: string) => {
-	const color = await getColor(tokenIconPaletteUrl(name))
-	return { [name]: color }
-}
-
 export const getStaticProps = withPerformanceLogging('comparison', async () => {
 	const { protocols } = await getSimpleProtocolsPageData(['name', 'logo'])
 
-	const stackColors = await Promise.allSettled(protocols.map((p) => protocolColor(p.name)))
-
-	let colors = {}
-
-	stackColors.forEach((p) => {
-		if (p.status === 'fulfilled' && p.value) {
-			colors = { ...colors, ...p.value }
-		}
-	})
-
 	return {
 		props: {
-			protocols: protocols.map((p) => p.name),
-			stackColors: colors
+			protocols: protocols.map((p) => p.name)
 		},
 		revalidate: maxAgeForNext([22])
 	}
 })
 
-const fetchProtocols = async (selectedProtocols: Array<string> | null) => {
-	if (!selectedProtocols) return null
+const fetchProtocol = async (selectedProtocol: string | null) => {
+	if (!selectedProtocol) return null
 
 	try {
-		const data = await Promise.all(
-			selectedProtocols.map((p) => fetch(`${PROTOCOL_API}/${slug(p)}`).then((res) => res.json()))
-		)
+		const data = await Promise.allSettled([
+			fetch(`${PROTOCOL_API}/${slug(selectedProtocol)}`).then((res) => res.json()),
+			getColor(tokenIconPaletteUrl(selectedProtocol))
+		])
 
-		return data
+		return {
+			protocolData: data[0].status === 'fulfilled' ? data[0].value : null,
+			color: data[1].status === 'fulfilled' ? data[1].value : null
+		}
 	} catch (error) {
 		throw new Error(error instanceof Error ? error.message : 'Failed to fetch')
 	}
 }
 
-export default function CompareProtocolsTvls({
-	protocols,
-	stackColors
-}: {
-	protocols: Array<string>
-	stackColors: { [stack: string]: string }
-}) {
+export default function CompareProtocolsTvls({ protocols }: { protocols: Array<string> }) {
 	const router = useRouter()
 	const [extraTvlEnabled] = useDefiManager()
 
 	const { protocol } = router.query
 	const selectedProtocols = protocol ? (typeof protocol === 'string' ? [protocol] : [...protocol]) : null
 
-	const { data, isLoading } = useQuery({
-		queryKey: ['compare-protocols', selectedProtocols?.join('') ?? ''],
-		queryFn: () => fetchProtocols(selectedProtocols),
-		staleTime: 60 * 60 * 1000
+	const results = useQueries({
+		queries:
+			selectedProtocols?.map((protocol) => ({
+				queryKey: ['protocol-to-compare', protocol],
+				queryFn: () => fetchProtocol(protocol),
+				staleTime: 60 * 60 * 1000,
+				refetchOnWindowFocus: false
+			})) ?? []
 	})
 
-	const chartData = React.useMemo(() => {
-		const formattedData =
-			data?.map((x) => {
-				const { historicalChainTvls } = fuseProtocolData(x)
+	const isLoading = results.some((r) => r.isLoading)
 
-				return {
-					protocolChartData: formatProtocolsTvlChartData({ historicalChainTvls, extraTvlEnabled }),
-					protocolName: x.name
-				}
-			}) ?? []
+	const { chartData, stackColors } = React.useMemo(() => {
+		const stackColors = {}
+
+		const formattedData =
+			results
+				.filter((r) => r.data)
+				.map((res: any) => {
+					const { historicalChainTvls } = fuseProtocolData(res.data.protocolData)
+
+					if (res.data.color) {
+						stackColors[res.data.protocolData.name] = res.data.color
+					}
+
+					return {
+						protocolChartData: formatProtocolsTvlChartData({ historicalChainTvls, extraTvlEnabled }),
+						protocolName: res.data.protocolData.name
+					}
+				}) ?? []
 
 		const chartData = {}
 
@@ -120,8 +116,8 @@ export default function CompareProtocolsTvls({
 			})
 		})
 
-		return Object.keys(chartData).map((date) => ({ date, ...chartData[date] }))
-	}, [data, extraTvlEnabled])
+		return { chartData: Object.keys(chartData).map((date) => ({ date, ...chartData[date] })), stackColors }
+	}, [results, extraTvlEnabled])
 
 	const setSelectedProtocols = (values) => {
 		router.push(
@@ -137,8 +133,6 @@ export default function CompareProtocolsTvls({
 			}
 		)
 	}
-
-	const colors = Object.fromEntries(selectedProtocols?.map((p) => [p, stackColors[p]]) ?? [])
 
 	return (
 		<Layout title={`Compare Protocols TVLs - DefiLlama`} defaultSEO>
@@ -159,18 +153,17 @@ export default function CompareProtocolsTvls({
 					}}
 				/>
 				<div className="relative col-span-2 p-4 shadow rounded-xl">
-					<AreaChart
-						chartData={chartData}
-						title="Protocols"
-						valueSymbol="$"
-						stacks={selectedProtocols}
-						stackColors={colors}
-						hideDefaultLegend
-					/>
-					{isLoading && (
-						<p className="text-[var(--text1)] text-center absolute top-0 right-0 bottom-0 left-0 grid place-items-center">
-							Loading...
-						</p>
+					{isLoading ? (
+						<p className="text-[var(--text1)] text-center mt-[20vh]">Loading...</p>
+					) : (
+						<AreaChart
+							chartData={chartData}
+							title="Protocols"
+							valueSymbol="$"
+							stacks={selectedProtocols}
+							stackColors={stackColors}
+							hideDefaultLegend
+						/>
 					)}
 				</div>
 			</div>
