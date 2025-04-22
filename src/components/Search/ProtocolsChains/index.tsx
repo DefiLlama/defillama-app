ff1ff01705bb33949c976158d44716c6754d97db
mintdart <96025197+mintdart@users.noreply.github.com>
2025-04-22 05:43:11 -0700

    update styles

--- src/components/Search/ProtocolsChains/index.tsx
+++ src/components/Search/ProtocolsChains/index.tsx
@@ -29,7 +29,7 @@ export const ProtocolsChainsSearch = memo(function ProtocolsChainsSearch({
 	}
 
 	return (
-		<span className="hidden lg:block min-h-[46px]">
+		<span className="hidden lg:block min-h-[44px]">
 			<SearchV2 indexName="protocols">
 				<Search {...props} />
 			</SearchV2>
