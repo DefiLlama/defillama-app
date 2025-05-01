f4af4f7d2d6ae464a5b3a8feee55ec3e1094a9da
Reynardo Etenia Wongso <reynardoew@gmail.com>
2025-05-01 23:08:18 +0700

    indexutils

--- src/utils/index.js
+++ src/utils/index.js
@@ -471,6 +471,54 @@ export function lastDayOfWeek(dateString) {
 	return Math.trunc(Date.UTC(date.getUTCFullYear(), date.getUTCMonth(), lastDayOfWeek) / 1000)
 }
 
+function hexToHSL(hex) {
+	let r = parseInt(hex.slice(1, 3), 16) / 255
+	let g = parseInt(hex.slice(3, 5), 16) / 255
+	let b = parseInt(hex.slice(5, 7), 16) / 255
+
+	const max = Math.max(r, g, b)
+	const min = Math.min(r, g, b)
+	let h,
+		s,
+		l = (max + min) / 2
+
+	if (max === min) {
+		h = s = 0
+	} else {
+		const d = max - min
+		s = l > 0.5 ? d / (2 - max - min) : d / (max + min)
+		switch (max) {
+			case r:
+				h = (g - b) / d + (g < b ? 6 : 0)
+				break
+			case g:
+				h = (b - r) / d + 2
+				break
+			case b:
+				h = (r - g) / d + 4
+				break
+		}
+		h /= 6
+	}
+
+	return { h: h * 360, s: s * 100, l: l * 100 }
+}
+
+function hslToHex(h, s, l) {
+	l /= 100
+	const a = (s * Math.min(l, 1 - l)) / 100
+
+	const f = (n) => {
+		const k = (n + h / 30) % 12
+		const color = l - a * Math.max(Math.min(k - 3, 9 - k, 1), -1)
+		return Math.round(255 * color)
+			.toString(16)
+			.padStart(2, '0')
+	}
+
+	return `#${f(0)}${f(8)}${f(4)}`
+}
+
 export const chunks = (array, size) => {
 	const result = []
 	for (let i = 0; i < array.length; i += size) {
