f7237053b9fd1aca96f2ee95877467478a5a10ae
mintdart <96025197+mintdart@users.noreply.github.com>
2025-07-03 04:17:19 +0900

    fix build

--- src/components/Search/InstantSearch.tsx
+++ src/components/Search/InstantSearch.tsx
@@ -1,14 +1,24 @@
 import { instantMeiliSearch } from '@meilisearch/instant-meilisearch'
 import { InstantSearch } from 'react-instantsearch'
+import { ReactNode } from 'react'
 
 const { searchClient } = instantMeiliSearch(
 	'https://search.defillama.com',
 	'0050e4b518781e324a259db278687ec0031b9601c1c6a87aa7174c13ecdbd057'
 )
 
-export const SearchV2 = ({ indexName, children }) => {
+interface SearchV2Props {
+	indexName: string
+	children: ReactNode
+}
+
+export const SearchV2 = ({ indexName, children }: SearchV2Props) => {
 	return (
-		<InstantSearch indexName={indexName} searchClient={searchClient} future={{ preserveSharedStateOnUnmount: true }}>
+		<InstantSearch
+			indexName={indexName}
+			searchClient={searchClient as any}
+			future={{ preserveSharedStateOnUnmount: true }}
+		>
 			{children}
 		</InstantSearch>
 	)
