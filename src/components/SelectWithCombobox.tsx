070436cc8b440765e7f6958ef05128aa9ec68306
Hilary Bidlake <47711475+automaticdog@users.noreply.github.com>
2025-07-14 19:16:09 +0100

    Revert "Fix issue with bottom edge of Combobox search box getting cut off"
    This reverts commit 22bc4a842be296700a83b7a915123a31d1abdd14.
    
    Revert fix for bottom edge of search box getting cut off

--- src/components/SelectWithCombobox.tsx
+++ src/components/SelectWithCombobox.tsx
@@ -184,7 +184,7 @@ export function SelectWithCombobox({
 					<Ariakit.Combobox
 						placeholder="Search..."
 						autoFocus
-						className="bg-white dark:bg-black rounded-md text-base py-1 px-3 m-3 mb-2"
+						className="bg-white dark:bg-black rounded-md text-base py-1 px-3 m-3 mb-0"
 					/>
 
 					{matches.length > 0 ? (
