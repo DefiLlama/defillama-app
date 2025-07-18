94ec364cc72ca5f96d1402e2b8c49f4b9e648a2b
Define101 <Lovelovenow@protonmail.com>
2025-07-18 10:05:51 +0100

    warning pumpBTC

--- src/containers/ProtocolOverview/Layout.tsx
+++ src/containers/ProtocolOverview/Layout.tsx
@@ -9,6 +9,7 @@ import * as Ariakit from '@ariakit/react'
 import { TokenLogo } from '~/components/TokenLogo'
 import { oldBlue } from '~/constants/colors'
 import { defaultProtocolPageStyles } from './Chart/constants'
+import { Announcement } from '~/components/Announcement'
 
 export function ProtocolOverviewLayout({
 	children,
@@ -111,6 +112,12 @@ export function ProtocolOverviewLayout({
 					This protocol includes unproductive positions that may contribute to inflated metrics. Be safe
 				</p>
 			)}
+
+			{name === 'PumpBTC' && (
+  <Announcement warning={true} notCancellable={true}>
+    This protocol has been identified using unbacked assets to artificially inflate its own and other protocols TVL metrics. Proceed with caution.
+  </Announcement>
+)}
 
 			<div className="flex flex-col gap-2 isolate">
 				<div className="w-full flex overflow-x-auto text-xs font-medium">
