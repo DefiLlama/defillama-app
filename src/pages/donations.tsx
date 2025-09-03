8433b3444dc4ebce4a7a6e84b58882f5ebf33edc
mintdart <96025197+mintdart@users.noreply.github.com>
2025-09-04 02:25:02 +0900

    fix affiliate links

--- src/pages/donations.tsx
+++ src/pages/donations.tsx
@@ -1,29 +1,62 @@
 import * as React from 'react'
 import { maxAgeForNext } from '~/api'
-import { getSimpleProtocolsPageData } from '~/api/categories/protocols'
+import { PROTOCOLS_API } from '~/constants'
 import Layout from '~/layout'
 import { tokenIconUrl } from '~/utils'
+import { fetchJson } from '~/utils/async'
 import { withPerformanceLogging } from '~/utils/perf'
 
 export const getStaticProps = withPerformanceLogging('donations', async () => {
-	const { protocols } = await getSimpleProtocolsPageData(['name', 'logo', 'url', 'referralUrl'])
+	const { protocols, parentProtocols } = await fetchJson(PROTOCOLS_API)
+
+	const protocolsWithReferralurl: Record<string, { name: string; logo: string; url: string; tvl: number }> = {}
+
+	for (const parent of parentProtocols) {
+		if (parent.referralUrl == null) continue
+		protocolsWithReferralurl[parent.id] = {
+			name: parent.name,
+			logo: tokenIconUrl(parent.name),
+			url: parent.referralUrl,
+			tvl: 0
+		}
+	}
+
+	for (const protocol of protocols) {
+		if (protocolsWithReferralurl[protocol.parentProtocol]) {
+			protocolsWithReferralurl[protocol.parentProtocol].tvl =
+				(protocolsWithReferralurl[protocol.parentProtocol].tvl ?? 0) + (protocol.tvl ?? 0)
+		}
+		if (protocol.referralUrl == null) continue
+
+		if (
+			protocol.parentProtocol
+				? protocolsWithReferralurl[protocol.parentProtocol] &&
+					protocolsWithReferralurl[protocol.parentProtocol].url !== protocol.referralUrl
+				: true
+		) {
+			protocolsWithReferralurl[protocol.defillamaId] = {
+				name: protocol.name,
+				logo: tokenIconUrl(protocol.name),
+				url: protocol.referralUrl,
+				tvl: protocol.tvl ?? 0
+			}
+		}
+	}
+
+	const finalProtocols = []
+	for (const protocol in protocolsWithReferralurl) {
+		finalProtocols.push(protocolsWithReferralurl[protocol])
+	}
+
 	return {
 		props: {
-			protocols: protocols
-				.filter(
-					(p) => p.referralUrl != null && !['Mycelium Perpetual Swaps', 'Hubble Exchange', 'NEX'].includes(p.name)
-				)
-				.map((protocol) => ({
-					name: protocol.name,
-					logo: tokenIconUrl(protocol.name),
-					url: protocol.referralUrl
-				}))
+			protocols: finalProtocols.sort((a, b) => b.tvl - a.tvl)
 		},
 		revalidate: maxAgeForNext([22])
 	}
 })
 
-function PressPage({ protocols }) {
+function DonationsPage({ protocols }) {
 	return (
 		<Layout
 			title="Donations - DefiLlama"
@@ -38,6 +71,16 @@ function PressPage({ protocols }) {
 					DefiLlama is an open-source project that runs no ads and provides all data for free. We have no revenue and
 					are supported by donations.
 				</p>
+			</div>
+			<div className="flex flex-col gap-4 rounded-md bg-(--cards-bg) p-3">
+				<h1 className="text-lg font-semibold">Direct donation</h1>
+				<hr className="border-black/20 dark:border-white/20" />
+				<div className="flex flex-col gap-2">
+					<p>You can send us any token, on any network, to the following address:</p>
+					<ul className="flex flex-col gap-1">
+						<li>0x08a3c2A819E3de7ACa384c798269B3Ce1CD0e437</li>
+					</ul>
+				</div>
 			</div>
 			<div className="flex flex-col gap-4 rounded-md bg-(--cards-bg) p-3">
 				<h1 className="text-lg font-semibold">Affiliate links</h1>
@@ -58,16 +101,6 @@ function PressPage({ protocols }) {
 				</div>
 			</div>
 			<div className="flex flex-col gap-4 rounded-md bg-(--cards-bg) p-3">
-				<h1 className="text-lg font-semibold">Direct donation</h1>
-				<hr className="border-black/20 dark:border-white/20" />
-				<div className="flex flex-col gap-2">
-					<p>You can send us any token, on any network, to the following address:</p>
-					<ul className="flex flex-col gap-1">
-						<li>0x08a3c2A819E3de7ACa384c798269B3Ce1CD0e437</li>
-					</ul>
-				</div>
-			</div>
-			<div className="flex flex-col gap-4 rounded-md bg-(--cards-bg) p-3">
 				<h1 className="text-lg font-semibold">Use of funds</h1>
 				<hr className="border-black/20 dark:border-white/20" />
 				<div className="flex flex-col gap-2">
@@ -82,4 +115,4 @@ function PressPage({ protocols }) {
 	)
 }
 
-export default PressPage
+export default DonationsPage
