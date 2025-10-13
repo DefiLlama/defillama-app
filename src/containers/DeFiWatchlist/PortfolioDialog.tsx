46e511a14140e052fed804bafc3482359cc46bee
mintdart <96025197+mintdart@users.noreply.github.com>
2025-10-13 02:33:16 -0700

    refactor

--- src/containers/DeFiWatchlist/PortfolioDialog.tsx
+++ src/containers/DeFiWatchlist/PortfolioDialog.tsx
@@ -1,8 +1,7 @@
-import { useState } from 'react'
-import { Combobox, ComboboxProvider, Dialog, DialogDescription, DialogHeading } from '@ariakit/react'
+import { Dialog, DialogDismiss, DialogHeading } from '@ariakit/react'
+import { Icon } from '~/components/Icon'
 
 export function PortfolioDialog({ open, setOpen, addPortfolio }) {
-	const [newPortfolio, setNewPortfolio] = useState('')
 	return (
 		<Dialog
 			open={open}
@@ -10,37 +9,36 @@ export function PortfolioDialog({ open, setOpen, addPortfolio }) {
 			modal={true}
 			className="fixed top-[50%] left-[50%] flex h-fit w-8/10 max-w-150 translate-x-[-50%] translate-y-[-50%] flex-col gap-4 rounded-md border border-(--cards-border) bg-(--cards-bg) p-6 sm:w-1/2 lg:w-1/3"
 		>
-			<div className="flex flex-col gap-2">
-				<button
-					className="absolute top-4 right-4 p-1 text-sm text-(--text-secondary) transition-colors hover:text-(--text-primary)"
+			<div className="flex items-center justify-between gap-2">
+				<DialogHeading className="text-lg font-medium">New portfolio</DialogHeading>
+				<DialogDismiss
+					className="-my-2 ml-auto rounded-lg p-2 text-(--text-tertiary) hover:bg-(--divider) hover:text-(--text-primary)"
 					onClick={() => setOpen(false)}
 					aria-label="Close"
 				>
-					x
-				</button>
-				<DialogHeading className="text-lg font-medium">New portfolio</DialogHeading>
-				<DialogDescription className="text-sm text-(--text-secondary)">
-					Enter the name of your new portfolio
-				</DialogDescription>
+					<Icon name="x" height={20} width={20} />
+					<span className="sr-only">Close dialog</span>
+				</DialogDismiss>
 			</div>
-			<ComboboxProvider defaultValue="" value={newPortfolio} resetValueOnHide open={open} setValue={setNewPortfolio}>
-				<Combobox
-					required
-					type="text"
-					className="rounded-md border border-(--cards-border) bg-(--app-bg) px-2 py-1.25 text-sm text-black dark:text-white"
-				/>
-			</ComboboxProvider>
-			<button
-				onClick={() => {
-					if (newPortfolio) {
-						addPortfolio(newPortfolio)
-						setOpen(false)
-					}
+			<form
+				onSubmit={(e) => {
+					e.preventDefault()
+					const formData = new FormData(e.target as HTMLFormElement)
+					const name = formData.get('name') as string
+					addPortfolio(name)
+					setOpen(false)
 				}}
-				className="ml-auto w-fit rounded-md bg-(--btn-bg) px-6 py-2 hover:bg-(--btn-hover-bg)"
 			>
-				Save
-			</button>
+				<label className="flex flex-col gap-1">
+					<span className="text-sm text-(--text-secondary)">Enter the name of your new portfolio</span>
+					<input
+						required
+						className="rounded-md border border-(--form-control-border) bg-white p-2 text-black disabled:opacity-50 dark:bg-black dark:text-white"
+						name="name"
+					/>
+				</label>
+			</form>
+			<button className="mt-3 rounded-md bg-(--link-active-bg) p-3 text-white disabled:opacity-50">Save</button>
 		</Dialog>
 	)
 }
