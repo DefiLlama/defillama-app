560ae16283ca9b656d7c1327496f2a36bbc88495
mintdart <96025197+mintdart@users.noreply.github.com>
2025-10-22 21:26:55 -0700

    Reapply "update styles"
    This reverts commit 2f96dc670d6a4ec11cd9ac6536d885d5bcf2a85b.

--- src/components/Nav/Desktop.tsx
+++ src/components/Nav/Desktop.tsx
@@ -24,134 +24,138 @@ export const DesktopNav = React.memo(function DesktopNav({
 	const { asPath } = useRouter()
 
 	return (
-		<nav className="thin-scrollbar fixed top-0 bottom-0 left-0 isolate z-10 hidden h-screen w-(--nav-width) flex-col gap-1 overflow-y-auto bg-(--app-bg) p-4 pl-0 *:pl-4 lg:flex">
-			<BasicLink href="/" className="mb-4 w-fit shrink-0">
-				<span className="sr-only">Navigate to Home Page</span>
-				<img
-					src="/icons/defillama.webp"
-					height={53}
-					width={155}
-					className="mr-auto hidden object-contain object-left dark:block"
-					alt=""
-					fetchPriority="high"
-				/>
-				<img
-					src="/icons/defillama-dark.webp"
-					height={53}
-					width={155}
-					className="mr-auto object-contain object-left dark:hidden"
-					alt=""
-					fetchPriority="high"
-				/>
-			</BasicLink>
+		<span className="col-span-1 max-lg:hidden">
+			<nav className="thin-scrollbar sticky top-0 bottom-0 left-0 isolate z-10 col-span-1 flex h-screen flex-col gap-1 overflow-y-auto bg-(--app-bg) py-4 *:pl-4">
+				<BasicLink href="/" className="mb-4 w-fit shrink-0">
+					<span className="sr-only">Navigate to Home Page</span>
+					<img
+						src="/icons/defillama.webp"
+						height={53}
+						width={155}
+						className="mr-auto hidden object-contain object-left dark:block"
+						alt=""
+						fetchPriority="high"
+					/>
+					<img
+						src="/icons/defillama-dark.webp"
+						height={53}
+						width={155}
+						className="mr-auto object-contain object-left dark:hidden"
+						alt=""
+						fetchPriority="high"
+					/>
+				</BasicLink>
 
-			<div className="flex flex-1 flex-col gap-1 overflow-y-auto">
-				{mainLinks.map(({ category, pages }) => (
-					<div key={`desktop-nav-${category}`} className="group flex flex-col">
-						{pages.map(({ name, route, icon, attention }) => (
-							<LinkToPage
-								key={`desktop-nav-${name}-${route}`}
-								route={route}
-								name={name}
-								icon={icon}
-								attention={attention}
-								asPath={asPath}
-							/>
-						))}
-					</div>
-				))}
-
-				<details className="group">
-					<summary className="-ml-1.5 flex items-center justify-between gap-3 rounded-md p-1.5 text-xs opacity-65 hover:bg-black/5 focus-visible:bg-black/5 dark:hover:bg-white/10 dark:focus-visible:bg-white/10">
-						<span>Old Menu</span>
-						<Icon name="chevron-down" className="h-4 w-4 shrink-0 group-open:rotate-180" />
-					</summary>
-					<div className="border-l border-black/20 pl-2 dark:border-white/20">
-						{oldMetricLinks.map(({ name, route, pages }: TOldNavLink) => (
-							<React.Fragment key={`old-nav-desktop-${name}-${route ?? ''}`}>
-								{pages ? (
-									<details className="group/second">
-										<summary className="-ml-1.5 flex items-center justify-between gap-3 rounded-md p-1.5 hover:bg-black/5 focus-visible:bg-black/5 dark:hover:bg-white/10 dark:focus-visible:bg-white/10">
-											<span>{name}</span>
-											<Icon name="chevron-down" className="h-4 w-4 shrink-0 group-open/second:rotate-180" />
-										</summary>
-										<div className="border-l border-black/20 pl-2 dark:border-white/20">
-											{pages.map(({ name, route }) => (
-												<LinkToPage
-													key={`old-desktop-nav-${name}-${route}`}
-													route={route}
-													name={name}
-													asPath={asPath}
-												/>
-											))}
-										</div>
-									</details>
-								) : route ? (
-									<LinkToPage key={`old-desktop-nav-${name}-${route}`} route={route} name={name} asPath={asPath} />
-								) : null}
-							</React.Fragment>
-						))}
-					</div>
-				</details>
-
-				{pinnedPages.length > 0 ? (
-					<div>
-						<p className="flex items-center justify-between gap-3 rounded-md pt-1.5 text-xs opacity-65">Pinned Pages</p>
-
-						{pinnedPages.map(({ name, route }) => (
-							<span key={`pinned-page-${name}-${route}`} className="group relative flex flex-wrap items-center gap-1">
-								<LinkToPage route={route} name={name} asPath={asPath} />
-								<Tooltip
-									content="Unpin from navigation"
-									render={
-										<button
-											onClick={(e) => {
-												const currentPinnedPages = JSON.parse(window.localStorage.getItem('pinned-metrics') || '[]')
-												window.localStorage.setItem(
-													'pinned-metrics',
-													JSON.stringify(currentPinnedPages.filter((page: string) => page !== route))
-												)
-												window.dispatchEvent(new Event('pinnedMetricsChange'))
-												e.preventDefault()
-												e.stopPropagation()
-											}}
-										/>
-									}
-									className="absolute top-1/2 right-1 hidden -translate-y-1/2 rounded-md bg-(--error) px-1 py-1 text-white group-hover:block"
-								>
-									<Icon name="x" className="h-4 w-4" />
-								</Tooltip>
-							</span>
-						))}
-					</div>
-				) : null}
-
-				{userDashboards.length > 0 ? (
-					<div className="group">
-						<p className="flex items-center justify-between gap-3 rounded-md pt-1.5 text-xs opacity-65">
-							Your Dashboards
-						</p>
-						<div>
-							{userDashboards.map(({ name, route }) => (
-								<LinkToPage key={`desktop-nav-${name}-${route}`} route={route} name={name} asPath={asPath} />
+				<div className="flex flex-1 flex-col gap-1 overflow-y-auto">
+					{mainLinks.map(({ category, pages }) => (
+						<div key={`desktop-nav-${category}`} className="group flex flex-col">
+							{pages.map(({ name, route, icon, attention }) => (
+								<LinkToPage
+									key={`desktop-nav-${name}-${route}`}
+									route={route}
+									name={name}
+									icon={icon}
+									attention={attention}
+									asPath={asPath}
+								/>
 							))}
 						</div>
-					</div>
-				) : null}
+					))}
 
-				{footerLinks.map(({ category, pages }) => (
-					<NavDetailsSection key={`desktop-nav-${category}`} category={category} pages={pages} asPath={asPath} />
-				))}
-			</div>
+					<details className="group">
+						<summary className="-ml-1.5 flex items-center justify-between gap-3 rounded-md p-1.5 text-xs opacity-65 hover:bg-black/5 focus-visible:bg-black/5 dark:hover:bg-white/10 dark:focus-visible:bg-white/10">
+							<span>Old Menu</span>
+							<Icon name="chevron-down" className="h-4 w-4 shrink-0 group-open:rotate-180" />
+						</summary>
+						<div className="border-l border-black/20 pl-2 dark:border-white/20">
+							{oldMetricLinks.map(({ name, route, pages }: TOldNavLink) => (
+								<React.Fragment key={`old-nav-desktop-${name}-${route ?? ''}`}>
+									{pages ? (
+										<details className="group/second">
+											<summary className="-ml-1.5 flex items-center justify-between gap-3 rounded-md p-1.5 hover:bg-black/5 focus-visible:bg-black/5 dark:hover:bg-white/10 dark:focus-visible:bg-white/10">
+												<span>{name}</span>
+												<Icon name="chevron-down" className="h-4 w-4 shrink-0 group-open/second:rotate-180" />
+											</summary>
+											<div className="border-l border-black/20 pl-2 dark:border-white/20">
+												{pages.map(({ name, route }) => (
+													<LinkToPage
+														key={`old-desktop-nav-${name}-${route}`}
+														route={route}
+														name={name}
+														asPath={asPath}
+													/>
+												))}
+											</div>
+										</details>
+									) : route ? (
+										<LinkToPage key={`old-desktop-nav-${name}-${route}`} route={route} name={name} asPath={asPath} />
+									) : null}
+								</React.Fragment>
+							))}
+						</div>
+					</details>
 
-			<div className="sticky bottom-0 flex w-full flex-col gap-2 bg-(--app-bg)">
-				<hr className="-ml-1.5 border-black/20 pb-1 dark:border-white/20" />
-				<React.Suspense fallback={<div className="flex min-h-7 w-full items-center justify-center" />}>
-					<Account />
-				</React.Suspense>
-				<ThemeSwitch />
-			</div>
-		</nav>
+					{pinnedPages.length > 0 ? (
+						<div>
+							<p className="flex items-center justify-between gap-3 rounded-md pt-1.5 text-xs opacity-65">
+								Pinned Pages
+							</p>
+
+							{pinnedPages.map(({ name, route }) => (
+								<span key={`pinned-page-${name}-${route}`} className="group relative flex flex-wrap items-center gap-1">
+									<LinkToPage route={route} name={name} asPath={asPath} />
+									<Tooltip
+										content="Unpin from navigation"
+										render={
+											<button
+												onClick={(e) => {
+													const currentPinnedPages = JSON.parse(window.localStorage.getItem('pinned-metrics') || '[]')
+													window.localStorage.setItem(
+														'pinned-metrics',
+														JSON.stringify(currentPinnedPages.filter((page: string) => page !== route))
+													)
+													window.dispatchEvent(new Event('pinnedMetricsChange'))
+													e.preventDefault()
+													e.stopPropagation()
+												}}
+											/>
+										}
+										className="absolute top-1/2 right-1 hidden -translate-y-1/2 rounded-md bg-(--error) px-1 py-1 text-white group-hover:block"
+									>
+										<Icon name="x" className="h-4 w-4" />
+									</Tooltip>
+								</span>
+							))}
+						</div>
+					) : null}
+
+					{userDashboards.length > 0 ? (
+						<div className="group">
+							<p className="flex items-center justify-between gap-3 rounded-md pt-1.5 text-xs opacity-65">
+								Your Dashboards
+							</p>
+							<div>
+								{userDashboards.map(({ name, route }) => (
+									<LinkToPage key={`desktop-nav-${name}-${route}`} route={route} name={name} asPath={asPath} />
+								))}
+							</div>
+						</div>
+					) : null}
+
+					{footerLinks.map(({ category, pages }) => (
+						<NavDetailsSection key={`desktop-nav-${category}`} category={category} pages={pages} asPath={asPath} />
+					))}
+				</div>
+
+				<div className="sticky bottom-0 flex w-full flex-col gap-2 bg-(--app-bg)">
+					<hr className="-ml-1.5 border-black/20 pb-1 dark:border-white/20" />
+					<React.Suspense fallback={<div className="flex min-h-7 w-full items-center justify-center" />}>
+						<Account />
+					</React.Suspense>
+					<ThemeSwitch />
+				</div>
+			</nav>
+		</span>
 	)
 })
 
