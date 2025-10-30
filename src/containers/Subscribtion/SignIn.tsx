ad1b3bd5c10bc50f5100d5b330272ba5bd3aa166
vrtnd <vrotend@proton.me>
2025-10-30 14:52:01 +0300

    fix

--- src/containers/Subscribtion/SignIn.tsx
+++ src/containers/Subscribtion/SignIn.tsx
@@ -242,7 +242,7 @@ export const SignIn = ({
 									id={`${text || 'default'}-signin-email`}
 									type="email"
 									required
-									className="w-full rounded-lg border border-[#39393E] bg-[#222429] p-2.5 pl-[36px] text-sm text-white transition-all duration-200 placeholder:text-[#8a8c90] focus:border-[#5C5CF9] focus:ring-1 focus:ring-[#5C5CF9] focus:outline-hidden sm:p-3"
+									className="w-full rounded-lg border border-[#39393E] bg-[#222429] p-2.5 pl-[36px] text-sm text-white transition-all duration-200 placeholder:text-[#8a8c90] focus:border-[#5C5CF9] focus:ring-1 focus:ring-[#5C5CF9] focus:outline-hidden"
 									value={email}
 									onChange={(e) => setEmail(e.target.value)}
 								/>
@@ -261,7 +261,7 @@ export const SignIn = ({
 									id="signin-password"
 									type="password"
 									required
-									className="w-full rounded-lg border border-[#39393E] bg-[#222429] p-2.5 pl-[36px] text-sm text-white transition-all duration-200 placeholder:text-[#8a8c90] focus:border-[#5C5CF9] focus:ring-1 focus:ring-[#5C5CF9] focus:outline-hidden sm:p-3"
+									className="w-full rounded-lg border border-[#39393E] bg-[#222429] p-2.5 pl-[36px] text-sm text-white transition-all duration-200 placeholder:text-[#8a8c90] focus:border-[#5C5CF9] focus:ring-1 focus:ring-[#5C5CF9] focus:outline-hidden"
 									value={password}
 									onChange={(e) => setPassword(e.target.value)}
 								/>
