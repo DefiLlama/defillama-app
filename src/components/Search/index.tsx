5292a25a9e7aa2e1259ae6537e128fc23bf7dd76
mintdart <96025197+mintdart@users.noreply.github.com>
2025-11-11 10:48:16 -0800

    update styles

--- src/components/Search/index.tsx
+++ src/components/Search/index.tsx
@@ -70,6 +70,8 @@ export const MobileSearch = memo(function MobileSearch() {
 	)
 })
 
+const hideLlamaAI = new Set(['/ai'])
+
 const Mobile = () => {
 	const { query, refine } = useSearchBox()
 
@@ -84,7 +86,7 @@ const Mobile = () => {
 
 	return (
 		<>
-			{!(router.pathname === '/ai' || router.pathname.startsWith('/ai/')) && (
+			{!hideLlamaAI.has(router.pathname) && (
 				<BasicLink
 					href={hasActiveSubscription ? '/ai/chat' : '/ai'}
 					className="llamaai-glow relative -my-0.5 overflow-hidden rounded-md bg-[linear-gradient(93.94deg,#FDE0A9_24.73%,#FBEDCB_57.42%,#FDE0A9_99.73%)] p-3 text-black shadow-[0px_0px_30px_0px_rgba(253,224,169,0.5),_0px_0px_1px_2px_rgba(255,255,255,0.1)] lg:hidden"
@@ -278,7 +280,7 @@ const Desktop = () => {
 					</Ariakit.ComboboxList>
 				</Ariakit.ComboboxPopover>
 			</Ariakit.ComboboxProvider>
-			{!(router.pathname === '/ai' || router.pathname.startsWith('/ai/')) && (
+			{!hideLlamaAI.has(router.pathname) && (
 				<BasicLink
 					href={hasActiveSubscription ? '/ai/chat' : '/ai'}
 					className="llamaai-glow relative mr-auto hidden items-center justify-between gap-[10px] overflow-hidden rounded-md bg-[linear-gradient(93.94deg,#FDE0A9_24.73%,#FBEDCB_57.42%,#FDE0A9_99.73%)] px-4 py-2 text-xs font-semibold text-black shadow-[0px_0px_30px_0px_rgba(253,224,169,0.5),_0px_0px_1px_2px_rgba(255,255,255,0.1)] lg:flex"
