6d2b38fee35d4132e07156a5b035a1b9261d9c84
mintdart <96025197+mintdart@users.noreply.github.com>
2025-12-16 03:38:13 -0800

    update

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -120,8 +120,8 @@ export const CHAINS_API_V2 = `${SERVER_URL}/chains2`
 export const CHAIN_ASSETS_FLOWS = `${SERVER_URL}/chain-assets/flows`
 export const CHAINS_ASSETS_CHART = `${SERVER_URL}/chain-assets/chart`
 
-export const DIMENSIONS_OVERVIEW_API = `${process.env.DIMENSIONS_SERVER_URL ?? SERVER_URL}/overview`
-export const DIMENSIONS_SUMMARY_API = `${process.env.DIMENSIONS_SERVER_URL ?? SERVER_URL}/summary`
+export const DIMENSIONS_OVERVIEW_API = `${SERVER_URL}/overview`
+export const DIMENSIONS_SUMMARY_API = `${SERVER_URL}/summary`
 
 export const getProtocolFEConfig = (id: string) => `${SERVER_URL}/config/smol/protocol-${id}.json`.replace('#', '-')
 
