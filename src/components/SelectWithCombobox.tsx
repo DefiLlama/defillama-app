fdf4607691ccd5bcdcb4c233c4655d3c1c7343e3
Jarren San Jose <sanjose.jarren@gmail.com>
2025-12-28 13:57:57 -0800

    ui: change column filter button labels
    Changed "Toggle all" → "Select All" and "Clear" → "Deselect All"

--- src/components/SelectWithCombobox.tsx
+++ src/components/SelectWithCombobox.tsx
@@ -105,12 +105,12 @@ export function SelectWithCombobox({
 							<span className="sticky top-0 z-1 flex flex-wrap justify-between gap-1 border-b border-(--form-control-border) bg-(--bg-main) text-xs text-(--link)">
 								{clearAll ? (
 									<button onClick={clearAll} className="p-3">
-										Clear
+										Deselect All
 									</button>
 								) : null}
 								{toggleAll ? (
 									<button onClick={toggleAll} className="p-3">
-										Toggle all
+										Select All
 									</button>
 								) : null}
 							</span>
@@ -226,12 +226,12 @@ export function SelectWithCombobox({
 								<span className="sticky top-0 z-1 flex flex-wrap justify-between gap-1 border-b border-(--form-control-border) bg-(--bg-main) text-xs text-(--link)">
 									{clearAll ? (
 										<button onClick={clearAll} className="p-3">
-											Clear
+											Deselect All
 										</button>
 									) : null}
 									{toggleAll ? (
 										<button onClick={toggleAll} className="p-3">
-											Toggle all
+											Select All
 										</button>
 									) : null}
 								</span>
