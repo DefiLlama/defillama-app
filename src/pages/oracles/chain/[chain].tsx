1da91035f7857a57b101add994e0bb4640aae844
mintdart <96025197+mintdart@users.noreply.github.com>
2026-02-05 07:09:18 -0800

    disabled oracle pages

--- src/pages/oracles/chain/[chain].tsx
+++ src/pages/oracles/chain/[chain].tsx
@@ -1,38 +1,42 @@
-import * as React from 'react'
-import { maxAgeForNext } from '~/api'
-import { OraclesByChain } from '~/containers/Oracles'
-import { getOraclePageData, getOraclePageDataByChain } from '~/containers/Oracles/queries'
-import { withPerformanceLogging } from '~/utils/perf'
+// import * as React from 'react'
+// import { maxAgeForNext } from '~/api'
+// import { OraclesByChain } from '~/containers/Oracles'
+// import { getOraclePageData, getOraclePageDataByChain } from '~/containers/Oracles/queries'
+// import { withPerformanceLogging } from '~/utils/perf'
 
-export const getStaticProps = withPerformanceLogging('oracles/[chain]', async ({ params: { chain } }) => {
-	const data = await getOraclePageDataByChain(chain as string)
+// export const getStaticProps = withPerformanceLogging('oracles/[chain]', async ({ params: { chain } }) => {
+// 	const data = await getOraclePageDataByChain(chain as string)
 
-	if (!data) {
-		return { notFound: true }
-	}
+// 	if (!data) {
+// 		return { notFound: true }
+// 	}
 
-	return {
-		props: { ...data },
-		revalidate: maxAgeForNext([22])
-	}
-})
+// 	return {
+// 		props: { ...data },
+// 		revalidate: maxAgeForNext([22])
+// 	}
+// })
 
-export async function getStaticPaths() {
-	const data = await getOraclePageData()
+// export async function getStaticPaths() {
+// 	const data = await getOraclePageData()
 
-	const chainsByOracle = data?.chainsByOracle ?? {}
+// 	const chainsByOracle = data?.chainsByOracle ?? {}
 
-	const chainsLits = [...new Set(Object.values(chainsByOracle).flat())]
+// 	const chainsLits = [...new Set(Object.values(chainsByOracle).flat())]
 
-	const paths = chainsLits.slice(0, 10).map((chain) => {
-		return {
-			params: { chain }
-		}
-	})
+// 	const paths = chainsLits.slice(0, 10).map((chain) => {
+// 		return {
+// 			params: { chain }
+// 		}
+// 	})
 
-	return { paths, fallback: 'blocking' }
-}
+// 	return { paths, fallback: 'blocking' }
+// }
 
-export default function OraclesPage(props) {
-	return <OraclesByChain {...props} />
+// export default function OraclesPage(props) {
+// 	return <OraclesByChain {...props} />
+// }
+
+export default function NotFoundPage() {
+	return <></>
 }
