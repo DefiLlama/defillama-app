b76a7b1f26cd68d87b2d54b3692a8b07e72f3eb5
vrtnd <vrotend@proton.me>
2026-02-16 19:58:59 +0300

    rename

--- src/containers/SuperLuminal/dashboards/spark/index.ts
+++ src/containers/SuperLuminal/dashboards/spark/index.ts
@@ -6,7 +6,7 @@ const LiquidityLayer = lazy(() => import('./LiquidityLayer'))
 const DistributionRewards = lazy(() => import('./DistributionRewards'))
 
 export const tabs: DashboardTabConfig[] = [
-	{ id: 'dashboard', label: 'Dashboard' },
+	{ id: 'dashboard', label: 'Overview' },
 	{ id: 'sparklend', label: 'SparkLend', component: SparkLend, source: 'DefiLlama' },
 	{ id: 'liquidity-layer', label: 'Liquidity Layer', component: LiquidityLayer, source: 'Dune' },
 	{ id: 'distribution-rewards', label: 'Distribution Rewards', component: DistributionRewards, source: 'Dune' }
