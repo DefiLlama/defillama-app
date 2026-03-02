64badfb5b763bcc98fd6aeb071e76be740382b7c
reynardoew <reynardoew@gmail.com>
2026-03-02 13:49:21 +0700

    test fix

--- src/pages/chain/[chain].tsx
+++ src/pages/chain/[chain].tsx
@@ -44,7 +44,7 @@ export async function getStaticPaths() {
 		params: { chain: slug(chain) }
 	}))
 
-	return { paths, fallback: 'blocking' }
+	return { paths: [], fallback: 'blocking' }
 }
 
 export default function Chain(props) {
