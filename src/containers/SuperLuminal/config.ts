29be4bad3f904b1c82f4a72c722b9fa0f0f4df82
vrtnd <vrotend@proton.me>
2026-03-12 18:14:41 +0300

    add bera

--- src/containers/SuperLuminal/config.ts
+++ src/containers/SuperLuminal/config.ts
@@ -1,7 +1,8 @@
 export const SUPERLUMINAL_PROJECTS = [
 	{ id: 'etherfi', name: 'Ether.fi', dashboardId: '73x90j3b28pfhgx' },
 	{ id: 'spark', name: 'Spark', dashboardId: 'roxh2oxb1b7fhjz' },
-	{ id: 'maple', name: 'Maple', dashboardId: 'l5accmh9zooc32q' }
+	{ id: 'maple', name: 'Maple', dashboardId: 'l5accmh9zooc32q' },
+	{ id: 'berachain', name: 'Berachain', dashboardId: 't62luatlj9thwx2' }
 ] as const
 
 export const SUPERLUMINAL_PROTOCOL_IDS: string[] = SUPERLUMINAL_PROJECTS.map((p) => p.id)
