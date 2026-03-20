b38f8c6ed1e7b92525f07db2d9f4fbc55fdcb507
0xngmi <0xngmi@protonmail.com>
2026-03-20 01:36:02 +0000

    remove rwa announcement

--- src/containers/ChainOverview/Announcement.tsx
+++ src/containers/ChainOverview/Announcement.tsx
@@ -2,12 +2,5 @@ import { Announcement } from '~/components/Announcement'
 import { BasicLink } from '~/components/Link'
 
 export const ChainOverviewAnnouncement = () => {
-	return (
-		<Announcement announcementId="rwa-dashboard" version="2026-03">
-			NEW!{' '}
-			<BasicLink href="/rwa" className="underline">
-				RWA dashboard
-			</BasicLink>
-		</Announcement>
-	)
+	return null;
 }
