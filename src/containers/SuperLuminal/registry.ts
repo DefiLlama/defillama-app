dd0ab284dd0262f3d4bfce466c2bb66b6d95c9df
vrtnd <vrotend@proton.me>
2026-03-31 16:43:33 +0300

    update dashboard

--- src/containers/SuperLuminal/registry.ts
+++ src/containers/SuperLuminal/registry.ts
@@ -15,7 +15,7 @@ export interface DashboardModule {
 const DASHBOARD_REGISTRY: Record<string, () => Promise<DashboardModule>> = {
 	eobgdbgg0d0hake: () => import('./dashboards/hyperliquid'),
 	'73x90j3b28pfhgx': () => import('./dashboards/etherfi'),
-	roxh2oxb1b7fhjz: () => import('./dashboards/spark'),
+	'9vn8xp43tdzo6gt': () => import('./dashboards/spark'),
 	l5accmh9zooc32q: () => import('./dashboards/maple'),
 	t62luatlj9thwx2: () => import('./dashboards/berachain'),
 	g3rswlkr9khxa03: () => import('./dashboards/aave'),
