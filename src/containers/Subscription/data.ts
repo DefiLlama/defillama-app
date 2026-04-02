c05f982272ecd38a0fad8faa9a87c68c7fd5e1e3
reynardoew <reynardoew@gmail.com>
2026-04-02 19:19:06 +0700

    fix smol inconsistency

--- src/containers/Subscription/data.ts
+++ src/containers/Subscription/data.ts
@@ -213,19 +213,19 @@ export const YEARLY_PRICING_CARDS: PricingCardData[] = [
 				title: 'API Access',
 				items: [
 					{
-						label: 'Access to TVL, revenue/fees and prices API endpoints',
-						availability: 'check'
-					},
-					{
 						label: 'Access to all data (unlocks, active users, token liq...)',
 						availability: 'check'
 					},
-					{ label: 'Priority support', availability: 'check' },
+					{
+						label: 'Access to TVL, revenue/fees and prices API endpoints',
+						availability: 'check'
+					},
 					{
 						label: 'Access to DefiLlama MCP Server for AI agents',
 						link: '/mcp',
 						availability: 'check'
 					},
+					{ label: 'Priority support', availability: 'check' },
 					{ label: '1000 requests/minute', availability: 'check' },
 					{ label: '1M calls/month', availability: 'check' },
 					{
