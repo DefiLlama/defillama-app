5bc8d0efaaea38f7216907a6f75c4fecc49d1328
caranell <caranell@proton.me>
2026-04-08 11:09:14 +0300

    Add user tracking to main announcement (#2778)
    * fix link + add tracking
    
    * Upd announcement
    
    * remove noreferrer

--- src/containers/ChainOverview/Announcement.tsx
+++ src/containers/ChainOverview/Announcement.tsx
@@ -1,10 +1,17 @@
 import { Announcement } from '~/components/Announcement'
+import { useAuthContext } from '../Subscription/auth'
 
 export const ChainOverviewAnnouncement = () => {
+	const { user } = useAuthContext()
 	return (
 		<Announcement announcementId="investor-relations" version="2026-04-07">
 			NEW!{' '}
-			<a href="https://investors.defillama.com/" target="_blank" rel="noreferrer noopener" className="underline">
+			<a
+				href={`https://investors.defillama.com/${user?.id ? `?referrer=${user.id}` : ''}`}
+				target="_blank"
+				rel="noopener"
+				className="underline"
+			>
 				Investor Relations
 			</a>{' '}
 			| View curated protocol dashboards and announcements
