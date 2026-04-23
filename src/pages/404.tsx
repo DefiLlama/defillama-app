a0287a07a8c7a366131c118ea7a673be1248ed0b
mintdart <96025197+mintdart@users.noreply.github.com>
2026-04-23 11:17:17 -0700

    add link to protocol page on /yields/pool/:pool
    - add link to protocol page on /yields/pool/:pool
    - return 404 if pool not found

--- src/pages/404.tsx
+++ src/pages/404.tsx
@@ -1,23 +1,5 @@
-import { BasicLink } from '~/components/Link'
-import Layout from '~/layout'
+import { NotFoundPage } from '~/components/NotFoundPage'
 
 export default function HomePage() {
-	return (
-		<Layout
-			title="Page not found - DefiLlama"
-			description={`DefiLlama is committed to providing accurate data without ads or sponsored content, as well as transparency.`}
-			canonicalUrl={`/404`}
-		>
-			<div className="isolate flex flex-1 flex-col items-center justify-center rounded-md border border-(--cards-border) bg-(--cards-bg) p-1">
-				<img src="/assets/404.png" width={350} height={350} alt="Want a ride?" />
-				<p className="text-center text-base text-(--text-label)">
-					This page doesn&apos;t exist. Check out{' '}
-					<BasicLink href="/metrics" className="underline">
-						other dashboards
-					</BasicLink>
-					.
-				</p>
-			</div>
-		</Layout>
-	)
+	return <NotFoundPage />
 }
