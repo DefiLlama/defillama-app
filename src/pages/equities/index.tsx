48925cdb2373653389cf3e39e67d7f90b3dac01e
mintdart <96025197+mintdart@users.noreply.github.com>
2026-06-08 10:34:51 -0700

    temporarily disable equities page

--- src/pages/equities/index.tsx
+++ src/pages/equities/index.tsx
@@ -1,28 +1,43 @@
-import type { InferGetStaticPropsType } from 'next'
-import { EquitiesOverview } from '~/containers/Equities'
-import { getEquitiesListPageData } from '~/containers/Equities/queries'
-import Layout from '~/layout'
-import { maxAgeForNext } from '~/utils/maxAgeForNext'
-import { withPerformanceLogging } from '~/utils/perf'
+// import type { InferGetStaticPropsType } from 'next'
+// import { EquitiesOverview } from '~/containers/Equities'
+// import { getEquitiesListPageData } from '~/containers/Equities/queries'
+// import Layout from '~/layout'
+// import { maxAgeForNext } from '~/utils/maxAgeForNext'
+// import { withPerformanceLogging } from '~/utils/perf'
 
-export const getStaticProps = withPerformanceLogging('equities/index', async () => {
-	const props = await getEquitiesListPageData()
+import { TemporarilyDisabledPage } from '~/components/TemporarilyDisabledPage'
 
-	return {
-		props,
-		revalidate: maxAgeForNext([5])
-	}
-})
+// export const getStaticProps = withPerformanceLogging('equities/index', async () => {
+// 	const props = await getEquitiesListPageData()
 
-export default function EquitiesIndexPage(props: InferGetStaticPropsType<typeof getStaticProps>) {
+// 	return {
+// 		props,
+// 		revalidate: maxAgeForNext([5])
+// 	}
+// })
+
+// export default function EquitiesIndexPage(props: InferGetStaticPropsType<typeof getStaticProps>) {
+// 	return (
+// 		<Layout
+// 			title="Equities Rankings - DefiLlama"
+// 			description="Track public equities by market cap, price, trading volume, and 24h change on DefiLlama."
+// 			canonicalUrl="/equities"
+// 			pageName={['Equities']}
+// 		>
+// 			<EquitiesOverview {...props} />
+// 		</Layout>
+// 	)
+// }
+
+export default function EquityTickerDetailPage() {
 	return (
-		<Layout
-			title="Equities Rankings - DefiLlama"
-			description="Track public equities by market cap, price, trading volume, and 24h change on DefiLlama."
-			canonicalUrl="/equities"
-			pageName={['Equities']}
+		<TemporarilyDisabledPage
+			title="Equities"
+			description="This page is temporarily disabled and will be back shortly."
+			canonicalUrl="/equities/[ticker]"
 		>
-			<EquitiesOverview {...props} />
-		</Layout>
+			<p>Equity ticker data is not available on DefiLlama for the time being.</p>
+			<p>We&apos;re working on bringing this page back in a future update.</p>
+		</TemporarilyDisabledPage>
 	)
 }
